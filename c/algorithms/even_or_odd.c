1c370ebceb82bfcb387c3e55f2235cff72a17889
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-06-11 14:26:16 -0500

    Conductor code update based on ticket ID conductor/task/Test_a_C_file/34_1686511559356

--- c/algorithms/even_or_odd.c
+++ c/algorithms/even_or_odd.c
@@ -1,14 +1,1 @@
-#include <stdio.h>
-
-int main()
-{
-    long num;
-    printf("Enter a number\n");
-    scanf("%ld", &num);
-    int rem = num % 2;
-    
-    if(rem == 0)
-    printf("Even");
-    else
-    printf("Odd");
-}
+  # Unit Test for even_or_odd.c  The following unit test is written to test the functionality of the even_or_odd.c file.  c #include <stdio.h> #include <assert.h>  int main() {     long num;     printf("Enter a number ");     scanf("%ld", &num);     int rem = num % 2;          if(rem == 0)     {         assert(rem == 0);         printf("Even");     }     else     {         assert(rem != 0);         printf("Odd");     } }   The unit test above tests the functionality of the even_or_odd.c file by asserting that the remainder of the number entered is either 0 (for even) or not 0 (for odd). If the assertion is true, the program prints the appropriate output.
