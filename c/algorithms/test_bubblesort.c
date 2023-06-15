c14b81fb78ca360cc75e219825a29ea22d908a6b
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-06-15 15:24:56 -0500

    Conductor code update based on ticket ID conductor/task/Write_unit_test_for_C_file_/47_1686860663736

---
+++ c/algorithms/test_bubblesort.c
@@ -0,0 +1,57 @@
+
+
+#include <stdio.h>
+#include <assert.h>
+
+void bubble_sort(int *array, int len) 
+{
+    for (int i = 0; i < len; i++) {
+        for (int j = 0; j < len - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                int tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+            }
+        }
+    }
+}
+
+int main(void) 
+{
+    int yarr[] = {3, 9, 4, 8, 7, 6, 1, 2, 0, 10};
+    
+    bubble_sort(yarr, 10);
+
+    printf("[");
+    for (int i = 0; i < 10; i++)
+        printf("%d, ", yarr[i]);	
+    printf("\b\b]\n");
+    return 0;
+}
+
+void test_bubble_sort() {
+    int arr[] = {3, 9, 4, 8, 7, 6, 1, 2, 0, 10};
+    int expected[] = {0, 1, 2, 3, 4, 6, 7, 8, 9, 10};
+    int len = 10;
+
+    bubble_sort(arr, len);
+
+    for (int i = 0; i < len; i++) {
+        assert(arr[i] == expected[i]);
+    }
+}
+
+int main(void) 
+{
+    test_bubble_sort();
+
+    int yarr[] = {3, 9, 4, 8, 7, 6, 1, 2, 0, 10};
+    
+    bubble_sort(yarr, 10);
+
+    printf("[");
+    for (int i = 0; i < 10; i++)
+        printf("%d, ", yarr[i]);	
+    printf("\b\b]\n");
+    return 0;
+}
