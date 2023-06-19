dc19a614b0a4527fa6a8bdac2ac82504c4b42be8
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-06-19 15:52:27 -0500

    Conductor code update based on ticket ID conductor/task/Test_a_C_file/55_1687207913124

--- c/algorithms/bubblesort.c
+++ c/algorithms/bubblesort.c
@@ -1,4 +1,7 @@
+
+
 #include <stdio.h>
+#include <assert.h>
 
 void bubble_sort(int *array, int len) 
 {
@@ -13,8 +16,23 @@ void bubble_sort(int *array, int len)
     }
 }
 
+void test_bubble_sort()
+{
+    int arr[] = {3, 9, 4, 8, 7, 6, 1, 2, 0, 10};
+    int expected[] = {0, 1, 2, 3, 4, 6, 7, 8, 9, 10};
+    int len = 10;
+
+    bubble_sort(arr, len);
+
+    for (int i = 0; i < len; i++) {
+        assert(arr[i] == expected[i]);
+    }
+}
+
 int main(void) 
 {
+    test_bubble_sort();
+
     int yarr[] = {3, 9, 4, 8, 7, 6, 1, 2, 0, 10};
     
     bubble_sort(yarr, 10);
