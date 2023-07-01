fdb55c948c8ba9434ff14e505124ac8c0023ea12
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-07-01 12:53:42 -0500

    Conductor code update based on ticket ID conductor/task/create_a_unit_test/68_1688233960605

--- c/algorithms/even_or_odd.c
+++ c/algorithms/even_or_odd.c
@@ -1,14 +1,38 @@
+
+
+
+// even_or_odd.c
 #include <stdio.h>
 
-int main()
-{
-    long num;
-    printf("Enter a number\n");
-    scanf("%ld", &num);
-    int rem = num % 2;
-    
-    if(rem == 0)
-    printf("Even");
-    else
-    printf("Odd");
+int even_or_odd(int num) {
+    if (num % 2 == 0) {
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (even_or_odd(num) == 0) {
+        printf("The number is even.\n");
+    } else {
+        printf("The number is odd.\n");
+    }
+    return 0;
+}
+
+// even_or_odd_test.c
+#include <stdio.h>
+#include <assert.h>
+
+int even_or_odd(int num);
+
+int main() {
+    assert(even_or_odd(2) == 0);
+    assert(even_or_odd(3) == 1);
+    printf("All tests passed!\n");
+    return 0;
 }
