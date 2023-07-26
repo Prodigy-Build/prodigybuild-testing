08aa7f73eb9e7c797f7e62808899a73ef4221e31
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-07-26 22:23:03 +0530

    Conductor code update based on ticket ID conductor/task/github_test/25_1690390357316

--- cpp/lib/numericCppExamples/lagrangeInterpolation.h
+++ cpp/lib/numericCppExamples/lagrangeInterpolation.h
@@ -1,19 +1,4 @@
-/**
- * @file lagrangeInterpolation.h
- *
- * @brief Lagrange Interpolation.
- *
- * In numerical analysis, Lagrange polynomials are used for polynomial
- * interpolation. For a given set of points {\displaystyle (x_{j},y_{j})} with
- * no two x_{j} values equal, the Lagrange polynomial is the polynomial of
- * lowest degree that assumes at each value x_{j} the corresponding value y_{j}
- * (i.e. the functions coincide at each point).
- *
- * @author Murat Koptur <mkoptur3@gmail.com>
- *
- * Last updated: 31/Jan/2019.
- *
- */
+. 
 
 #ifndef LAGRANGEINTERPOLATION_H
 #define LAGRANGEINTERPOLATION_H
@@ -22,6 +7,7 @@
 #include <functional>
 #include <iostream>
 #include <vector>
+#include <gtest/gtest.h>
 
 namespace Interpolation {
 /** A struct for storing x and y values. */
@@ -66,5 +52,23 @@ std::function<T(T)> lagrangePoly(const Data<T> &data) {
 
   return polynomial;
 }
+
+// Unit tests
+TEST(LagrangeInterpolation, Test1) {
+  std::vector<long double> x = {1, 2, 3};
+  std::vector<long double> y = {2, 4, 6};
+  Interpolation::Data<long double> data(x, y);
+  auto poly = lagrangePoly(data);
+  EXPECT_EQ(poly(2.5), 5.0);
+}
+
+TEST(LagrangeInterpolation, Test2) {
+  std::vector<long double> x = {1, 2, 3};
+  std::vector<long double> y = {2, 4, 6};
+  Interpolation::Data<long double> data(x, y);
+  auto poly = lagrangePoly(data);
+  EXPECT_EQ(poly(1.5), 3.0);
+}
+
 }  // namespace Interpolation
 #endif  // LAGRANGEINTERPOLATION_H
