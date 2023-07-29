c87bae1bed593153fdb8a98d5900ab73af8966ab
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-07-29 16:39:51 +0530

    Conductor code update based on ticket ID conductor/task/test_AI/52_1690628963637

---
+++ c/algorithms/linearsearch_test.c
@@ -0,0 +1,38 @@
+
+
+#include <stdio.h>
+
+int linsearch(int *yarr, int val)
+{
+    for(int i = 0; i < sizeof(yarr); i++)
+    {
+        if(yarr[i] == val)
+        {
+            return 0;
+        }    
+        else
+        {
+          return 1;
+        }
+    }
+}
+
+int main()
+{
+    int yarr[] = {1, 2, 3, 4, 5};
+    int val = 3;
+    linsearch(yarr, val);
+}
+
+// Unit Test Cases
+void test_linsearch() {
+    int yarr[] = {1, 2, 3, 4, 5};
+    assert(linsearch(yarr, 3) == 0);
+    assert(linsearch(yarr, 6) == 1);
+}
+
+int main() {
+    test_linsearch();
+    printf("All tests passed!\n");
+    return 0;
+}
