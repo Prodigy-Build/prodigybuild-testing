44f5216fb0e3d84c9f88fd9b5f607a36419d7d4f
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-08-15 22:46:40 +0530

    Conductor code update based on ticket ID conductor/task/Test_2_-_AI/117_1692119788181

--- c/algorithms/fac.c
+++ c/algorithms/fac.c
@@ -1,5 +1,8 @@
+.
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 int fak(int n) {
     if (n == 0 || n == 1) {
@@ -19,6 +22,14 @@ int main() {
     printf("fak(5): %d\n", fak(5));
     
     printf("fak(-1): %d\n", fak(-1));
+
+    // Unit test cases
+    assert(fak(1) == 1);
+    assert(fak(2) == 2);
+    assert(fak(3) == 6);
+    assert(fak(4) == 24);
+    assert(fak(5) == 120);
+    assert(fak(-1) == 1);
 
     return 1;
 }
