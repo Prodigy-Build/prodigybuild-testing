9dcc0a22e449c8f3162c047f9699b6c1fe96b69e
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-08-19 00:33:54 +0530

    Conductor code update based on ticket ID conductor/task/18th_Aug_-_Gitlab_Integration_-_AI_Testing_-_2/108_1692385424861

--- c/algorithms/count.c
+++ c/algorithms/count.c
@@ -1,6 +1,9 @@
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <assert.h>
 
 int count(int num) 
 {
@@ -31,4 +34,16 @@ int main()
     printf("Count:~$ ");
     scanf("%d", &num);
     count(num);
+}
+
+// Unit Test Cases
+void test_count() {
+    assert(count(5) == "[0, 1, 2, 3, 4]");
+    assert(count(20) == "[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]");
+}
+
+int main() {
+    test_count();
+    printf("All tests passed!");
+    return 0;
 }
