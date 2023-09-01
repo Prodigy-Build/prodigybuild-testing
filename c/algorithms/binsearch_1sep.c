bf10860d0688821d35c25a870591184940d6b773
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-09-01 17:50:28 +0530

    Conductor code update based on ticket ID conductor/task/Test_AI_-_Sep1_-_1/200_1693570802765

---
+++ c/algorithms/binsearch_1sep.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <assert.h>
+
+int binsearch(int yarr[10], int element)
+{
+    int mid = sizeof(yarr) % 2;
+    int left = (int)yarr / 2 - mid;
+    int right = yarr - left;
+    extern int i;
+    
+    if(element == mid)
+    printf("%d", &mid);
+    
+    if(element != yarr[right])
+    {
+        for(i = 0; i < left; i++)
+        {
+            if(element == i)
+            {
+            printf("%d", i);
+            }
+        }
+    }
+
+    return -1;
+};
+
+void test_binsearch() {
+    int yarr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    
+    assert(binsearch(yarr, 4) == 4);
+    assert(binsearch(yarr, 11) == -1);
+}
+
+int main(int argc, int argv[])
+{
+    test_binsearch();
+    return 0;
+}
