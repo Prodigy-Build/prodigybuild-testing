7f4500c80a3adbbb5d0b01dc0c3f48cd0ca570f0
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-09-01 18:06:27 +0530

    Conductor code update based on ticket ID conductor/task/Test_AI_-_Sep1_-_1/201_1693571757536

---
+++ c/algorithms/binsearch_1sep.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <assert.h>
+
+int binsearch(int yarr[10], int element)
+{
+    int mid = sizeof(yarr) / sizeof(yarr[0]) / 2;
+    int left = 0;
+    int right = sizeof(yarr) / sizeof(yarr[0]) - 1;
+    
+    while(left <= right)
+    {
+        mid = left + (right - left) / 2;
+        if(yarr[mid] == element)
+        {
+            return mid;
+        }
+        else if(yarr[mid] < element)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+    return -1;
+};
+
+void testCases()
+{
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    assert(binsearch(arr, 5) == 4);
+    assert(binsearch(arr, 11) == -1);
+    assert(binsearch(arr, 1) == 0);
+    assert(binsearch(arr, 10) == 9);
+    printf("All test cases passed.\n");
+}
+
+int main()
+{
+    testCases();
+    return 0;
+}
