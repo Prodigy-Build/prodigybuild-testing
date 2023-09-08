6074d49dd69f204edf7ed82cdd4bc327736711e8
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-09-08 23:26:02 +0530

    Conductor code update based on ticket ID conductor/task/Test_AI/50_1694195761738

--- c/algorithms/quicksort.c
+++ c/algorithms/quicksort.c
@@ -1,74 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
 
-void swap(int *a, int *b);
-int partition(int *a, int l, int h);
-void quick_sort(int *a, int l, int h);
-
-int main()
-{
-    int size;
-    printf("Size: ");
-    scanf("%d", &size);
-
-    int *arr = malloc(size * sizeof(int));
-    printf("Array elements: ");
-    for (int i = 0; i < size; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
-
-    quick_sort(arr, 0, size - 1);
-
-    printf("Sorted array:");
-    for (int i = 0; i < size; i++)
-    {
-        printf(" %d", arr[i]);
-    }
-    printf("\n");
-
-    free(arr);
-    return 0;
+void test_partition() {
+    int a[] = {5, 2, 9, 3, 6, 0};
+    int p = partition(a, 0, 6);
+    assert(p == 3);
+    assert(a[p] == 5);
 }
 
-void swap(int *a, int *b)
-{
-    int tmp;
-    tmp = *a;
-    *a = *b;
-    *b = tmp;
-    return;
-}
-
-int partition(int a[], int l, int h)
-{
-    int i = l, j = l, p = h;
-
-    while (i < h)
-    {
-        if (a[i] < a[p])
-        {
-            swap(&a[i], &a[j]);
-            j++;
+void test_quick_sort() {
+    int a[] = {5, 2, 9, 3, 6, 0};
+    quick_sort(a, 0, 6);
+    bool is_sorted = true;
+    for (int i = 0; i < 6 - 1; i++) {
+        if (a[i] > a[i + 1]) {
+            is_sorted = false;
+            break;
         }
-        i++;
     }
-
-    swap(&a[p], &a[j]);
-
-    return j;
+    assert(is_sorted);
 }
 
-void quick_sort(int a[], int l, int h)
-{
-    int p;
+void test_swap() {
+    int a = 5;
+    int b = 2;
+    swap(&a, &b);
+    assert(a == 2);
+    assert(b == 5);
+}
 
-    if (l < h)
-    {
-        p = partition(a, l, h);
-        quick_sort(a, l, p - 1);
-        quick_sort(a, p + 1, h);
-    }
-
-    return;
+int main() {
+    test_partition();
+    test_quick_sort();
+    test_swap();
+    printf("All tests passed successfully.\n");
+    return 0;
 }
