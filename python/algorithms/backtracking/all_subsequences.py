3acb14f7ac4269010381817f5ca549a94cd28648
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-11 08:50:01 -0500

    Conductor code update based on ticket ID conductor/task/Test_a_Python_file/135_1694440199779

--- python/algorithms/backtracking/all_subsequences.py
+++ python/algorithms/backtracking/all_subsequences.py
@@ -1,42 +1,43 @@
+
+
 """
-In this problem, we want to determine all possible subsequences
-of the given sequence. We use backtracking to solve this problem.
+In this problem, we want to identify all possible subsequences
+of the provided sequence. It will be solved using backtracking.
 
 Time complexity: O(2^n),
-where n denotes the length of the given sequence.
+Where n is the length of the given sequence.
 """
-from __future__ import annotations
-
-from typing import Any
+from typing import Any, List
 
 
-def generate_all_subsequences(sequence: list[Any]) -> None:
-    create_state_space_tree(sequence, [], 0)
+def generate_all_subsequences(sequence: List[Any]) -> None:
+    _create_state_space_tree(sequence, [], 0)
 
 
-def create_state_space_tree(
-    sequence: list[Any], current_subsequence: list[Any], index: int
+def _create_state_space_tree(
+    sequence: List[Any], current_subsequence: List[Any], index: int
 ) -> None:
     """
-    Creates a state space tree to iterate through each branch using DFS.
-    We know that each state has exactly two children.
-    It terminates when it reaches the end of the given sequence.
+    Creating a state space tree to iterate through each branch by using DFS.
+    Each state has exactly two children. It will stop 
+    when it reaches the end of the given sequence.
     """
 
     if index == len(sequence):
         print(current_subsequence)
         return
 
-    create_state_space_tree(sequence, current_subsequence, index + 1)
+    _create_state_space_tree(sequence, current_subsequence, index + 1)
     current_subsequence.append(sequence[index])
-    create_state_space_tree(sequence, current_subsequence, index + 1)
+    _create_state_space_tree(sequence, current_subsequence, index + 1)
     current_subsequence.pop()
 
 
 if __name__ == "__main__":
-    seq: list[Any] = [3, 1, 2, 4]
+    seq: List[Any] = [3, 1, 2, 4]
     generate_all_subsequences(seq)
 
     seq.clear()
     seq.extend(["A", "B", "C"])
     generate_all_subsequences(seq)
+
