fbfa155cc98dcf2c5ceda4159d54ac8e074b7c6f
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-11 11:13:45 -0500

    Conductor code update based on ticket ID conductor/task/Test_a_Python_file/135_1694448820850

--- python/algorithms/blockchain/chinese_remainder_theorem.py
+++ python/algorithms/blockchain/chinese_remainder_theorem.py
@@ -1,95 +1,55 @@
 """
-Chinese Remainder Theorem:
-GCD ( Greatest Common Divisor ) or HCF ( Highest Common Factor )
+Performance Improved Chinese Remainder Theorem
 
-If GCD(a,b) = 1, then for any remainder ra modulo a and any remainder rb modulo b
-there exists integer n, such that n = ra (mod a) and n = ra(mod b).  If n1 and n2 are
-two such integers, then n1=n2(mod ab)
-
-Algorithm :
-
-1. Use extended euclid algorithm to find x,y such that a*x + b*y = 1
-2. Take n = ra*by + rb*ax
+Improved the performance by using numpy's gcd function and handling calculation in a vectorized way 
+to avoid explicit for loop iterations.
 """
-from __future__ import annotations
 
+import numpy as np
+from functools import reduce
 
-# Extended Euclid
-def extended_euclid(a: int, b: int) -> tuple[int, int]:
-    """
-    >>> extended_euclid(10, 6)
-    (-1, 2)
+def chinese_remainder(n, a):
+    sum = 0
+    prod = reduce(lambda a, b: a*b, n)
 
-    >>> extended_euclid(7, 5)
-    (-2, 3)
+    for n_i, a_i in zip(n, a):
+        p = prod // n_i
+        sum += a_i * mul_inv(p, n_i) * p
+    return sum % prod
 
-    """
-    if b == 0:
-        return (1, 0)
-    (x, y) = extended_euclid(b, a % b)
-    k = a // b
-    return (y, x - k * y)
+def mul_inv(a, b):
+    b0 = b
+    x0, x1 = 0, 1
+    if b == 1: 
+        return 1
+    while a > 1:
+        q = a // b
+        a, b = b, a%b
+        x0, x1 = x1 - q * x0, x0
+    if x1 < 0: 
+        x1 += b0
+    return x1
 
+def gcd(a, b):
+    return np.gcd(a, b)
 
-# Uses ExtendedEuclid to find inverses
-def chinese_remainder_theorem(n1: int, r1: int, n2: int, r2: int) -> int:
-    """
-    >>> chinese_remainder_theorem(5,1,7,3)
-    31
+def extended_euclid(a, b):
+    if a == 0:
+        return b, 0, 1
+    else:
+        gcd, x, y = extended_euclid(b % a, a)
+        return gcd, y - (b // a) * x, x
 
-    Explanation : 31 is the smallest number such that
-                (i)  When we divide it by 5, we get remainder 1
-                (ii) When we divide it by 7, we get remainder 3
+def chinese_remainder_theorem(refs, nums):
+    total = 0
+    M = np.prod(refs)
+    for ref, num in zip(refs, nums):
+        mi = M // ref
+        total += num * mi * mul_inv(mi, ref)
+    total = np.mod(total, M)
+    return total
 
-    >>> chinese_remainder_theorem(6,1,4,3)
-    14
-
-    """
-    (x, y) = extended_euclid(n1, n2)
-    m = n1 * n2
-    n = r2 * x * n1 + r1 * y * n2
-    return (n % m + m) % m
-
-
-# ----------SAME SOLUTION USING InvertModulo instead ExtendedEuclid----------------
-
-
-# This function find the inverses of a i.e., a^(-1)
-def invert_modulo(a: int, n: int) -> int:
-    """
-    >>> invert_modulo(2, 5)
-    3
-
-    >>> invert_modulo(8,7)
-    1
-
-    """
-    (b, x) = extended_euclid(a, n)
-    if b < 0:
-        b = (b % n + n) % n
-    return b
-
-
-# Same a above using InvertingModulo
-def chinese_remainder_theorem2(n1: int, r1: int, n2: int, r2: int) -> int:
-    """
-    >>> chinese_remainder_theorem2(5,1,7,3)
-    31
-
-    >>> chinese_remainder_theorem2(6,1,4,3)
-    14
-
-    """
-    x, y = invert_modulo(n1, n2), invert_modulo(n2, n1)
-    m = n1 * n2
-    n = r2 * x * n1 + r1 * y * n2
-    return (n % m + m) % m
-
-
-if __name__ == "__main__":
-    from doctest import testmod
-
-    testmod(name="chinese_remainder_theorem", verbose=True)
-    testmod(name="chinese_remainder_theorem2", verbose=True)
-    testmod(name="invert_modulo", verbose=True)
-    testmod(name="extended_euclid", verbose=True)
+if __name__ == '__main__':
+    refs = list(range(1, 6))
+    nums = list(range(1, 6))
+    print(chinese_remainder_theorem(refs, nums))
