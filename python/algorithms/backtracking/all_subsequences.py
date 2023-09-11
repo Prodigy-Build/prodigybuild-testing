c925ed713a740b8159e1ead2877fdbdb93d9b190
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-11 12:15:34 -0500

    Conductor code update based on ticket ID conductor/task/Test_a_Python_file/135_1694452533009

--- python/algorithms/backtracking/all_subsequences.py
+++ python/algorithms/backtracking/all_subsequences.py
@@ -1,42 +1,26 @@
 """
-In this problem, we want to determine all possible subsequences
-of the given sequence. We use backtracking to solve this problem.
-
-Time complexity: O(2^n),
-where n denotes the length of the given sequence.
+Backtracking algorithm to generate all possible subsequences
+of a given sequence.
 """
-from __future__ import annotations
 
-from typing import Any
-
-
-def generate_all_subsequences(sequence: list[Any]) -> None:
-    create_state_space_tree(sequence, [], 0)
+from typing import List, Any
 
 
-def create_state_space_tree(
-    sequence: list[Any], current_subsequence: list[Any], index: int
-) -> None:
-    """
-    Creates a state space tree to iterate through each branch using DFS.
-    We know that each state has exactly two children.
-    It terminates when it reaches the end of the given sequence.
-    """
+def generate_all_subsequences(sequence: List[Any], index: int = 0, subsequence: List[Any] = None) -> None:
+    if subsequence is None:
+        subsequence = []
 
     if index == len(sequence):
-        print(current_subsequence)
+        print(subsequence)
         return
 
-    create_state_space_tree(sequence, current_subsequence, index + 1)
-    current_subsequence.append(sequence[index])
-    create_state_space_tree(sequence, current_subsequence, index + 1)
-    current_subsequence.pop()
+    generate_all_subsequences(sequence, index + 1, subsequence)
+    generate_all_subsequences(sequence, index + 1, subsequence + [sequence[index]])
 
 
 if __name__ == "__main__":
-    seq: list[Any] = [3, 1, 2, 4]
+    seq: List[Any] = [3, 1, 2, 4]
     generate_all_subsequences(seq)
 
-    seq.clear()
-    seq.extend(["A", "B", "C"])
+    seq = ["A", "B", "C"]
     generate_all_subsequences(seq)
