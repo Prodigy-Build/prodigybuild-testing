f3c99de94d5245ff0479f8eeffee3cb38e5ccd1c
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-11 08:37:08 -0500

    Conductor code update based on ticket ID conductor/task/Test_a_Python_file/135_1694439426493

--- python/algorithms/backtracking/all_subsequences.py
+++ python/algorithms/backtracking/all_subsequences.py
@@ -6,37 +6,41 @@ Time complexity: O(2^n),
 where n denotes the length of the given sequence.
 """
 from __future__ import annotations
-
-from typing import Any
-
-
-def generate_all_subsequences(sequence: list[Any]) -> None:
-    create_state_space_tree(sequence, [], 0)
+from typing import Any, List
 
 
-def create_state_space_tree(
-    sequence: list[Any], current_subsequence: list[Any], index: int
-) -> None:
-    """
-    Creates a state space tree to iterate through each branch using DFS.
-    We know that each state has exactly two children.
-    It terminates when it reaches the end of the given sequence.
-    """
+class SubsequenceGenerator:
 
-    if index == len(sequence):
-        print(current_subsequence)
-        return
+    def __init__(self, sequence: List[Any]):
+        self.sequence = sequence
 
-    create_state_space_tree(sequence, current_subsequence, index + 1)
-    current_subsequence.append(sequence[index])
-    create_state_space_tree(sequence, current_subsequence, index + 1)
-    current_subsequence.pop()
+    def generate_all_subsequences(self) -> None:
+        self._create_state_space_tree([], 0)
+        
+    def _create_state_space_tree(self, current_subsequence: List[Any], index: int) -> None:
+        """
+        Creates a state space tree to iterate through each branch using DFS.
+        We know that each state has exactly two children.
+        It terminates when it reaches the end of the given sequence.
+        """
+        if index == len(self.sequence):
+            print(current_subsequence)
+            return
+
+        self._create_state_space_tree(current_subsequence, index + 1)
+        current_subsequence.append(self.sequence[index])
+        self._create_state_space_tree(current_subsequence, index + 1)
+        current_subsequence.pop()
+
+
+def main():
+    generator = SubsequenceGenerator([3, 1, 2, 4])
+    generator.generate_all_subsequences()
+
+    generator = SubsequenceGenerator(["A", "B", "C"])
+    generator.generate_all_subsequences()
 
 
 if __name__ == "__main__":
-    seq: list[Any] = [3, 1, 2, 4]
-    generate_all_subsequences(seq)
+    main()
 
-    seq.clear()
-    seq.extend(["A", "B", "C"])
-    generate_all_subsequences(seq)
