8e8f7f243f14e64d8c1c9540185f2073b9d9e6a2
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-11 12:15:37 -0500

    Conductor code update based on ticket ID conductor/task/Test_a_Python_file/135_1694452533009

--- c/algorithms/count.c
+++ c/algorithms/count.c
@@ -2,24 +2,30 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+// function to print numbers from 0 to num
 int count(int num) 
 {
     printf("[");
     
+    // Check if num is greater than 20
     if(num > 20)
     {
         int i;
+        // loop from 0 to num
         for(i = 0; i < num; i++)
-            printf("\n%d, \n", i);
+            printf("\n%d, \n", i); // print each number
             
+        // if i is equal to num + 1, print ']'
         if(i == num + 1)
             printf("]");
     } else
     {
         int i;
+        // loop from 0 to num
         for(i = 0; i < num; i++)
-            printf("%d", i);
+            printf("%d", i); // print each number
             
+        // if i is equal to num, print ']'
         if(i == num)
             printf("]");
     }
@@ -28,7 +34,8 @@ int count(int num)
 int main()
 {
     int num;
-    printf("Count:~$ ");
-    scanf("%d", &num);
+    printf("Count:~$ "); // Ask for a number
+    scanf("%d", &num); 
+    // call count function
     count(num);
 }
