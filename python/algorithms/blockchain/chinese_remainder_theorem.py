ba6f70f4f95a0f38bdf18178e1b68af7a9818850
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-11 12:15:35 -0500

    Conductor code update based on ticket ID conductor/task/Test_a_Python_file/135_1694452533009

--- python/algorithms/blockchain/chinese_remainder_theorem.py
+++ python/algorithms/blockchain/chinese_remainder_theorem.py
@@ -12,84 +12,33 @@ Algorithm :
 2. Take n = ra*by + rb*ax
 """
 from __future__ import annotations
+from math import gcd
 
+def chinese_remainder_theorem(l: list[tuple[int, int]]) -> int:
+    N = 1
+    for a, p in l:
+        N *= p
 
-# Extended Euclid
-def extended_euclid(a: int, b: int) -> tuple[int, int]:
-    """
-    >>> extended_euclid(10, 6)
-    (-1, 2)
+    result = 0
+    for a, p in l:
+        m = N // p
+        r, s, d = extended_gcd(p, m)
+        if r != 1:
+            raise ValueError("Input not pairwise co-prime")
+        result += a * s * m
+        result %= N  
+    return result
 
-    >>> extended_euclid(7, 5)
-    (-2, 3)
-
-    """
-    if b == 0:
-        return (1, 0)
-    (x, y) = extended_euclid(b, a % b)
-    k = a // b
-    return (y, x - k * y)
-
-
-# Uses ExtendedEuclid to find inverses
-def chinese_remainder_theorem(n1: int, r1: int, n2: int, r2: int) -> int:
-    """
-    >>> chinese_remainder_theorem(5,1,7,3)
-    31
-
-    Explanation : 31 is the smallest number such that
-                (i)  When we divide it by 5, we get remainder 1
-                (ii) When we divide it by 7, we get remainder 3
-
-    >>> chinese_remainder_theorem(6,1,4,3)
-    14
-
-    """
-    (x, y) = extended_euclid(n1, n2)
-    m = n1 * n2
-    n = r2 * x * n1 + r1 * y * n2
-    return (n % m + m) % m
-
-
-# ----------SAME SOLUTION USING InvertModulo instead ExtendedEuclid----------------
-
-
-# This function find the inverses of a i.e., a^(-1)
-def invert_modulo(a: int, n: int) -> int:
-    """
-    >>> invert_modulo(2, 5)
-    3
-
-    >>> invert_modulo(8,7)
-    1
-
-    """
-    (b, x) = extended_euclid(a, n)
-    if b < 0:
-        b = (b % n + n) % n
-    return b
-
-
-# Same a above using InvertingModulo
-def chinese_remainder_theorem2(n1: int, r1: int, n2: int, r2: int) -> int:
-    """
-    >>> chinese_remainder_theorem2(5,1,7,3)
-    31
-
-    >>> chinese_remainder_theorem2(6,1,4,3)
-    14
-
-    """
-    x, y = invert_modulo(n1, n2), invert_modulo(n2, n1)
-    m = n1 * n2
-    n = r2 * x * n1 + r1 * y * n2
-    return (n % m + m) % m
-
+def extended_gcd(a: int, b: int) -> tuple[int, int, int]:
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, x, y = extended_gcd(b % a, a)
+        return (g, y - (b // a) * x, x)
+        
 
 if __name__ == "__main__":
     from doctest import testmod
 
     testmod(name="chinese_remainder_theorem", verbose=True)
-    testmod(name="chinese_remainder_theorem2", verbose=True)
-    testmod(name="invert_modulo", verbose=True)
-    testmod(name="extended_euclid", verbose=True)
+    testmod(name="extended_gcd", verbose=True)
