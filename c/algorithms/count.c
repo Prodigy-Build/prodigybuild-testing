fa99568c2cdcca86c2e54d37f69821f1a32c9d6a
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-11 10:58:19 -0500

    Conductor code update based on ticket ID conductor/task/Test_a_Python_file/135_1694447894457

--- c/algorithms/count.c
+++ c/algorithms/count.c
@@ -2,33 +2,39 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+// Function to count from 0 up to the provided number
 int count(int num) 
 {
-    printf("[");
+    printf("["); // Starts printing output within brackets
     
-    if(num > 20)
+    if(num > 20) // Controls the format of the output depending on the input number
     {
         int i;
-        for(i = 0; i < num; i++)
+        // Loop to print each number on a new line when input number is greater than 20
+        for(i = 0; i < num; i++) 
             printf("\n%d, \n", i);
             
+        // Closes the bracket when loop has completed
         if(i == num + 1)
             printf("]");
     } else
     {
         int i;
+        // Loop to print each number in line when the input number is 20 or less
         for(i = 0; i < num; i++)
             printf("%d", i);
             
+        // Closes the bracket when loop has completed
         if(i == num)
             printf("]");
     }
 }
 
+// Main function that asks for input from user
 int main()
 {
     int num;
-    printf("Count:~$ ");
-    scanf("%d", &num);
-    count(num);
+    printf("Count:~$ "); // Prompt user to input number
+    scanf("%d", &num); // Scans the input number
+    count(num); // Executes the count function with the input number
 }
