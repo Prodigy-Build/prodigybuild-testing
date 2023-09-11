7b28c8968e4855ea6f4c45c0d04da48dab1cb8f1
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-11 10:06:39 -0500

    Conductor code update based on ticket ID conductor/task/Test_a_Python_file/135_1694444794670

--- c/algorithms/count.c
+++ c/algorithms/count.c
@@ -2,24 +2,31 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+// Function to count from 0 up to a given number
 int count(int num) 
 {
     printf("[");
     
+    // If number is greater than 20, print each number on a new line
     if(num > 20)
     {
         int i;
+        // Loop from 0 to number-1
         for(i = 0; i < num; i++)
             printf("\n%d, \n", i);
             
+        // Print closing bracket at the end of count
         if(i == num + 1)
             printf("]");
-    } else
+    } 
+    else
     {
         int i;
+        // If number is 20 or less, print the numbers inline
         for(i = 0; i < num; i++)
             printf("%d", i);
             
+        // Print closing bracket at the end of count
         if(i == num)
             printf("]");
     }
@@ -29,6 +36,8 @@ int main()
 {
     int num;
     printf("Count:~$ ");
+    // Get the number to count up to from user
     scanf("%d", &num);
+    // Call count function
     count(num);
 }
