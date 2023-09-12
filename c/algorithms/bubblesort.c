d39968f75ff4f9a7c24d3259fbefa41db9542b1a
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-09-12 17:49:02 +0530

    Conductor code update based on ticket ID conductor/task/Test_AI/55_1694521139959

--- c/algorithms/bubblesort.c
+++ c/algorithms/bubblesort.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <assert.h>
 
 void bubble_sort(int *array, int len) 
 {
@@ -13,15 +14,35 @@ void bubble_sort(int *array, int len)
     }
 }
 
+void unit_test(void)
+{
+    int a[6] = {2, 7, 4, 1, 5, 3};
+    int sorted_a[6] = {1, 2, 3, 4, 5, 7};
+    bubble_sort(a, 6);
+    for (int i = 0; i < 6; i++)
+        assert(a[i] == sorted_a[i]);
+
+    int b[6] = {9, 8, 7, 6, 5, 2};
+    int sorted_b[6] = {2, 5, 6, 7, 8, 9};
+    bubble_sort(b, 6);
+    for (int i = 0; i < 6; i++)
+        assert(b[i] == sorted_b[i]);
+
+    printf("All test cases passed.\n");
+}
+
 int main(void) 
 {
     int yarr[] = {3, 9, 4, 8, 7, 6, 1, 2, 0, 10};
-    
+
     bubble_sort(yarr, 10);
 
     printf("[");
     for (int i = 0; i < 10; i++)
         printf("%d, ", yarr[i]);	
     printf("\b\b]\n");
+
+    unit_test();
+
     return 0;
 }
