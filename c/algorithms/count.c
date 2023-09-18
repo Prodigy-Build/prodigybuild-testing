527f233884fde21228a8235ab1c7718411f5d923
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-09-18 16:19:18 +0530

    Conductor code update based on ticket ID conductor/subtask/Issue_7/74_1695034156159

--- c/algorithms/count.c
+++ c/algorithms/count.c
@@ -2,33 +2,28 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-int count(int num) 
+void count(int num) 
 {
     printf("[");
-    
-    if(num > 20)
+
+    int i;
+    for(i = 0; i < num; i++)
     {
-        int i;
-        for(i = 0; i < num; i++)
-            printf("\n%d, \n", i);
-            
-        if(i == num + 1)
-            printf("]");
-    } else
-    {
-        int i;
-        for(i = 0; i < num; i++)
-            printf("%d", i);
-            
-        if(i == num)
-            printf("]");
+        printf("%d", i);
+        
+        if(i < num - 1)
+            printf(", ");
     }
+            
+    printf("]\n");
 }
 
 int main()
 {
     int num;
-    printf("Count:~$ ");
+    printf("Enter a number: ");
     scanf("%d", &num);
     count(num);
+
+    return 0;
 }
