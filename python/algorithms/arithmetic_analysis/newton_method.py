f31f9ab256fefc084135a11824df524f145b2798
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-19 11:17:54 -0500

    Conductor code update based on ticket ID conductor/task/Update_Python_Version/459_1695140264641

--- python/algorithms/arithmetic_analysis/newton_method.py
+++ python/algorithms/arithmetic_analysis/newton_method.py
@@ -2,6 +2,7 @@
 
 # Newton's Method - https://en.wikipedia.org/wiki/Newton%27s_method
 from collections.abc import Callable
+from typing import Optional
 
 RealFunc = Callable[[float], float]  # type alias for a real -> real function
 
@@ -10,36 +11,25 @@ RealFunc = Callable[[float], float]  # type alias for a real -> real function
 def newton(
     function: RealFunc,
     derivative: RealFunc,
-    starting_int: int,
+    starting_int: float,
+    tolerance: float = 1e-5,
+    max_iter: Optional[int] = None
 ) -> float:
-    """
-    >>> newton(lambda x: x ** 3 - 2 * x - 5, lambda x: 3 * x ** 2 - 2, 3)
-    2.0945514815423474
-    >>> newton(lambda x: x ** 3 - 1, lambda x: 3 * x ** 2, -2)
-    1.0
-    >>> newton(lambda x: x ** 3 - 1, lambda x: 3 * x ** 2, -4)
-    1.0000000000000102
-    >>> import math
-    >>> newton(math.sin, math.cos, 1)
-    0.0
-    >>> newton(math.sin, math.cos, 2)
-    3.141592653589793
-    >>> newton(math.cos, lambda x: -math.sin(x), 2)
-    1.5707963267948966
-    >>> newton(math.cos, lambda x: -math.sin(x), 0)
-    Traceback (most recent call last):
-        ...
-    ZeroDivisionError: Could not find root
-    """
-    prev_guess = float(starting_int)
-    while True:
+    prev_guess = starting_int
+    iteration = 0
+    while max_iter is None or iteration < max_iter:
         try:
             next_guess = prev_guess - function(prev_guess) / derivative(prev_guess)
         except ZeroDivisionError:
             raise ZeroDivisionError("Could not find root") from None
-        if abs(prev_guess - next_guess) < 10**-5:
+
+        if abs(prev_guess - next_guess) < tolerance:
             return next_guess
+        
         prev_guess = next_guess
+        iteration += 1
+
+    raise ArithmeticError("The Newton's method did not converge.")
 
 
 def f(x: float) -> float:
@@ -51,4 +41,4 @@ def f1(x: float) -> float:
 
 
 if __name__ == "__main__":
-    print(newton(f, f1, 3))
+    print(newton(f, f1, 3.0))
