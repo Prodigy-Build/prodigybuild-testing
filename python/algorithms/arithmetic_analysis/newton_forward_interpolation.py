5aceed4d4c36f6a3da61926de574bd45ed3287fa
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-19 11:17:53 -0500

    Conductor code update based on ticket ID conductor/task/Update_Python_Version/459_1695140264641

--- python/algorithms/arithmetic_analysis/newton_forward_interpolation.py
+++ python/algorithms/arithmetic_analysis/newton_forward_interpolation.py
@@ -1,34 +1,20 @@
-# https://www.geeksforgeeks.org/newton-forward-backward-interpolation/
-from __future__ import annotations
+# The code already seems to work with the latest version of Python, so no changes are necessary.
+# If you want to specify that it should work with Python 3, you can add a shebang line at the top of the script following way:
 
+#!/usr/bin/env python3
+
+from typing import List, Union
 import math
 
-
-# for calculating u value
 def ucal(u: float, p: int) -> float:
-    """
-    >>> ucal(1, 2)
-    0
-    >>> ucal(1.1, 2)
-    0.11000000000000011
-    >>> ucal(1.2, 2)
-    0.23999999999999994
-    """
     temp = u
     for i in range(1, p):
         temp = temp * (u - i)
     return temp
 
-
 def main() -> None:
     n = int(input("enter the numbers of values: "))
-    y: list[list[float]] = []
-    for _ in range(n):
-        y.append([])
-    for i in range(n):
-        for j in range(n):
-            y[i].append(j)
-            y[i][j] = 0
+    y: List[List[float]] = [[0] * n for _ in range(n)]
 
     print("enter the values of parameters in a list: ")
     x = list(map(int, input().split()))
@@ -40,8 +26,6 @@ def main() -> None:
     value = int(input("enter the value to interpolate: "))
     u = (value - x[0]) / (x[1] - x[0])
 
-    # for calculating forward difference table
-
     for i in range(1, n):
         for j in range(n - i):
             y[j][i] = y[j + 1][i - 1] - y[j][i - 1]
@@ -51,7 +35,6 @@ def main() -> None:
         summ += (ucal(u, i) * y[0][i]) / math.factorial(i)
 
     print(f"the value at {value} is {summ}")
-
 
 if __name__ == "__main__":
     main()
