edf4972ff3dd49a910daabe270a9df8e80c7248a
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-20 14:33:13 -0500

    Conductor code update based on ticket ID conductor/task/Conductor_Issue_1_file/461_1695238392420

--- c/algorithms/count.c
+++ c/algorithms/count.c
@@ -1,14 +1,25 @@
+/* File c/algorithms/count.c */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 
+/*
+Function to count from 0 to the provided number and print the count
+@param num: integer number to count to
+*/
 int count(int num) 
 {
+    /* Printing opening square bracket */
     printf("[");
     
+    /* Declaring loop variable */
     int i;
+    
+    /* Looping through the num */
     for(i = 0; i < num; i++)
     {
+        /* Formatting the print statements based on the input size */
         if(num > 20)
         {
             printf("\n%d, \n", i);
@@ -18,6 +29,7 @@ int count(int num)
             printf("%d", i);
         }
         
+        /* Print closing bracket at the end of count */
         if(i == num - 1)
         {
             printf("]");
@@ -27,8 +39,14 @@ int count(int num)
 
 int main()
 {
+    /* Declaring and reading the input number*/
     int num;
     printf("Count:~$ ");
     scanf("%d", &num);
+
+    /* Calling count function with the read input */
     count(num);
+
+    /* Indicating successful program completion */
+    return 0;
 }
