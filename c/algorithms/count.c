8cec815e68b7fd59349d732c5540dd98b6a54bc7
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-20 14:18:37 -0500

    Conductor code update based on ticket ID conductor/task/Conductor_Issue_1_file/461_1695237515300

--- c/algorithms/count.c
+++ c/algorithms/count.c
@@ -1,34 +1,56 @@
+/* Includes standard input/output library functions */
 #include <stdio.h>
+/* Includes standard library functions */
 #include <stdlib.h>
+/* Includes character handling library functions */
 #include <ctype.h>
 
+/* Function to count numbers from 0 to the specified input */
 int count(int num) 
 {
+    /* Print opening bracket for the output */
     printf("[");
-    
+
+    /* Loop to iterate from 0 to the input number */
     int i;
     for(i = 0; i < num; i++)
     {
+        /* Print number with newline if input is greater than 20 */
         if(num > 20)
         {
-            printf("\n%d, \n", i);
+            printf("\n%d, ", i);
         }
+        /* Print number without newline if input is less than or equal to 20 */
         else 
         {
-            printf("%d", i);
+            printf("%d, ", i);
         }
         
+        /* Print closing bracket at the end of numbers */
         if(i == num - 1)
         {
-            printf("]");
+            printf("\b\b]"); /* Backspace to remove trailing comma */
         }
     }
+
+    return 0;
 }
 
+/* Entry point of the program */
 int main()
 {
+    /* Variable to hold the input number */
     int num;
-    printf("Count:~$ ");
+
+    /* Output to ask user for input */
+    printf("Enter a number: ");
+
+    /* Get user input */
     scanf("%d", &num);
+
+    /* Call count function with user input */
     count(num);
+
+    return 0;
 }
+
