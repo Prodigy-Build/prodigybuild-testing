856e8cf2747200e120396af12024df9a894d38b9
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-09-21 03:55:35 +0530

    Conductor code update based on ticket ID conductor/task/Github_test/138_1695248730103

--- ruby/sample_app/app/controllers/microposts_controller.rb
+++ ruby/sample_app/app/controllers/microposts_controller.rb
@@ -1,31 +1,35 @@
-class MicropostsController < ApplicationController
-  before_action :logged_in_user, only: [:create, :destroy]
-  before_action :correct_user,   only: :destroy
+# Adding unit tests to "ruby/sample_app/test/controllers/microposts_controller_test.rb"
 
-  def create
-    @micropost = current_user.microposts.build(micropost_params)
-    if @micropost.save
-      flash[:success] = "Micropost created!"
-      redirect_to root_url
-    else
-      @feed_items = []
-      render 'static_pages/home'
-    end
+```ruby
+require 'test_helper'
+
+class MicropostsControllerTest < ActionDispatch::IntegrationTest
+  
+def setup
+    @micropost = microposts(:orange)
   end
 
-  def destroy
-    @micropost.destroy
-    flash[:success] = "Micropost deleted"
-    redirect_to request.referrer || root_url
+  test "should redirect create when not logged in" do
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, params: { micropost: { content: "Lorem ipsum" } }
+    end
+    assert_redirected_to login_url
   end
 
-  private
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(@micropost)
+    end
+    assert_redirected_to login_url
+  end
 
-    def micropost_params
-      params.require(:micropost).permit(:content, :picture)
+  test "should redirect destroy for wrong micropost" do
+    log_in_as(users(:michael))
+    micropost = microposts(:ants)
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(micropost)
     end
-    def correct_user
-      @micropost = current_user.microposts.find_by(id: params[:id])
-      redirect_to root_url if @micropost.nil?
-    end
+    assert_redirected_to root_url
+  end
 end
+```
