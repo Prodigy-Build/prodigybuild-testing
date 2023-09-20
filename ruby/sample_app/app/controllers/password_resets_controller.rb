18cd94ff81646e03d59d17b77b32ca69197cf76b
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-09-21 03:55:36 +0530

    Conductor code update based on ticket ID conductor/task/Github_test/138_1695248730103

--- ruby/sample_app/app/controllers/password_resets_controller.rb
+++ ruby/sample_app/app/controllers/password_resets_controller.rb
@@ -1,65 +1,59 @@
-class PasswordResetsController < ApplicationController
-  before_action :get_user,         only: [:edit, :update]
-  before_action :valid_user,       only: [:edit, :update]
-  before_action :check_expiration, only: [:edit, :update]    # Case (1)
+require 'test_helper'
 
-  def new
+class PasswordResetsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+  
+  test "should get new" do
+    get new_password_reset_path
+    assert_response :success
   end
 
-  def create
-    @user = User.find_by(email: params[:password_reset][:email].downcase)
-    if @user
-      @user.create_reset_digest
-      @user.send_password_reset_email
-      flash[:info] = "Email sent with password reset instructions"
-      redirect_to root_url
-    else
-      flash.now[:danger] = "Email address not found"
-      render 'new'
-    end
+  test "should get new, with non-existing email" do
+    post password_resets_path, params: { password_reset: { email: "" } }
+    assert_not flash.empty?
+    assert_redirected_to new_password_reset_url
+  end
+  
+  test "should get valid user with active account" do
+    post password_resets_path, params: { password_reset: { email: @user.email } }
+    assert_not_equal @user.reset_digest, @user.reload.reset_digest
+    assert_not flash.empty?
+    assert_redirected_to root_url
   end
 
-  def edit
+  test "get edit password reset form" do
+    get edit_password_reset_path(@user.reset_token, email: "")
+    assert_redirected_to root_url
+    
+    user.toggle!(:activated)
+    get edit_password_reset_path(@user.reset_token, email: @user.email)
+    assert_redirected_to root_url
+    
+    user.toggle!(:activated)
+    get edit_password_reset_path('wrong token', email: @user.email)
+    assert_redirected_to root_url
+    
+    get edit_password_reset_path(@user.reset_token, email: @user.email)
+    assert_template 'password_resets/edit'
+    assert_select "input[name=email][type=hidden][value=?]", user.email
   end
 
-  def update
-    if params[:user][:password].empty?                  # Case (3)
-      @user.errors.add(:password, :blank)
-      render 'edit'
-    elsif @user.update_attributes(user_params)          # Case (4)
-      log_in @user
-      flash[:success] = "Password has been reset."
-      redirect_to @user
-    else
-      render 'edit'                                     # Case (2)
-    end
+  test "invalid password & confirmation input" do
+    patch password_reset_path(@user.reset_token), params: { email: @user.email, user: { password:              "foobaz", password_confirmation: "barquux" } }
+    assert_select 'div#error_explanation'
   end
 
-  private
-
-    def user_params
-      params.require(:user).permit(:password, :password_confirmation)
-    end
-
-    # Before filters
-
-    def get_user
-      @user = User.find_by(email: params[:email])
-    end
-
-    # Confirms a valid user.
-    def valid_user
-      unless (@user && @user.activated? &&
-              @user.authenticated?(:reset, params[:id]))
-        redirect_to root_url
-      end
-    end
-
-    # Checks expiration of reset token.
-    def check_expiration
-      if @user.password_reset_expired?
-        flash[:danger] = "Password reset has expired."
-        redirect_to new_password_reset_url
-      end
-    end
+  test "empty password input" do
+    patch password_reset_path(@user.reset_token), params: { email: @user.email, user: { password:              "", password_confirmation: "" } }
+    assert_select 'div#error_explanation'
+  end
+  
+  test "valid password & confirmation" do
+    patch password_reset_path(@user.reset_token), params: { email: @user.email, user: { password:              "foobaz", password_confirmation: "foobaz" } }
+    assert is_logged_in?
+    assert_not flash.empty?
+    assert_redirected_to @user
+  end
 end
