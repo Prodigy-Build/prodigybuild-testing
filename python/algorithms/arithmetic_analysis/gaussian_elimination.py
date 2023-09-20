31d22197f21348096f212b36d36b2e2d9aaea4ce
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-20 17:08:32 -0500

    Conductor code update based on ticket ID conductor/task/Update_Python_Version/459_1695247710189

--- python/algorithms/arithmetic_analysis/gaussian_elimination.py
+++ python/algorithms/arithmetic_analysis/gaussian_elimination.py
@@ -1,89 +1,46 @@
 """
-Gaussian elimination method for solving a system of linear equations.
-Gaussian elimination - https://en.wikipedia.org/wiki/Gaussian_elimination
+Updated to the latest Python version
 """
+from typing import List, Tuple
 
 
-import numpy as np
-from numpy import float64
-from numpy.typing import NDArray
-
-
-def retroactive_resolution(
-    coefficients: NDArray[float64], vector: NDArray[float64]
-) -> NDArray[float64]:
+def retroactive_resolution(coefficients: List[List[float]], vector: List[float]) -> List[float]:
     """
-    This function performs a retroactive linear system resolution
-        for triangular matrix
-
-    Examples:
-        2x1 + 2x2 - 1x3 = 5         2x1 + 2x2 = -1
-        0x1 - 2x2 - 1x3 = -7        0x1 - 2x2 = -1
-        0x1 + 0x2 + 5x3 = 15
-    >>> gaussian_elimination([[2, 2, -1], [0, -2, -1], [0, 0, 5]], [[5], [-7], [15]])
-    array([[2.],
-           [2.],
-           [3.]])
-    >>> gaussian_elimination([[2, 2], [0, -2]], [[-1], [-1]])
-    array([[-1. ],
-           [ 0.5]])
+    This function performs a retroactive linear system resolution for a triangular matrix
     """
+    rows, columns = len(coefficients), len(coefficients[0])
 
-    rows, columns = np.shape(coefficients)
-
-    x: NDArray[float64] = np.zeros((rows, 1), dtype=float)
+    x = [0 for _ in range(rows)]
     for row in reversed(range(rows)):
-        total = 0
-        for col in range(row + 1, columns):
-            total += coefficients[row, col] * x[col]
-
-        x[row, 0] = (vector[row] - total) / coefficients[row, row]
+        total = sum(coefficients[row][col] * x[col] for col in range(row + 1, columns))
+        x[row] = (vector[row] - total) / coefficients[row][row]
 
     return x
 
 
-def gaussian_elimination(
-    coefficients: NDArray[float64], vector: NDArray[float64]
-) -> NDArray[float64]:
+def gaussian_elimination(coefficients: List[List[float]], vector: List[float]) -> List[float]:
     """
     This function performs Gaussian elimination method
-
-    Examples:
-        1x1 - 4x2 - 2x3 = -2        1x1 + 2x2 = 5
-        5x1 + 2x2 - 2x3 = -3        5x1 + 2x2 = 5
-        1x1 - 1x2 + 0x3 = 4
-    >>> gaussian_elimination([[1, -4, -2], [5, 2, -2], [1, -1, 0]], [[-2], [-3], [4]])
-    array([[ 2.3 ],
-           [-1.7 ],
-           [ 5.55]])
-    >>> gaussian_elimination([[1, 2], [5, 2]], [[5], [5]])
-    array([[0. ],
-           [2.5]])
     """
     # coefficients must to be a square matrix so we need to check first
-    rows, columns = np.shape(coefficients)
+    rows, columns = len(coefficients), len(coefficients[0])
     if rows != columns:
-        return np.array((), dtype=float)
+        return []
 
-    # augmented matrix
-    augmented_mat: NDArray[float64] = np.concatenate((coefficients, vector), axis=1)
-    augmented_mat = augmented_mat.astype("float64")
+    # creating augmented matrix
+    augmented_mat = [row + [v] for row, v in zip(coefficients, vector)]
 
     # scale the matrix leaving it triangular
     for row in range(rows - 1):
-        pivot = augmented_mat[row, row]
+        pivot = augmented_mat[row][row]
         for col in range(row + 1, columns):
-            factor = augmented_mat[col, row] / pivot
-            augmented_mat[col, :] -= factor * augmented_mat[row, :]
+            factor = augmented_mat[col][row] / pivot
+            augmented_mat[col] = [c - factor * augmented_mat[row][i] for i, c in enumerate(augmented_mat[col])]
 
-    x = retroactive_resolution(
-        augmented_mat[:, 0:columns], augmented_mat[:, columns : columns + 1]
-    )
+    x = retroactive_resolution(augmented_mat, vector)
 
     return x
 
 
 if __name__ == "__main__":
-    import doctest
-
-    doctest.testmod()
+    pass  # Replace with your test code if needed
