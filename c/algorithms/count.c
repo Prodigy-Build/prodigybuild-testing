6eae8feb52ddce7652775c5eadf2a026105faa43
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-20 12:51:23 -0500

    Conductor code update based on ticket ID conductor/task/Conductor_Issue/461_1695232281492

--- c/algorithms/count.c
+++ c/algorithms/count.c
@@ -1,34 +1,50 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
+/* Updated file: c/algorithms/count.c */
 
+#include <stdio.h> /* Standard Input/Output library */
+#include <stdlib.h> /* Standard library */
+#include <ctype.h> /* Character type functions */
+
+/* Function to count from zero up to a given number */
 int count(int num) 
 {
-    printf("[");
+    printf("["); /* Start of the range of counting */
     
+    /* Iteration from 0 to num */
     int i;
     for(i = 0; i < num; i++)
     {
+        /* Formatting the outputs based on the length of the range to be displayed */
         if(num > 20)
         {
-            printf("\n%d, \n", i);
+            printf("\n%d, \n", i); /* Newlines for long range */
         }
         else 
         {
-            printf("%d", i);
+            printf("%d", i); /* Stays on the same line for short range */
         }
         
+        /* Checking if reached the last index to close the range */
         if(i == num - 1)
         {
-            printf("]");
+            printf("]"); /* End of the range */
         }
     }
+
+    return 0; /* Successful execution of the counting function */
 }
 
+/* The main function of the program */
 int main()
 {
+    /* User input for the range */
     int num;
     printf("Count:~$ ");
     scanf("%d", &num);
+
+    /* Calling the counting function */
     count(num);
+
+    return 0; /* Successful execution of the program */
 }
+
+/* End of the file */
