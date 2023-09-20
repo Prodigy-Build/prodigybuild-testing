5cf195f0aab99aa9dd3ee01023507881cfa5bc45
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-20 02:49:56 -0500

    Conductor code update based on ticket ID conductor/task/Update_Python_Version/459_1695196184366

--- python/algorithms/arithmetic_analysis/lu_decomposition.py
+++ python/algorithms/arithmetic_analysis/lu_decomposition.py
@@ -15,70 +15,16 @@ matrix and raise an error if no such decomposition exists.
 
 Reference: https://en.wikipedia.org/wiki/LU_decomposition
 """
-from __future__ import annotations
-
 import numpy as np
+from typing import Tuple
 
-
-def lower_upper_decomposition(table: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+def lower_upper_decomposition(table: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     """
     Perform LU decomposition on a given matrix and raises an error if the matrix
     isn't square or if no such decomposition exists
-    >>> matrix = np.array([[2, -2, 1], [0, 1, 2], [5, 3, 1]])
-    >>> lower_mat, upper_mat = lower_upper_decomposition(matrix)
-    >>> lower_mat
-    array([[1. , 0. , 0. ],
-           [0. , 1. , 0. ],
-           [2.5, 8. , 1. ]])
-    >>> upper_mat
-    array([[  2. ,  -2. ,   1. ],
-           [  0. ,   1. ,   2. ],
-           [  0. ,   0. , -17.5]])
-
-    >>> matrix = np.array([[4, 3], [6, 3]])
-    >>> lower_mat, upper_mat = lower_upper_decomposition(matrix)
-    >>> lower_mat
-    array([[1. , 0. ],
-           [1.5, 1. ]])
-    >>> upper_mat
-    array([[ 4. ,  3. ],
-           [ 0. , -1.5]])
-
-    # Matrix is not square
-    >>> matrix = np.array([[2, -2, 1], [0, 1, 2]])
-    >>> lower_mat, upper_mat = lower_upper_decomposition(matrix)
-    Traceback (most recent call last):
-        ...
-    ValueError: 'table' has to be of square shaped array but got a 2x3 array:
-    [[ 2 -2  1]
-     [ 0  1  2]]
-
-    # Matrix is invertible, but its first leading principal minor is 0
-    >>> matrix = np.array([[0, 1], [1, 0]])
-    >>> lower_mat, upper_mat = lower_upper_decomposition(matrix)
-    Traceback (most recent call last):
-    ...
-    ArithmeticError: No LU decomposition exists
-
-    # Matrix is singular, but its first leading principal minor is 1
-    >>> matrix = np.array([[1, 0], [1, 0]])
-    >>> lower_mat, upper_mat = lower_upper_decomposition(matrix)
-    >>> lower_mat
-    array([[1., 0.],
-           [1., 1.]])
-    >>> upper_mat
-    array([[1., 0.],
-           [0., 0.]])
-
-    # Matrix is singular, but its first leading principal minor is 0
-    >>> matrix = np.array([[0, 1], [0, 1]])
-    >>> lower_mat, upper_mat = lower_upper_decomposition(matrix)
-    Traceback (most recent call last):
-    ...
-    ArithmeticError: No LU decomposition exists
     """
     # Ensure that table is a square array
-    rows, columns = np.shape(table)
+    rows, columns = table.shape
     if rows != columns:
         msg = (
             "'table' has to be of square shaped array but got a "
@@ -88,6 +34,7 @@ def lower_upper_decomposition(table: np.ndarray) -> tuple[np.ndarray, np.ndarray
 
     lower = np.zeros((rows, columns))
     upper = np.zeros((rows, columns))
+    # Calculate lower and upper triangular matrices
     for i in range(columns):
         for j in range(i):
             total = sum(lower[i][k] * upper[k][j] for k in range(j))
