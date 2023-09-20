dcbed104968ba4b058b63a7667aa576b148f2e80
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-20 14:46:23 -0500

    Conductor code update based on ticket ID conductor/task/Conductor_Issue_1_file/461_1695239181995

--- c/algorithms/count.c
+++ c/algorithms/count.c
@@ -1,23 +1,31 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
+/* updated c/algorithms/count.c file */
 
+#include <stdio.h>   /* needed for console I/O */
+#include <stdlib.h>  /* standard library */
+#include <ctype.h>   /* includes tolower(), toaggle() etc. */
+
+// function definition of count
 int count(int num) 
 {
     printf("[");
     
     int i;
+    
+    // using for loop to count till number passed in argument
     for(i = 0; i < num; i++)
     {
+        // if number is greater than 20, then print in newline
         if(num > 20)
         {
             printf("\n%d, \n", i);
         }
+        // for numbers less than or equal to 20, print in same line
         else 
         {
             printf("%d", i);
         }
         
+        // when it's last iteration print closing bracket
         if(i == num - 1)
         {
             printf("]");
@@ -25,10 +33,14 @@ int count(int num)
     }
 }
 
+// main function
 int main()
 {
     int num;
     printf("Count:~$ ");
+    // taking number input
     scanf("%d", &num);
+    // invoking count function
     count(num);
 }
+
