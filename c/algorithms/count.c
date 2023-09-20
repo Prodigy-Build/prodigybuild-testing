495538607456e5f9fcd3a67bc2e1fd8542e9fcf0
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-20 14:53:30 -0500

    Conductor code update based on ticket ID conductor/task/Conductor_Issue_1_file/461_1695239609195

--- c/algorithms/count.c
+++ c/algorithms/count.c
@@ -1,34 +1,43 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
+#include <stdio.h> /* Standard I/O functions */
+#include <stdlib.h> /* Standard library functions */
+#include <ctype.h> /* Character type functions */
 
+/*
+* Function to count from 0 to num - 1 and print
+* the result in a format
+* Input: num - the number to count up to
+*/
 int count(int num) 
 {
-    printf("[");
+    printf("["); /* Starting point */
     
-    int i;
+    int i; /* Counter */
     for(i = 0; i < num; i++)
     {
-        if(num > 20)
+        if(num > 20) /* Handling for limit > 20 */
         {
-            printf("\n%d, \n", i);
+            printf("\n%d, \n", i); /* Print number and comma in new line */
         }
         else 
         {
-            printf("%d", i);
+            printf("%d", i); /* Otherwise just print number */
         }
         
-        if(i == num - 1)
+        if(i == num - 1) /* If last number */
         {
-            printf("]");
+            printf("]"); /* End bracket for last number */
         }
     }
 }
 
+/*
+* Main function to take user input and initiate count
+*/
 int main()
 {
-    int num;
-    printf("Count:~$ ");
-    scanf("%d", &num);
-    count(num);
+    int num; /* User input number */
+    printf("Count:~$ "); /* Prompt for input */
+    scanf("%d", &num); /* Read user input */
+    count(num); /* Initiate counting */
 }
+
