928a73c89494f4550c0a7c005b8c510ec84f7535
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-20 14:24:17 -0500

    Conductor code update based on ticket ID conductor/task/Conductor_Issue_1_file/461_1695237855749

--- c/algorithms/count.c
+++ c/algorithms/count.c
@@ -1,23 +1,34 @@
+/* File: c/algorithms/count.c */
+
+/* Header files: stdio for input-output functions,
+   stdlib for general utility functions,
+   ctype for character test functions */
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 
+/* Function to count from 0 to num and print the numbers */
 int count(int num) 
 {
+    /* Start of the number array */
     printf("[");
-    
+
+    /* Loop for counting */
     int i;
     for(i = 0; i < num; i++)
     {
+        /* If num is more than 20, print each number on new line */
         if(num > 20)
         {
             printf("\n%d, \n", i);
         }
+        /* If num is 20 or less, print numbers on same line */
         else 
         {
             printf("%d", i);
         }
-        
+
+        /* Add closing bracket at the end of array */
         if(i == num - 1)
         {
             printf("]");
@@ -25,10 +36,17 @@ int count(int num)
     }
 }
 
+/* Main function that accepts user input */
 int main()
 {
+    /* Variable declaration */
     int num;
+
+    /* Prompt user to enter a number */
     printf("Count:~$ ");
     scanf("%d", &num);
+
+    /* Call the count function */
     count(num);
 }
+
