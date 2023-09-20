41ba3c5603bb1026fd167b02285d1a4cda9e17dc
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-20 13:14:49 -0500

    Conductor code update based on ticket ID conductor/task/Refactor_a_file/460_1695233687611

--- c/algorithms/count.c
+++ c/algorithms/count.c
@@ -1,34 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <stdbool.h>
 
-int count(int num) 
+#define MAX_VALUE 20
+
+void count(int num) 
 {
-    printf("[");
-    
     int i;
+    bool is_print_new_line = num > MAX_VALUE;
+    
+    printf("[");
+
     for(i = 0; i < num; i++)
     {
-        if(num > 20)
+        if(is_print_new_line)
         {
-            printf("\n%d, \n", i);
+            printf("\n%d, ", i);
         }
         else 
         {
             printf("%d", i);
-        }
-        
-        if(i == num - 1)
-        {
-            printf("]");
+            if(i != num - 1)
+            {
+                printf(", ");
+            }
         }
     }
+
+    printf("]\n");
 }
 
 int main()
 {
     int num;
+    
     printf("Count:~$ ");
     scanf("%d", &num);
+    
     count(num);
+
+    return 0;
 }
