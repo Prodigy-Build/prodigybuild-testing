b87256f0247c607f943a992d109c7a0ab6500272
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-09-21 03:55:40 +0530

    Conductor code update based on ticket ID conductor/task/Github_test/138_1695248730103

--- ruby/sample_app/app/controllers/static_pages_controller.rb
+++ ruby/sample_app/app/controllers/static_pages_controller.rb
@@ -1,18 +1,35 @@
-class StaticPagesController < ApplicationController
+```ruby
+require 'test_helper'
 
-  def home
-    if logged_in?
-      @micropost  = current_user.microposts.build
-      @feed_items = current_user.feed.paginate(page: params[:page])
-    end
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Ruby on Rails Sample App"
   end
 
-  def help
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
   
-  def about
+  test "should get help" do
+    get static_pages_help_url
+    assert_response :success
+    assert_select "title", "Help | #{@base_title}"
   end
 
-  def contact
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 end
+```
+This is only an example of how you might write these tests. Actual code can vary depending on your actual requirements and setup.
