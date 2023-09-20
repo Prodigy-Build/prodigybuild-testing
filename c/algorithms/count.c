d5605c137040b3472e0138a27b46336cd80bcc54
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-20 16:07:57 -0500

    Conductor code update based on ticket ID conductor/task/Conductor_Issue_1_file/461_1695244075450

--- c/algorithms/count.c
+++ c/algorithms/count.c
@@ -1,23 +1,31 @@
+/* File: c/algorithms/count.c */
+
+/* Including necessary libraries */
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 
+/* 
+Function to print numbers from 0 to num 
+For numbers greater than 20, it prints each number in a new line
+*/
 int count(int num) 
 {
-    printf("[");
-    
+    printf("["); // Start of array notation
+
     int i;
-    for(i = 0; i < num; i++)
+    for(i = 0; i < num; i++) // Loop through 0 to num
     {
         if(num > 20)
         {
-            printf("\n%d, \n", i);
+            printf("\n%d, \n", i); // Print each number in new line if num > 20
         }
         else 
         {
-            printf("%d", i);
+            printf("%d", i); // Print in same line if num <= 20
         }
-        
+
+        /* Print the closing bracket for last number */
         if(i == num - 1)
         {
             printf("]");
@@ -25,10 +33,14 @@ int count(int num)
     }
 }
 
+/* Main function */
 int main()
 {
     int num;
-    printf("Count:~$ ");
-    scanf("%d", &num);
-    count(num);
+    printf("Count:~$ "); // Prompt to enter the number
+    scanf("%d", &num); // Reading the number
+    count(num); // Calling the count function
 }
+
+/* End of File */
+
