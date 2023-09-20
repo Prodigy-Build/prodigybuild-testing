b2ea04d16921a62e3f5af5f2e16ca20de9d697c8
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-20 14:09:04 -0500

    Conductor code update based on ticket ID conductor/task/Update_Python_Version/459_1695236930600

--- python/algorithms/arithmetic_analysis/secant_method.py
+++ python/algorithms/arithmetic_analysis/secant_method.py
@@ -21,7 +21,7 @@ def secant_method(lower_bound: float, upper_bound: float, repeats: int) -> float
     x0 = lower_bound
     x1 = upper_bound
     for _ in range(0, repeats):
-        x0, x1 = x1, x1 - (f(x1) * (x1 - x0)) / (f(x1) - f(x0))
+        x0, x1 = x1, x1 - (f(x1) * (x1 - x0)) / (f(x1) - f(x0)) if f(x1) != f(x0) else (x0, x1)
     return x1
 
 
