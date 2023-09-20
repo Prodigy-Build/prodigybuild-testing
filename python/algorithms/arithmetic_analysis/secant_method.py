4cbd3e70ef93cce42c6ef7a9f36281a3ca4d3711
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-20 17:08:43 -0500

    Conductor code update based on ticket ID conductor/task/Update_Python_Version/459_1695247710189

--- python/algorithms/arithmetic_analysis/secant_method.py
+++ python/algorithms/arithmetic_analysis/secant_method.py
@@ -3,7 +3,7 @@ Implementing Secant method in Python
 Author: dimgrichr
 """
 from math import exp
-
+from typing import Tuple
 
 def f(x: float) -> float:
     """
@@ -13,17 +13,16 @@ def f(x: float) -> float:
     return 8 * x - 2 * exp(-x)
 
 
-def secant_method(lower_bound: float, upper_bound: float, repeats: int) -> float:
+def secant_method(bounds: Tuple[float, float], repeats: int) -> float:
     """
-    >>> secant_method(1, 3, 2)
+    >>> secant_method((1, 3), 2)
     0.2139409276214589
     """
-    x0 = lower_bound
-    x1 = upper_bound
-    for _ in range(0, repeats):
+    x0, x1 = bounds
+    for _ in range(repeats):
         x0, x1 = x1, x1 - (f(x1) * (x1 - x0)) / (f(x1) - f(x0))
     return x1
 
 
 if __name__ == "__main__":
-    print(f"Example: {secant_method(1, 3, 2)}")
+    print(f"Example: {secant_method((1, 3), 2)}")
