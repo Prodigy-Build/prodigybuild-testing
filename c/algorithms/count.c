d4366609d446b15cd532fe6bd5177b7295658479
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-20 17:29:19 -0500

    Conductor code update based on ticket ID conductor/task/Conductor_Issue_1_file/461_1695248957458

--- c/algorithms/count.c
+++ c/algorithms/count.c
@@ -2,22 +2,29 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+// Function to print numbers from 0 to input number
 int count(int num) 
 {
-    printf("[");
-    
+    printf("["); // Start with a "["
+
+    // Declare an integer i for loop
     int i;
+
+    // Loop from 0 to num (exclusive)
     for(i = 0; i < num; i++)
     {
+        // If num > 20 print numbers in a new line
         if(num > 20)
         {
             printf("\n%d, \n", i);
         }
+        // Else, print numbers in the same line
         else 
         {
             printf("%d", i);
         }
-        
+
+        // If i is the last element, print a "]"
         if(i == num - 1)
         {
             printf("]");
@@ -25,10 +32,19 @@ int count(int num)
     }
 }
 
+// Main function to run the program
 int main()
 {
-    int num;
+    int num; // Declare an integer num to hold the input
+
+    // Print the prompt for input
     printf("Count:~$ ");
+
+    // Take the input from the user
     scanf("%d", &num);
+
+    // Call the count function
     count(num);
+
+    return 0; // End the main function
 }
