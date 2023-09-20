b4a99013d1178b64d9a533d6f8d19a081ee2beee
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-20 16:35:54 -0500

    Conductor code update based on ticket ID conductor/task/Conductor_Issue_1_file/461_1695245753372

--- c/algorithms/count.c
+++ c/algorithms/count.c
@@ -1,23 +1,31 @@
+```C
+/* File: c/algorithms/count.c */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 
+/* Count function: This function takes an integer 'num' and prints all numbers up to it */
 int count(int num) 
 {
     printf("[");
     
     int i;
+    /* Iterate from 0 to 'num' */
     for(i = 0; i < num; i++)
     {
+        /* If 'num' is greater than 20, print each number on a new line for readability */
         if(num > 20)
         {
             printf("\n%d, \n", i);
         }
+        /* Else, print the number on the same line */
         else 
         {
             printf("%d", i);
         }
         
+        /* If current number is the last one, print closing bracket */
         if(i == num - 1)
         {
             printf("]");
@@ -25,6 +33,7 @@ int count(int num)
     }
 }
 
+/* Main function: This function asks user for a number and calls the count function */
 int main()
 {
     int num;
@@ -32,3 +41,4 @@ int main()
     scanf("%d", &num);
     count(num);
 }
+```
