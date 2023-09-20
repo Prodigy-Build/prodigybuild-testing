53ce5fc489cee33544557dbc869fbd610362e01e
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-09-21 03:55:38 +0530

    Conductor code update based on ticket ID conductor/task/Github_test/138_1695248730103

--- ruby/sample_app/app/controllers/sessions_controller.rb
+++ ruby/sample_app/app/controllers/sessions_controller.rb
@@ -1,29 +1,33 @@
-class SessionsController < ApplicationController
+require 'test_helper'
 
-  def new
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
   end
 
-  def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      if user.activated?
-        log_in user
-        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-        redirect_back_or user
-      else
-        message  = "Account not activated. "
-        message += "Check your email for the activation link."
-        flash[:warning] = message
-        redirect_to root_url
-      end
-    else
-      flash.now[:danger] = 'Invalid email/password combination'
-      render 'new'
-    end
+  test "should get new" do
+    get login_path
+    assert_response :success
   end
 
-  def destroy
-    log_out if logged_in?
-    redirect_to root_url
+  test "should login with valid email/password" do
+    post login_path, params: { session: { email: @user.email, password: 'password' } }
+    assert_redirected_to @user
+    assert is_logged_in?
+  end
+
+  test "should not login with invalid email/password" do
+    post login_path, params: { session: { email: "wrong", password: "wrong" } }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    assert_not is_logged_in?
+  end
+
+  test "should logout" do
+    log_in_as(@user)
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
   end
 end
