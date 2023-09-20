bef7179992d34832844ae08e0c73e8de0e8b9aca
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-20 11:15:56 -0500

    Conductor code update based on ticket ID conductor/task/Update_Python_Version/459_1695226555050

--- python/algorithms/arithmetic_analysis/bisection.py
+++ python/algorithms/arithmetic_analysis/bisection.py
@@ -1,40 +1,18 @@
-from collections.abc import Callable
-
+from typing import Callable
 
 def bisection(function: Callable[[float], float], a: float, b: float) -> float:
-    """
-    finds where function becomes 0 in [a,b] using bolzano
-    >>> bisection(lambda x: x ** 3 - 1, -5, 5)
-    1.0000000149011612
-    >>> bisection(lambda x: x ** 3 - 1, 2, 1000)
-    Traceback (most recent call last):
-        ...
-    ValueError: could not find root in given interval.
-    >>> bisection(lambda x: x ** 2 - 4 * x + 3, 0, 2)
-    1.0
-    >>> bisection(lambda x: x ** 2 - 4 * x + 3, 2, 4)
-    3.0
-    >>> bisection(lambda x: x ** 2 - 4 * x + 3, 4, 1000)
-    Traceback (most recent call last):
-        ...
-    ValueError: could not find root in given interval.
-    """
-    start: float = a
-    end: float = b
-    if function(a) == 0:  # one of the a or b is a root for the function
+    start, end = a, b
+    if function(a) == 0:  # a is a root for the function
         return a
-    elif function(b) == 0:
+    elif function(b) == 0:  # b is root for the function
         return b
-    elif (
-        function(a) * function(b) > 0
-    ):  # if none of these are root and they are both positive or negative,
-        # then this algorithm can't find the root
-        raise ValueError("could not find root in given interval.")
+    elif function(a) * function(b) > 0:
+        raise ValueError("No root found within given interval.")
     else:
-        mid: float = start + (end - start) / 2.0
-        while abs(start - mid) > 10**-7:  # until precisely equals to 10^-7
+        mid = start + (end - start) / 2.0
+        while abs(start - mid) > 1e-7:  # until difference is ~ 0
             if function(mid) == 0:
-                return mid
+                return mid  # mid is root for the function
             elif function(mid) * function(start) < 0:
                 end = mid
             else:
@@ -42,14 +20,11 @@ def bisection(function: Callable[[float], float], a: float, b: float) -> float:
             mid = start + (end - start) / 2.0
         return mid
 
-
 def f(x: float) -> float:
     return x**3 - 2 * x - 5
-
 
 if __name__ == "__main__":
     print(bisection(f, 1, 1000))
 
     import doctest
-
     doctest.testmod()
