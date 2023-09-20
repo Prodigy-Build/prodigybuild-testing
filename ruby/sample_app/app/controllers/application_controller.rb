1a56a4d89ccb12a4fc7dd7a5f35e87efc389baba
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-09-21 03:55:33 +0530

    Conductor code update based on ticket ID conductor/task/Github_test/138_1695248730103

--- ruby/sample_app/app/controllers/application_controller.rb
+++ ruby/sample_app/app/controllers/application_controller.rb
@@ -1,15 +1,35 @@
+```ruby
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
   private
 
-    # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
     end
+  end
 end
+
+# Adding below the unit test for the ApplicationController
+
+require 'test_helper'
+
+class ApplicationControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get danger flash when not logged in" do
+    get root_url
+    assert_redirected_to login_url
+    assert_not flash[:danger].empty?
+  end
+
+  test "should store location when not logged in" do
+    get root_url
+    follow_redirect!
+    assert_equal 'Please log in.', flash[:danger]
+  end
+end
+```
