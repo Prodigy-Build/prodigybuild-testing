64977c01d89446dd67f8f5e60725dc96343fe6a2
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-20 02:50:09 -0500

    Conductor code update based on ticket ID conductor/task/Update_Python_Version/459_1695196205912

--- python/algorithms/arithmetic_analysis/gaussian_elimination.py
+++ python/algorithms/arithmetic_analysis/gaussian_elimination.py
@@ -3,35 +3,18 @@ Gaussian elimination method for solving a system of linear equations.
 Gaussian elimination - https://en.wikipedia.org/wiki/Gaussian_elimination
 """
 
-
+from typing import Any, Tuple, Union
 import numpy as np
-from numpy import float64
-from numpy.typing import NDArray
 
 
-def retroactive_resolution(
-    coefficients: NDArray[float64], vector: NDArray[float64]
-) -> NDArray[float64]:
+def retroactive_resolution(coefficients: np.ndarray, vector: np.ndarray) -> np.ndarray:
     """
     This function performs a retroactive linear system resolution
         for triangular matrix
-
-    Examples:
-        2x1 + 2x2 - 1x3 = 5         2x1 + 2x2 = -1
-        0x1 - 2x2 - 1x3 = -7        0x1 - 2x2 = -1
-        0x1 + 0x2 + 5x3 = 15
-    >>> gaussian_elimination([[2, 2, -1], [0, -2, -1], [0, 0, 5]], [[5], [-7], [15]])
-    array([[2.],
-           [2.],
-           [3.]])
-    >>> gaussian_elimination([[2, 2], [0, -2]], [[-1], [-1]])
-    array([[-1. ],
-           [ 0.5]])
     """
-
     rows, columns = np.shape(coefficients)
 
-    x: NDArray[float64] = np.zeros((rows, 1), dtype=float)
+    x: np.ndarray = np.zeros((rows, 1), dtype=float)
     for row in reversed(range(rows)):
         total = 0
         for col in range(row + 1, columns):
@@ -42,23 +25,9 @@ def retroactive_resolution(
     return x
 
 
-def gaussian_elimination(
-    coefficients: NDArray[float64], vector: NDArray[float64]
-) -> NDArray[float64]:
+def gaussian_elimination(coefficients: np.ndarray, vector: np.ndarray) -> np.ndarray:
     """
     This function performs Gaussian elimination method
-
-    Examples:
-        1x1 - 4x2 - 2x3 = -2        1x1 + 2x2 = 5
-        5x1 + 2x2 - 2x3 = -3        5x1 + 2x2 = 5
-        1x1 - 1x2 + 0x3 = 4
-    >>> gaussian_elimination([[1, -4, -2], [5, 2, -2], [1, -1, 0]], [[-2], [-3], [4]])
-    array([[ 2.3 ],
-           [-1.7 ],
-           [ 5.55]])
-    >>> gaussian_elimination([[1, 2], [5, 2]], [[5], [5]])
-    array([[0. ],
-           [2.5]])
     """
     # coefficients must to be a square matrix so we need to check first
     rows, columns = np.shape(coefficients)
@@ -66,7 +35,7 @@ def gaussian_elimination(
         return np.array((), dtype=float)
 
     # augmented matrix
-    augmented_mat: NDArray[float64] = np.concatenate((coefficients, vector), axis=1)
+    augmented_mat: np.ndarray = np.concatenate((coefficients, vector), axis=1)
     augmented_mat = augmented_mat.astype("float64")
 
     # scale the matrix leaving it triangular
@@ -85,5 +54,4 @@ def gaussian_elimination(
 
 if __name__ == "__main__":
     import doctest
-
     doctest.testmod()
