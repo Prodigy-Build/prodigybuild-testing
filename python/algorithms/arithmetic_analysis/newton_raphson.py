fe531b9eca7aec69627cc7cd3977b479a69755a5
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-20 13:29:23 -0500

    Conductor code update based on ticket ID conductor/task/Update_Python_Version/459_1695234551318

--- python/algorithms/arithmetic_analysis/newton_raphson.py
+++ python/algorithms/arithmetic_analysis/newton_raphson.py
@@ -2,7 +2,6 @@
 # Author: Syed Haseeb Shah (github.com/QuantumNovice)
 # The Newton-Raphson method (also known as Newton's method) is a way to
 # quickly find a good approximation for the root of a real-valued function
-from __future__ import annotations
 
 from decimal import Decimal
 from math import *  # noqa: F403
@@ -35,10 +34,10 @@ def newton_raphson(
 if __name__ == "__main__":
     # Find root of trigonometric function
     # Find value of pi
-    print(f"The root of sin(x) = 0 is {newton_raphson('sin(x)', 2)}")
+    print(f"The root of sin(x) = 0 is {newton_raphson('sin(x)', 2.0)}")
     # Find root of polynomial
     print(f"The root of x**2 - 5*x + 2 = 0 is {newton_raphson('x**2 - 5*x + 2', 0.4)}")
     # Find Square Root of 5
-    print(f"The root of log(x) - 1 = 0 is {newton_raphson('log(x) - 1', 2)}")
+    print(f"The root of log(x) - 1 = 0 is {newton_raphson('log(x) - 1', 2.0)}")
     # Exponential Roots
-    print(f"The root of exp(x) - 1 = 0 is {newton_raphson('exp(x) - 1', 0)}")
+    print(f"The root of exp(x) - 1 = 0 is {newton_raphson('exp(x) - 1', 0.0)}")
