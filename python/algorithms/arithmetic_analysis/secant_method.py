f68c947f5428a7429f8e252d3ba61c91c33f8157
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-20 11:16:12 -0500

    Conductor code update based on ticket ID conductor/task/Update_Python_Version/459_1695226555050

--- python/algorithms/arithmetic_analysis/secant_method.py
+++ python/algorithms/arithmetic_analysis/secant_method.py
@@ -20,10 +20,20 @@ def secant_method(lower_bound: float, upper_bound: float, repeats: int) -> float
     """
     x0 = lower_bound
     x1 = upper_bound
-    for _ in range(0, repeats):
-        x0, x1 = x1, x1 - (f(x1) * (x1 - x0)) / (f(x1) - f(x0))
+    try:
+        for _ in range(0, repeats):
+            x0, x1 = x1, x1 - (f(x1) * (x1 - x0)) / (f(x1) - f(x0))
+    except ZeroDivisionError:
+        print("f(x1) and f(x0) are same. So, the method cannot proceed further")
+        return x0
+    except Exception as e:
+        print("Something went wrong! Error: ", e)
+        return x0
     return x1
 
 
 if __name__ == "__main__":
-    print(f"Example: {secant_method(1, 3, 2)}")
+    try:
+        print(f"Example: {secant_method(1, 3, 2)}")
+    except Exception as e:
+        print("Error in execution:", e)
