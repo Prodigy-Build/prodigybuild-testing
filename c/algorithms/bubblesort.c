55b6a096aacc2b6b5a31771a6658cc85a2713bc9
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-20 18:30:58 -0500

    Conductor code update based on ticket ID conductor/task/Conductor_issue_2/462_1695252656176

--- c/algorithms/bubblesort.c
+++ c/algorithms/bubblesort.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-void bubble_sort(int *array, int len) 
-{
-    for (int i = 0; i < len; i++) {
-        for (int j = 0; j < len - i - 1; j++) {
+void bubble_sort(int *array, int len) {
+    int i, j, tmp;
+    for (i = 0; i < len - 1; i++) {
+        for (j = 0; j < len - i - 1; j++) {
             if (array[j] > array[j + 1]) {
-                int tmp = array[j];
+                tmp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = tmp;
             }
@@ -13,15 +13,20 @@ void bubble_sort(int *array, int len)
     }
 }
 
-int main(void) 
-{
+int main(void) {
     int yarr[] = {3, 9, 4, 8, 7, 6, 1, 2, 0, 10};
-    
-    bubble_sort(yarr, 10);
+    int i, len = sizeof(yarr)/sizeof(yarr[0]);
+
+    bubble_sort(yarr, len);
 
     printf("[");
-    for (int i = 0; i < 10; i++)
-        printf("%d, ", yarr[i]);	
-    printf("\b\b]\n");
+    for (i = 0; i < len; i++){
+        printf("%d", yarr[i]);
+        if(i < len - 1) {
+            printf(", ");
+        } 
+    }
+    printf("]\n");
+
     return 0;
 }
