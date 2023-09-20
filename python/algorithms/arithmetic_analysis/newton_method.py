615c328286588228fcbb35cc5fc8b48f3ad07f4c
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-20 02:49:58 -0500

    Conductor code update based on ticket ID conductor/task/Update_Python_Version/459_1695196184366

--- python/algorithms/arithmetic_analysis/newton_method.py
+++ python/algorithms/arithmetic_analysis/newton_method.py
@@ -2,6 +2,7 @@
 
 # Newton's Method - https://en.wikipedia.org/wiki/Newton%27s_method
 from collections.abc import Callable
+from typing import Tuple
 
 RealFunc = Callable[[float], float]  # type alias for a real -> real function
 
@@ -10,45 +11,32 @@ RealFunc = Callable[[float], float]  # type alias for a real -> real function
 def newton(
     function: RealFunc,
     derivative: RealFunc,
-    starting_int: int,
-) -> float:
-    """
-    >>> newton(lambda x: x ** 3 - 2 * x - 5, lambda x: 3 * x ** 2 - 2, 3)
-    2.0945514815423474
-    >>> newton(lambda x: x ** 3 - 1, lambda x: 3 * x ** 2, -2)
-    1.0
-    >>> newton(lambda x: x ** 3 - 1, lambda x: 3 * x ** 2, -4)
-    1.0000000000000102
-    >>> import math
-    >>> newton(math.sin, math.cos, 1)
-    0.0
-    >>> newton(math.sin, math.cos, 2)
-    3.141592653589793
-    >>> newton(math.cos, lambda x: -math.sin(x), 2)
-    1.5707963267948966
-    >>> newton(math.cos, lambda x: -math.sin(x), 0)
-    Traceback (most recent call last):
-        ...
-    ZeroDivisionError: Could not find root
-    """
-    prev_guess = float(starting_int)
-    while True:
-        try:
-            next_guess = prev_guess - function(prev_guess) / derivative(prev_guess)
-        except ZeroDivisionError:
-            raise ZeroDivisionError("Could not find root") from None
-        if abs(prev_guess - next_guess) < 10**-5:
-            return next_guess
-        prev_guess = next_guess
-
-
-def f(x: float) -> float:
-    return (x**3) - (2 * x) - 5
-
-
-def f1(x: float) -> float:
-    return 3 * (x**2) - 2
+    starting_int: float,
+    tolerance: float = 1e-5,
+    max_iter: int = 100
+) -> Tuple[float, int]:
+    
+    x = starting_int
+    for _ in range(max_iter):
+        fx = function(x)
+        if abs(fx) < tolerance:
+            return x
+        fpx = derivative(x)
+        if abs(fpx) < tolerance:
+            raise ZeroDivisionError("Could not find root")
+        x -= fx/fpx
+    else:
+        raise Exception(f"Failed to find root in {max_iter} iterations.")
 
 
 if __name__ == "__main__":
-    print(newton(f, f1, 3))
+    # Function and its derivative
+    f = lambda x: (x**3) - (2 * x) - 5
+    fpx = lambda x: 3 * (x**2) - 2
+
+    # Invoke newton method with starting_int as 3
+    try:
+        root, iterations = newton(f, fpx, 3)
+        print(f"Root: {root:.5f} (found in {iterations} iterations)")
+    except Exception as e:
+        print(str(e))
