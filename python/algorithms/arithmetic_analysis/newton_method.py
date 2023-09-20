81c5344cfa49ccae41472b075c8c50437025911d
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-20 11:16:05 -0500

    Conductor code update based on ticket ID conductor/task/Update_Python_Version/459_1695226555050

--- python/algorithms/arithmetic_analysis/newton_method.py
+++ python/algorithms/arithmetic_analysis/newton_method.py
@@ -2,6 +2,7 @@
 
 # Newton's Method - https://en.wikipedia.org/wiki/Newton%27s_method
 from collections.abc import Callable
+from typing import Tuple
 
 RealFunc = Callable[[float], float]  # type alias for a real -> real function
 
@@ -11,44 +12,47 @@ def newton(
     function: RealFunc,
     derivative: RealFunc,
     starting_int: int,
-) -> float:
+) -> Tuple[float, int]:
     """
     >>> newton(lambda x: x ** 3 - 2 * x - 5, lambda x: 3 * x ** 2 - 2, 3)
-    2.0945514815423474
+    (2.0945514815423474, 10)
     >>> newton(lambda x: x ** 3 - 1, lambda x: 3 * x ** 2, -2)
-    1.0
+    (1.0, 5)
     >>> newton(lambda x: x ** 3 - 1, lambda x: 3 * x ** 2, -4)
-    1.0000000000000102
+    (1.0000000000000102, 7)
     >>> import math
     >>> newton(math.sin, math.cos, 1)
-    0.0
+    (0.0, 1)
     >>> newton(math.sin, math.cos, 2)
-    3.141592653589793
+    (3.141592653589793, 27)
     >>> newton(math.cos, lambda x: -math.sin(x), 2)
-    1.5707963267948966
+    (1.5707963267948966, 4)
     >>> newton(math.cos, lambda x: -math.sin(x), 0)
     Traceback (most recent call last):
         ...
     ZeroDivisionError: Could not find root
     """
+    iterations = 0
     prev_guess = float(starting_int)
     while True:
         try:
             next_guess = prev_guess - function(prev_guess) / derivative(prev_guess)
+            iterations += 1
         except ZeroDivisionError:
             raise ZeroDivisionError("Could not find root") from None
         if abs(prev_guess - next_guess) < 10**-5:
-            return next_guess
+            return next_guess, iterations
         prev_guess = next_guess
 
 
 def f(x: float) -> float:
     return (x**3) - (2 * x) - 5
 
-
+    
 def f1(x: float) -> float:
     return 3 * (x**2) - 2
 
 
 if __name__ == "__main__":
-    print(newton(f, f1, 3))
+    result, iterations = newton(f, f1, 3)
+    print(f'The root is {result} and it was found in {iterations} iterations')
