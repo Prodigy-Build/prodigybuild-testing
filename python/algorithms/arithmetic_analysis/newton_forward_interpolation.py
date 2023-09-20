e9d091ab18038574e208dd328958899d86055a05
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-20 02:56:58 -0500

    Conductor code update based on ticket ID conductor/task/Update_Python_Version/459_1695196609143

--- python/algorithms/arithmetic_analysis/newton_forward_interpolation.py
+++ python/algorithms/arithmetic_analysis/newton_forward_interpolation.py
@@ -1,8 +1,6 @@
 # https://www.geeksforgeeks.org/newton-forward-backward-interpolation/
-from __future__ import annotations
 
 import math
-
 
 # for calculating u value
 def ucal(u: float, p: int) -> float:
@@ -21,27 +19,20 @@ def ucal(u: float, p: int) -> float:
 
 
 def main() -> None:
-    n = int(input("enter the numbers of values: "))
-    y: list[list[float]] = []
-    for _ in range(n):
-        y.append([])
-    for i in range(n):
-        for j in range(n):
-            y[i].append(j)
-            y[i][j] = 0
+    n = int(input("Enter the numbers of values: "))
+    y: list[list[float]] = [[0 for _ in range(n)] for _ in range(n)]
 
-    print("enter the values of parameters in a list: ")
+    print("Enter the values of parameters in a list: ")
     x = list(map(int, input().split()))
 
-    print("enter the values of corresponding parameters: ")
+    print("Enter the values of corresponding parameters: ")
     for i in range(n):
         y[i][0] = float(input())
 
-    value = int(input("enter the value to interpolate: "))
+    value = int(input("Enter the value to interpolate: "))
     u = (value - x[0]) / (x[1] - x[0])
 
     # for calculating forward difference table
-
     for i in range(1, n):
         for j in range(n - i):
             y[j][i] = y[j + 1][i - 1] - y[j][i - 1]
@@ -50,7 +41,7 @@ def main() -> None:
     for i in range(1, n):
         summ += (ucal(u, i) * y[0][i]) / math.factorial(i)
 
-    print(f"the value at {value} is {summ}")
+    print(f"The value at {value} is {summ:.2f}")
 
 
 if __name__ == "__main__":
