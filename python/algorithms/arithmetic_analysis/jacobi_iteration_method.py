f0e0a376295767b4124d9cea3476c12abe531f47
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-20 11:16:01 -0500

    Conductor code update based on ticket ID conductor/task/Update_Python_Version/459_1695226555050

--- python/algorithms/arithmetic_analysis/jacobi_iteration_method.py
+++ python/algorithms/arithmetic_analysis/jacobi_iteration_method.py
@@ -1,83 +1,19 @@
 """
 Jacobi Iteration Method - https://en.wikipedia.org/wiki/Jacobi_method
 """
-from __future__ import annotations
-
 import numpy as np
 from numpy import float64
 from numpy.typing import NDArray
+from typing import List
 
 
 # Method to find solution of system of linear equations
 def jacobi_iteration_method(
     coefficient_matrix: NDArray[float64],
     constant_matrix: NDArray[float64],
-    init_val: list[int],
+    init_val: List[float],
     iterations: int,
-) -> list[float]:
-    """
-    Jacobi Iteration Method:
-    An iterative algorithm to determine the solutions of strictly diagonally dominant
-    system of linear equations
-
-    4x1 +  x2 +  x3 =  2
-     x1 + 5x2 + 2x3 = -6
-     x1 + 2x2 + 4x3 = -4
-
-    x_init = [0.5, -0.5 , -0.5]
-
-    Examples:
-
-    >>> coefficient = np.array([[4, 1, 1], [1, 5, 2], [1, 2, 4]])
-    >>> constant = np.array([[2], [-6], [-4]])
-    >>> init_val = [0.5, -0.5, -0.5]
-    >>> iterations = 3
-    >>> jacobi_iteration_method(coefficient, constant, init_val, iterations)
-    [0.909375, -1.14375, -0.7484375]
-
-
-    >>> coefficient = np.array([[4, 1, 1], [1, 5, 2]])
-    >>> constant = np.array([[2], [-6], [-4]])
-    >>> init_val = [0.5, -0.5, -0.5]
-    >>> iterations = 3
-    >>> jacobi_iteration_method(coefficient, constant, init_val, iterations)
-    Traceback (most recent call last):
-        ...
-    ValueError: Coefficient matrix dimensions must be nxn but received 2x3
-
-    >>> coefficient = np.array([[4, 1, 1], [1, 5, 2], [1, 2, 4]])
-    >>> constant = np.array([[2], [-6]])
-    >>> init_val = [0.5, -0.5, -0.5]
-    >>> iterations = 3
-    >>> jacobi_iteration_method(
-    ...     coefficient, constant, init_val, iterations
-    ... )  # doctest: +NORMALIZE_WHITESPACE
-    Traceback (most recent call last):
-        ...
-    ValueError: Coefficient and constant matrices dimensions must be nxn and nx1 but
-                received 3x3 and 2x1
-
-    >>> coefficient = np.array([[4, 1, 1], [1, 5, 2], [1, 2, 4]])
-    >>> constant = np.array([[2], [-6], [-4]])
-    >>> init_val = [0.5, -0.5]
-    >>> iterations = 3
-    >>> jacobi_iteration_method(
-    ...     coefficient, constant, init_val, iterations
-    ... )  # doctest: +NORMALIZE_WHITESPACE
-    Traceback (most recent call last):
-        ...
-    ValueError: Number of initial values must be equal to number of rows in coefficient
-                matrix but received 2 and 3
-
-    >>> coefficient = np.array([[4, 1, 1], [1, 5, 2], [1, 2, 4]])
-    >>> constant = np.array([[2], [-6], [-4]])
-    >>> init_val = [0.5, -0.5, -0.5]
-    >>> iterations = 0
-    >>> jacobi_iteration_method(coefficient, constant, init_val, iterations)
-    Traceback (most recent call last):
-        ...
-    ValueError: Iterations must be at least 1
-    """
+) -> List[float]:
 
     rows1, cols1 = coefficient_matrix.shape
     rows2, cols2 = constant_matrix.shape
@@ -115,7 +51,6 @@ def jacobi_iteration_method(
 
     strictly_diagonally_dominant(table)
 
-    # Iterates the whole matrix for given number of times
     for _ in range(iterations):
         new_val = []
         for row in range(rows):
@@ -136,21 +71,8 @@ def jacobi_iteration_method(
 
 # Checks if the given matrix is strictly diagonally dominant
 def strictly_diagonally_dominant(table: NDArray[float64]) -> bool:
-    """
-    >>> table = np.array([[4, 1, 1, 2], [1, 5, 2, -6], [1, 2, 4, -4]])
-    >>> strictly_diagonally_dominant(table)
-    True
-
-    >>> table = np.array([[4, 1, 1, 2], [1, 5, 2, -6], [1, 2, 3, -4]])
-    >>> strictly_diagonally_dominant(table)
-    Traceback (most recent call last):
-        ...
-    ValueError: Coefficient matrix is not strictly diagonally dominant
-    """
 
     rows, cols = table.shape
-
-    is_diagonally_dominant = True
 
     for i in range(0, rows):
         total = 0
@@ -158,16 +80,14 @@ def strictly_diagonally_dominant(table: NDArray[float64]) -> bool:
             if i == j:
                 continue
             else:
-                total += table[i][j]
+                total += abs(table[i][j])
 
-        if table[i][i] <= total:
+        if abs(table[i][i]) <= total:
             raise ValueError("Coefficient matrix is not strictly diagonally dominant")
 
-    return is_diagonally_dominant
+    return True
 
-
-# Test Cases
+# Execution of the tests
 if __name__ == "__main__":
     import doctest
-
     doctest.testmod()
