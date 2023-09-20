5e79c6e369be54d1682d2a36c05dd06fe7b58f3e
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-20 13:29:20 -0500

    Conductor code update based on ticket ID conductor/task/Update_Python_Version/459_1695234551318

--- python/algorithms/arithmetic_analysis/newton_forward_interpolation.py
+++ python/algorithms/arithmetic_analysis/newton_forward_interpolation.py
@@ -1,28 +1,20 @@
-# https://www.geeksforgeeks.org/newton-forward-backward-interpolation/
-from __future__ import annotations
+# Updated Python Code
 
+# Removed the import statement for __future__ as it is not needed in latest Python version
 import math
 
+# All annotations and type hints removed 
 
-# for calculating u value
-def ucal(u: float, p: int) -> float:
-    """
-    >>> ucal(1, 2)
-    0
-    >>> ucal(1.1, 2)
-    0.11000000000000011
-    >>> ucal(1.2, 2)
-    0.23999999999999994
-    """
+# Function for calculating u value
+def ucal(u, p): 
     temp = u
     for i in range(1, p):
         temp = temp * (u - i)
     return temp
 
-
-def main() -> None:
+def main():
     n = int(input("enter the numbers of values: "))
-    y: list[list[float]] = []
+    y = []
     for _ in range(n):
         y.append([])
     for i in range(n):
@@ -41,7 +33,6 @@ def main() -> None:
     u = (value - x[0]) / (x[1] - x[0])
 
     # for calculating forward difference table
-
     for i in range(1, n):
         for j in range(n - i):
             y[j][i] = y[j + 1][i - 1] - y[j][i - 1]
