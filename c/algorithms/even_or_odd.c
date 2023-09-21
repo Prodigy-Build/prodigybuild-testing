b21c41d608020e07a9dcc3a9dbb15ba24777701a
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-09-21 06:41:53 +0530

    Conductor code update based on ticket ID conductor/task/Github_test/101_1695258710672

--- c/algorithms/even_or_odd.c
+++ c/algorithms/even_or_odd.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
+#include <assert.h>
+
+int even_or_odd(long num)
+{
+    return num % 2 == 0;
+}
+
+void test_even_or_odd() 
+{
+    assert(even_or_odd(2) == 1);
+    assert(even_or_odd(3) == 0);
+    assert(even_or_odd(0) == 1);
+    assert(even_or_odd(-2) == 1);
+    assert(even_or_odd(-3) == 0);
+}
 
 int main()
 {
+    test_even_or_odd();
+
     long num;
     printf("Enter a number\n");
     scanf("%ld", &num);
-    int rem = num % 2;
     
-    if(rem == 0)
-    printf("Even");
+    if(even_or_odd(num))
+        printf("Even");
     else
-    printf("Odd");
+        printf("Odd");
+    
+    return 0;
 }
