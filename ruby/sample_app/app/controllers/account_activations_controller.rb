50bbfc198a74b7ca906d4d9e6e49fecda6542ec4
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-21 15:46:37 -0500

    Conductor code update based on ticket ID conductor/task/Update_word_usage_in_teamsync/465_1695329195838

--- ruby/sample_app/app/controllers/account_activations_controller.rb
+++ ruby/sample_app/app/controllers/account_activations_controller.rb
@@ -1,15 +1,28 @@
 class AccountActivationsController < ApplicationController
+  before_action :find_user, only: :edit
 
   def edit
-    user = User.find_by(email: params[:email])
-    if user && !user.activated? && user.authenticated?(:activation, params[:id])
-      user.activate
-      log_in user
-      flash[:success] = "Account activated!"
-      redirect_to user
+    if @user && !@user.activated? && @user.authenticated?(:activation, params[:id])
+      activate_user
     else
       flash[:danger] = "Invalid activation link"
       redirect_to root_url
     end
+  rescue ActiveRecord::RecordNotFound
+    flash[:danger] = "User not found"
+    redirect_to root_url
+  end
+
+  private
+
+  def find_user
+    @user = User.find_by!(email: params[:email])
+  end
+
+  def activate_user
+    @user.activate
+    log_in @user
+    flash[:success] = "Account activated!"
+    redirect_to @user
   end
 end
