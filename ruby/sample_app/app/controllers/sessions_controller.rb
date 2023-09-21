20da70a68f52e071b9c6fb087b6efa493d9fb6d3
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-21 15:46:46 -0500

    Conductor code update based on ticket ID conductor/task/Update_word_usage_in_teamsync/465_1695329195838

--- ruby/sample_app/app/controllers/sessions_controller.rb
+++ ruby/sample_app/app/controllers/sessions_controller.rb
@@ -1,21 +1,14 @@
+```ruby
 class SessionsController < ApplicationController
+  before_action :downcase_email, only: :create
+  before_action :find_user, only: :create
 
   def new
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      if user.activated?
-        log_in user
-        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-        redirect_back_or user
-      else
-        message  = "Account not activated. "
-        message += "Check your email for the activation link."
-        flash[:warning] = message
-        redirect_to root_url
-      end
+    if @user && @user.authenticate(params[:session][:password])
+      user_authenticated
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
@@ -26,4 +19,31 @@ class SessionsController < ApplicationController
     log_out if logged_in?
     redirect_to root_url
   end
+
+  private
+
+  def downcase_email
+    params[:session][:email].downcase!
+  end
+
+  def find_user
+    @user = User.find_by(email: params[:session][:email])
+  end
+
+  def user_authenticated
+    if @user.activated?
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_back_or @user
+    else
+      flash[:warning] = "Account not activated. Check your email for the activation link."
+      redirect_to root_url
+    end
+  end
+
+  rescue_from ActiveRecord::RecordNotFound do
+    flash.now[:danger] = 'Invalid email/password combination'
+    render 'new'
+  end
 end
+```
