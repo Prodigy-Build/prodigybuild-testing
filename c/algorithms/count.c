e9d9e407740e2999be9dc28357a147eeca7462dd
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-09-21 06:41:51 +0530

    Conductor code update based on ticket ID conductor/task/Github_test/101_1695258710672

--- c/algorithms/count.c
+++ c/algorithms/count.c
@@ -1,32 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <assert.h>
 
 int count(int num) 
 {
     printf("[");
-    
-    if(num > 20)
+    int i;
+    for(i = 0; i < num; i++)
     {
-        int i;
-        for(i = 0; i < num; i++)
+        if(num > 20)
             printf("\n%d, \n", i);
-            
-        if(i == num + 1)
-            printf("]");
-    } else
-    {
-        int i;
-        for(i = 0; i < num; i++)
+        else
             printf("%d", i);
-            
-        if(i == num)
-            printf("]");
-    }
+    } 
+    if(i == num)
+        printf("]");
+    return i;
+}
+
+void test_count() 
+{
+    assert(count(5) == 5);
+    assert(count(30) == 30);
 }
 
 int main()
 {
+    test_count();
+    printf("All tests passed\n");
     int num;
     printf("Count:~$ ");
     scanf("%d", &num);
