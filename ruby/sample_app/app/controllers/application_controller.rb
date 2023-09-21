4c18e64c07b9d42803efcf0cadfeab4c2fcc67e4
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-21 15:46:39 -0500

    Conductor code update based on ticket ID conductor/task/Update_word_usage_in_teamsync/465_1695329195838

--- ruby/sample_app/app/controllers/application_controller.rb
+++ ruby/sample_app/app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+```ruby
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
@@ -6,10 +7,25 @@ class ApplicationController < ActionController::Base
 
     # Confirms a logged-in user.
     def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
+      if logged_in?
+        yield
+      else
+        handle_not_logged_in
       end
+    rescue
+      handle_error
     end
+
+    def handle_not_logged_in
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+
+    def handle_error
+      flash[:error] = "An error occurred."
+      redirect_to root_url
+    end
+
 end
+```
