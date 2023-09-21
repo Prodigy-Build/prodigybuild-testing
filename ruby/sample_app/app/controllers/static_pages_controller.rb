b0eeb640b616d576bf526cf855dd7bdc5edfa952
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-21 15:46:47 -0500

    Conductor code update based on ticket ID conductor/task/Update_word_usage_in_teamsync/465_1695329195838

--- ruby/sample_app/app/controllers/static_pages_controller.rb
+++ ruby/sample_app/app/controllers/static_pages_controller.rb
@@ -5,14 +5,22 @@ class StaticPagesController < ApplicationController
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
+  rescue
+    render 'errors/standard_error'
   end
 
   def help
+  rescue
+    render 'errors/standard_error'
   end
   
   def about
+  rescue
+    render 'errors/standard_error'
   end
 
   def contact
+  rescue
+    render 'errors/standard_error'
   end
 end
