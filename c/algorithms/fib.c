9395b248be02ded49e7c47f916c07df0518d23e1
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-09-21 06:41:55 +0530

    Conductor code update based on ticket ID conductor/task/Github_test/101_1695258710672

--- c/algorithms/fib.c
+++ c/algorithms/fib.c
@@ -1,18 +1,35 @@
+```c
 #include <stdio.h>
+#include <assert.h>
+
+// Function to be tested
+long fib(long n)
+{
+    long a = 0;
+    long b = 1;
+    
+    for(long i = 0; i < n; i++)
+    {
+        long temp = b;
+        b = a + b;
+        a = temp;
+    }
+    return a;
+}
+
+// Our test
+void test_fib() {
+    assert(fib(0) == 0);
+    assert(fib(1) == 1);
+    assert(fib(2) == 1);
+    assert(fib(3) == 2);
+    assert(fib(10) == 55);
+    printf("All tests passed.\n");
+}
 
 int main()
 {
-    long a;
-    long b;
-    
-    for(a = 0, b = 1; b > a; b++)
-    {
-        a++;
-        printf("%ld \n %ld", &a, &b);
-    }
-    
-    if(b == sizeof(long))
-    {
-        exit(1);
-    }
+    test_fib();
+    return 0;
 }
+```
