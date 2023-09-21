0f24bae419b400fd8d8c9e8994c4605aa3ea35f8
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-21 09:26:52 -0500

    Conductor code update based on ticket ID conductor/task/Conductor_issue_2/462_1695306409699

--- c/algorithms/bubblesort.c
+++ c/algorithms/bubblesort.c
@@ -2,10 +2,12 @@
 
 void bubble_sort(int *array, int len) 
 {
-    for (int i = 0; i < len; i++) {
-        for (int j = 0; j < len - i - 1; j++) {
+    int i, j, tmp;
+
+    for (i = 0; i < len; i++) {
+        for (j = 0; j < len - i - 1; j++) {
             if (array[j] > array[j + 1]) {
-                int tmp = array[j];
+                tmp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = tmp;
             }
@@ -16,12 +18,14 @@ void bubble_sort(int *array, int len)
 int main(void) 
 {
     int yarr[] = {3, 9, 4, 8, 7, 6, 1, 2, 0, 10};
-    
-    bubble_sort(yarr, 10);
+    int len = sizeof(yarr) / sizeof(yarr[0]);
+
+    bubble_sort(yarr, len);
 
     printf("[");
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < len; i++) {
         printf("%d, ", yarr[i]);	
+    }
     printf("\b\b]\n");
     return 0;
 }
