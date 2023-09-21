9b03940945710bdb7e9c6147f04d17c3c830b1c4
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-21 05:59:24 -0500

    Conductor code update based on ticket ID conductor/task/Conductor_Issue_1_file/461_1695293962325

--- c/algorithms/count.c
+++ c/algorithms/count.c
@@ -1,23 +1,32 @@
+```c
+// Including necessary library files
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 
+/* This function receives an integer input parameter num.
+ * It prints each number from 0 to num-1 in individual lines if input num is greater than 20.
+ * Else it just prints the numbers with no newline character.
+ * The formatted print is enclosed within square brackets */
 int count(int num) 
 {
+    // Each output will be enclosed within a square bracker, here we insert the opening bracket
     printf("[");
-    
-    int i;
-    for(i = 0; i < num; i++)
+
+    // Iterating from 0 to num - 1
+    for(int i = 0; i < num; i++)
     {
+        // Checking if number is greater than 20. If yes, printing number in new line. If not, in the same line.
         if(num > 20)
         {
             printf("\n%d, \n", i);
         }
         else 
         {
-            printf("%d", i);
+            printf("%d ", i);
         }
         
+        // Closing bracket is printed after last number
         if(i == num - 1)
         {
             printf("]");
@@ -25,10 +34,12 @@ int count(int num)
     }
 }
 
+// Main function
 int main()
 {
-    int num;
-    printf("Count:~$ ");
-    scanf("%d", &num);
-    count(num);
+    int num;     // Variable for storing user input
+    printf("Count:~$ ");  // Asking for user input
+    scanf("%d", &num);    // Storing user input on variable num
+    count(num);   // Calling function count with user given input
 }
+```
