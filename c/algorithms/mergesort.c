8b96f3be5910eccb8182b84c6fe56d9802e994d1
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-22 13:08:42 -0500

    Conductor code update based on ticket ID conductor/task/Batch_refactor_C_files/461_1695406113348

--- c/algorithms/mergesort.c
+++ c/algorithms/mergesort.c
@@ -2,52 +2,69 @@
 #include<time.h>
 #include<stdlib.h>
 
-// function to merge the sub-arrays
-void merge(int a[],int low,int mid ,int high)
-{
-	int b[20]; //same size of a[]
-	int i, j, k;
-	i = low, j = mid + 1,k = low;
-	while(i <= mid && j <= high)
-	{
-		if(a[i]<=a[j])
-		    b[k++]=a[i++];
-		else
-		   b[k++]=a[j++]; //copying the elements 
-	}
-	while (i<=mid)
-		b[k++]=a[i++];
-	while 
-		(j<=high) b[k++]=a[j++];
-		for (k=low;k<=high;k++)
-	        a[k]=b[k];
+// Function to merge the sub-arrays
+void merge(int a[], int low, int mid, int high) {
+    int b[20]; // Temporary array
+
+    // Pointers for a[low..mid] and a[mid+1..high]
+    int i = low;
+    int j = mid + 1;
+    int k = low;
+
+    // Merge the two halves
+    while (i <= mid && j <= high) {
+        // Choose minimum between two
+        if (a[i] <= a[j])
+            b[k++] = a[i++];
+        else
+            b[k++] = a[j++];
+    }
+
+    // If any elements are left in first half
+    while (i <= mid)
+        b[k++] = a[i++];
+
+    // If any elements are left in second half
+    while (j <= high)
+        b[k++] = a[j++];
+
+    // Copy temporary array back into original array
+    for (k = low; k <= high; k++)
+        a[k] = b[k];
 }
 
-// merge sort function
-void mergesort(int a[],int low,int high)
-{
-	int mid;
-	if(low>=high)
-	  return;
-	mid=(low+high)/2;
-	mergesort(a,low,mid);
-	mergesort(a,mid+1,high);
-	merge(a,low,mid,high);
+// Merge sort function
+void mergesort(int a[], int low, int high) {
+    int mid;
+
+    // Base case (single element)
+    if (low >= high)
+        return;
+
+    // Find mid point
+    mid = (low + high) / 2;
+
+    // Recursively split and sort
+    mergesort(a, low, mid);
+    mergesort(a, mid + 1, high);
+
+    // Merge results
+    merge(a, low, mid, high);
 }
 
-// main fucntion
-int main()
-{
-	int a[7] = {83, 20, 9, 50, 115, 61, 17};
-	int n = 7;
+// Main function
+int main() {
+    int a[7] = {83, 20, 9, 50, 115, 61, 17}; // Input array
+    int n = 7; // Size of array
 
-	mergesort(a, 0, n-1);
-	
-	printf("\n Sorted numbers are: ");
+    // Perform merge sort
+    mergesort(a, 0, n-1);
 
-	// function to print the sorted array
-	int k;
-	for(k = 0; k < 7; k++)
-	    printf("%d, ",a[k]);
-	return 0;
+    printf("\nSorted numbers are: ");
+
+    // Function to print the sorted array
+    for (int k = 0; k < 7; k++)
+        printf("%d ", a[k]);
+
+    return 0;
 }
