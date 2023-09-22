e095b9461525b0f76f7461786aac86a49d9de6c0
jvirgo-vs <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-22 13:08:37 -0500

    Conductor code update based on ticket ID conductor/task/Batch_refactor_C_files/461_1695406113348

--- c/algorithms/bubblesort.c
+++ c/algorithms/bubblesort.c
@@ -1,10 +1,19 @@
+/* Filename: c/algorithms/bubblesort.c 
+ * Purpose: Program to perform bubble sort
+ */
+
 #include <stdio.h>
 
+/* bubble_sort: function that sorts an array using bubble sort method */
 void bubble_sort(int *array, int len) 
 {
+    /* Initial loop to iterate over array */
     for (int i = 0; i < len; i++) {
+        /* Inner loop to perform swapping operation */
         for (int j = 0; j < len - i - 1; j++) {
+            /* Check if current element is greater than next element */
             if (array[j] > array[j + 1]) {
+                /* Swap operation if current element is greater than next element */
                 int tmp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = tmp;
@@ -15,13 +24,17 @@ void bubble_sort(int *array, int len)
 
 int main(void) 
 {
+    /* Create and initialise array */
     int yarr[] = {3, 9, 4, 8, 7, 6, 1, 2, 0, 10};
     
+    /* Perform bubble sort on array */
     bubble_sort(yarr, 10);
 
+    /* Print out sorted array */
     printf("[");
     for (int i = 0; i < 10; i++)
-        printf("%d, ", yarr[i]);	
+        printf("%d, ", yarr[i]);    
     printf("\b\b]\n");
+    
     return 0;
 }
