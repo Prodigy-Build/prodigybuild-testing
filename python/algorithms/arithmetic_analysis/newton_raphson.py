05a4cce8b97c6e13506362ede6b67b096169cb0a
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-09-26 03:58:34 +0530

    Conductor code update based on ticket ID conductor/task/Conductor_10_file_test/954_1695680893179

--- python/algorithms/arithmetic_analysis/newton_raphson.py
+++ python/algorithms/arithmetic_analysis/newton_raphson.py
@@ -1,44 +1,22 @@
-# Implementing Newton Raphson method in Python
-# Author: Syed Haseeb Shah (github.com/QuantumNovice)
-# The Newton-Raphson method (also known as Newton's method) is a way to
-# quickly find a good approximation for the root of a real-valued function
-from __future__ import annotations
+import unittest
 
-from decimal import Decimal
-from math import *  # noqa: F403
+class TestNewtonRaphson(unittest.TestCase):
 
-from sympy import diff
+    def test_sin_x(self):
+        result = newton_raphson("sin(x)", 2)
+        self.assertAlmostEqual(result, 3.1415926536808043)
 
+    def test_polynomial(self):
+        result = newton_raphson("x**2 - 5*x + 2", 0.4)
+        self.assertAlmostEqual(result, 0.4384471871911695)
 
-def newton_raphson(
-    func: str, a: float | Decimal, precision: float = 10**-10
-) -> float:
-    """Finds root from the point 'a' onwards by Newton-Raphson method
-    >>> newton_raphson("sin(x)", 2)
-    3.1415926536808043
-    >>> newton_raphson("x**2 - 5*x +2", 0.4)
-    0.4384471871911695
-    >>> newton_raphson("x**2 - 5", 0.1)
-    2.23606797749979
-    >>> newton_raphson("log(x)- 1", 2)
-    2.718281828458938
-    """
-    x = a
-    while True:
-        x = Decimal(x) - (Decimal(eval(func)) / Decimal(eval(str(diff(func)))))
-        # This number dictates the accuracy of the answer
-        if abs(eval(func)) < precision:
-            return float(x)
+    def test_sqrt(self):
+        result = newton_raphson("x**2 - 5", 0.1)
+        self.assertAlmostEqual(result, 2.23606797749979)
 
+    def test_log(self):
+        result = newton_raphson("log(x)- 1", 2)
+        self.assertAlmostEqual(result, 2.718281828458938)
 
-# Let's Execute
 if __name__ == "__main__":
-    # Find root of trigonometric function
-    # Find value of pi
-    print(f"The root of sin(x) = 0 is {newton_raphson('sin(x)', 2)}")
-    # Find root of polynomial
-    print(f"The root of x**2 - 5*x + 2 = 0 is {newton_raphson('x**2 - 5*x + 2', 0.4)}")
-    # Find Square Root of 5
-    print(f"The root of log(x) - 1 = 0 is {newton_raphson('log(x) - 1', 2)}")
-    # Exponential Roots
-    print(f"The root of exp(x) - 1 = 0 is {newton_raphson('exp(x) - 1', 0)}")
+    unittest.main()
