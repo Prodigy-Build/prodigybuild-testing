0d287bad30ba8017953f8194e72c5657bd3d261a
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-09-26 03:48:13 +0530

    Conductor code update based on ticket ID conductor/task/Conductor_5_file_test/953_1695680283659

--- c/algorithms/count.c
+++ c/algorithms/count.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <assert.h>
+
+int count(int num);
+
+void test_count()
+{
+    assert(count(0) == 0);
+    assert(count(1) == 1);
+    assert(count(10) == 10);
+    assert(count(15) == 15);
+    assert(count(20) == 20);
+    assert(count(21) == 21);
+}
 
 int count(int num) 
 {
@@ -14,7 +27,8 @@ int count(int num)
             
         if(i == num + 1)
             printf("]");
-    } else
+    } 
+    else
     {
         int i;
         for(i = 0; i < num; i++)
@@ -23,6 +37,8 @@ int count(int num)
         if(i == num)
             printf("]");
     }
+    
+    return num;
 }
 
 int main()
@@ -31,4 +47,8 @@ int main()
     printf("Count:~$ ");
     scanf("%d", &num);
     count(num);
+    
+    test_count();
+    
+    return 0;
 }
