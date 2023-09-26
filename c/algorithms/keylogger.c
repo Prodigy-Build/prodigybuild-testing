1fc55a6ca913489e16ea84809d288cbabb6b0c8c
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-09-26 18:14:19 +0530

    Conductor code update based on ticket ID conductor/task/Test_issue_2/267_1695732256858

--- c/algorithms/keylogger.c
+++ c/algorithms/keylogger.c
@@ -1,27 +1,32 @@
+```c
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 int keylog()
 {
     FILE * fPtr;
-    fPtr = fopen("keylogger.txt", "w+");
+    fPtr = fopen("keylogger.txt", "a");
 
-    fopen("keylogger.txt", "w");
-    const char *a = getchar();
-    
+    const char *a = fgets(NULL, 1, stdin);
+
     if(a != NULL)
-        fprintf(fPtr, a);
-    
+        fprintf(fPtr, "%s", a);
+
     time_t now = time(NULL);
     struct tm *tm_struct = localtime(&now);
     int hour = tm_struct->tm_hour;
-    
+
     if(hour == 24)
         fclose(fPtr);
+    return 0;
 }
 
 int main()
 {
     keylog();
+
+    return 0;
 }
+```
