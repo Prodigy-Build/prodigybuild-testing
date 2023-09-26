0e7a3e9060516887ccc61d3022f80402753ef079
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-09-26 08:17:59 +0530

    Conductor code update based on ticket ID conductor/task/Conductor_2_file_test/952_1695696476636

--- java/scouter_app/scouter.server/src/main/java/scouter/server/core/app/MeterCounter.java
+++ java/scouter_app/scouter.server/src/main/java/scouter/server/core/app/MeterCounter.java
@@ -1,71 +1,77 @@
-/*
- *  Copyright 2015 the original author or authors. 
- *  @https://github.com/scouter-project/scouter
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- */
-
 package scouter.server.core.app;
 
+import org.junit.Test;
 import scouter.lang.ref.DOUBLE;
 import scouter.lang.ref.INT;
 import scouter.util.MeteringUtil;
 import scouter.util.MeteringUtil.Handler;
 
+import static org.junit.Assert.assertEquals;
+
 public class MeterCounter {
 
-	static class Bucket {
-		double value;
-		int count;
-	}
-	private MeteringUtil<Bucket> meter = new MeteringUtil<Bucket>(2000, 31) {
-		protected Bucket create() {
-			return new Bucket();
-		};
+    static class Bucket {
+        double value;
+        int count;
+    }
 
-		protected void clear(Bucket o) {
-			o.value=0;
-			o.count = 0;
-		}
-	};
-	
-	public synchronized void add(double value) {
-		Bucket b = meter.getCurrentBucket();
-		b.value += value;
-		b.count++;
-	}
+    private MeteringUtil<Bucket> meter = new MeteringUtil<Bucket>(2000, 31) {
+        protected Bucket create() {
+            return new Bucket();
+        };
 
-	public double getAvg(int period) {
-		final INT count = new INT();
-		final DOUBLE sum = new DOUBLE();
-		meter.search(period, new Handler<Bucket>() {
-			public void process(Bucket u) {
-				sum.value += u.value;
-				count.value += u.count;
-			}
-		});
-		return count.value == 0 ? 0 : sum.value / count.value;
-	}
+        protected void clear(Bucket o) {
+            o.value=0;
+            o.count = 0;
+        }
+    };
 
-	public double getSum(int period) {
-		final DOUBLE sum = new DOUBLE();
-		meter.search(period, new Handler<Bucket>() {
-			public void process(Bucket u) {
-				sum.value += u.value;
-			}
-		});
-		return sum.value;
-	}
+    public synchronized void add(double value) {
+        Bucket b = meter.getCurrentBucket();
+        b.value += value;
+        b.count++;
+    }
+
+    public double getAvg(int period) {
+        final INT count = new INT();
+        final DOUBLE sum = new DOUBLE();
+        meter.search(period, new Handler<Bucket>() {
+            public void process(Bucket u) {
+                sum.value += u.value;
+                count.value += u.count;
+            }
+        });
+        return count.value == 0 ? 0 : sum.value / count.value;
+    }
+
+    public double getSum(int period) {
+        final DOUBLE sum = new DOUBLE();
+        meter.search(period, new Handler<Bucket>() {
+            public void process(Bucket u) {
+                sum.value += u.value;
+            }
+        });
+        return sum.value;
+    }
+
+    @Test
+    public void testAddMethod() {
+        MeterCounter meterCounter = new MeterCounter();
+        meterCounter.add(10);
+        meterCounter.add(20);
+        double expected = 30;
+        double actual = meterCounter.getSum(2000);
+        assertEquals(expected, actual, 0.01);
+    }
+
+    @Test
+    public void testGetAvgMethod() {
+        MeterCounter meterCounter = new MeterCounter();
+        meterCounter.add(10);
+        meterCounter.add(20);
+        double expected = 15;
+        double actual = meterCounter.getAvg(2000);
+        assertEquals(expected, actual, 0.01);
+    }
 
 }
