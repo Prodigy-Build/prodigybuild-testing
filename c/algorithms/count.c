a1ca8409e7ea44b42824ebe38cfa6edb7e5ef832
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-09-26 08:16:50 +0530

    Conductor code update based on ticket ID conductor/task/Conductor_5_file_test/953_1695696398596

--- c/algorithms/count.c
+++ c/algorithms/count.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <assert.h>
 
 int count(int num) 
 {
@@ -25,8 +26,31 @@ int count(int num)
     }
 }
 
+// Unit Test 1
+void test1() {
+    count(10);
+    // Expected output: [0123456789]
+}
+
+// Unit Test 2
+void test2() {
+    count(26);
+    // Expected output: [0, 
+    //1, 
+    //2, 
+    //...
+    //23, 
+    //24, 
+    //25, 
+    //]
+}
+
 int main()
 {
+    // Run unit tests
+    test1();
+    test2();
+    
     int num;
     printf("Count:~$ ");
     scanf("%d", &num);
