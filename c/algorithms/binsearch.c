682c9fdcb6bc5ad5ae379f2f426c206cc2a0e3b4
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-09-26 08:21:16 +0530

    Conductor code update based on ticket ID conductor/task/Conductor_5_file_test/953_1695696673514

--- c/algorithms/binsearch.c
+++ c/algorithms/binsearch.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
 
-int binsearch(int yarr[10], int element)
+int binsearch(int yarr[], int size, int element)
 {
-    int mid = sizeof(yarr) % 2;
-    int left = (int)yarr / 2 - mid;
-    int right = yarr - left;
+    int mid = size / 2;
+    int left = mid - 1;
+    int right = mid + 1;
     extern int i;
-    
-    if(element == mid)
-    printf("%d", &mid);
-    
+
+    if(element == yarr[mid])
+        printf("%d", yarr[mid]);
+
     if(element != yarr[right])
     {
         for(i = 0; i < left; i++)
         {
-            if(element == i)
+            if(element == yarr[i])
             {
-            //Does this so that it doesn't print multiple times
-            printf("%d", i);
+                printf("%d", yarr[i]);
             }
         }
     }
-};
+}
 
-int main(int argc, int argv[])
+int main(int argc, int *argv[])
 {
-    binsearch(argv[0], argv[1]);
+    binsearch(argv[0], argc, argv[1]);
 }
