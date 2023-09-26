05664a149060eb86268a478f696affcb829f0fc2
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-09-26 09:34:38 +0530

    Conductor code update based on ticket ID conductor/task/Github_10_file_test/119_1695701076641

--- python/algorithms/audio_filters/butterworth_filter.py
+++ python/algorithms/audio_filters/butterworth_filter.py
@@ -2,25 +2,10 @@ from math import cos, sin, sqrt, tau
 
 from audio_filters.iir_filter import IIRFilter
 
-"""
-Create 2nd-order IIR filters with Butterworth design.
-
-Code based on https://webaudio.github.io/Audio-EQ-Cookbook/audio-eq-cookbook.html
-Alternatively you can use scipy.signal.butter, which should yield the same results.
-"""
-
 
 def make_lowpass(
     frequency: int, samplerate: int, q_factor: float = 1 / sqrt(2)  # noqa: B008
 ) -> IIRFilter:
-    """
-    Creates a low-pass filter
-
-    >>> filter = make_lowpass(1000, 48000)
-    >>> filter.a_coeffs + filter.b_coeffs  # doctest: +NORMALIZE_WHITESPACE
-    [1.0922959556412573, -1.9828897227476208, 0.9077040443587427, 0.004277569313094809,
-     0.008555138626189618, 0.004277569313094809]
-    """
     w0 = tau * frequency / samplerate
     _sin = sin(w0)
     _cos = cos(w0)
@@ -41,14 +26,6 @@ def make_lowpass(
 def make_highpass(
     frequency: int, samplerate: int, q_factor: float = 1 / sqrt(2)  # noqa: B008
 ) -> IIRFilter:
-    """
-    Creates a high-pass filter
-
-    >>> filter = make_highpass(1000, 48000)
-    >>> filter.a_coeffs + filter.b_coeffs  # doctest: +NORMALIZE_WHITESPACE
-    [1.0922959556412573, -1.9828897227476208, 0.9077040443587427, 0.9957224306869052,
-     -1.9914448613738105, 0.9957224306869052]
-    """
     w0 = tau * frequency / samplerate
     _sin = sin(w0)
     _cos = cos(w0)
@@ -69,14 +46,6 @@ def make_highpass(
 def make_bandpass(
     frequency: int, samplerate: int, q_factor: float = 1 / sqrt(2)  # noqa: B008
 ) -> IIRFilter:
-    """
-    Creates a band-pass filter
-
-    >>> filter = make_bandpass(1000, 48000)
-    >>> filter.a_coeffs + filter.b_coeffs  # doctest: +NORMALIZE_WHITESPACE
-    [1.0922959556412573, -1.9828897227476208, 0.9077040443587427, 0.06526309611002579,
-     0, -0.06526309611002579]
-    """
     w0 = tau * frequency / samplerate
     _sin = sin(w0)
     _cos = cos(w0)
@@ -98,14 +67,6 @@ def make_bandpass(
 def make_allpass(
     frequency: int, samplerate: int, q_factor: float = 1 / sqrt(2)  # noqa: B008
 ) -> IIRFilter:
-    """
-    Creates an all-pass filter
-
-    >>> filter = make_allpass(1000, 48000)
-    >>> filter.a_coeffs + filter.b_coeffs  # doctest: +NORMALIZE_WHITESPACE
-    [1.0922959556412573, -1.9828897227476208, 0.9077040443587427, 0.9077040443587427,
-     -1.9828897227476208, 1.0922959556412573]
-    """
     w0 = tau * frequency / samplerate
     _sin = sin(w0)
     _cos = cos(w0)
@@ -126,14 +87,6 @@ def make_peak(
     gain_db: float,
     q_factor: float = 1 / sqrt(2),  # noqa: B008
 ) -> IIRFilter:
-    """
-    Creates a peak filter
-
-    >>> filter = make_peak(1000, 48000, 6)
-    >>> filter.a_coeffs + filter.b_coeffs  # doctest: +NORMALIZE_WHITESPACE
-    [1.0653405327119334, -1.9828897227476208, 0.9346594672880666, 1.1303715025601122,
-     -1.9828897227476208, 0.8696284974398878]
-    """
     w0 = tau * frequency / samplerate
     _sin = sin(w0)
     _cos = cos(w0)
@@ -158,14 +111,6 @@ def make_lowshelf(
     gain_db: float,
     q_factor: float = 1 / sqrt(2),  # noqa: B008
 ) -> IIRFilter:
-    """
-    Creates a low-shelf filter
-
-    >>> filter = make_lowshelf(1000, 48000, 6)
-    >>> filter.a_coeffs + filter.b_coeffs  # doctest: +NORMALIZE_WHITESPACE
-    [3.0409336710888786, -5.608870992220748, 2.602157875636628, 3.139954022810743,
-     -5.591841778072785, 2.5201667380627257]
-    """
     w0 = tau * frequency / samplerate
     _sin = sin(w0)
     _cos = cos(w0)
@@ -195,14 +140,6 @@ def make_highshelf(
     gain_db: float,
     q_factor: float = 1 / sqrt(2),  # noqa: B008
 ) -> IIRFilter:
-    """
-    Creates a high-shelf filter
-
-    >>> filter = make_highshelf(1000, 48000, 6)
-    >>> filter.a_coeffs + filter.b_coeffs  # doctest: +NORMALIZE_WHITESPACE
-    [2.2229172136088806, -3.9587208137297303, 1.7841414181566304, 4.295432981120543,
-     -7.922740859457287, 3.6756456963725253]
-    """
     w0 = tau * frequency / samplerate
     _sin = sin(w0)
     _cos = cos(w0)
