6af5394903244f8cfbd9cd4049380f5cfc394928
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-09-26 08:21:24 +0530

    Conductor code update based on ticket ID conductor/task/Conductor_5_file_test/953_1695696673514

--- c/algorithms/count.c
+++ c/algorithms/count.c
@@ -4,25 +4,16 @@
 
 int count(int num) 
 {
-    printf("[");
-    
-    if(num > 20)
+    int count = 0;
+    int i;
+    for(i = 0; i < num; i++)
     {
-        int i;
-        for(i = 0; i < num; i++)
-            printf("\n%d, \n", i);
-            
-        if(i == num + 1)
-            printf("]");
-    } else
-    {
-        int i;
-        for(i = 0; i < num; i++)
-            printf("%d", i);
-            
-        if(i == num)
-            printf("]");
+        printf("%d", i);
+        count++;
     }
+    
+    if(count == num)
+        printf("]");
 }
 
 int main()
