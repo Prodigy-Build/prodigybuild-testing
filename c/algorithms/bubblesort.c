2b40763d1c82681267190cabe18d3a653d01314b
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-09-26 08:21:19 +0530

    Conductor code update based on ticket ID conductor/task/Conductor_5_file_test/953_1695696673514

--- c/algorithms/bubblesort.c
+++ c/algorithms/bubblesort.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <assert.h>
 
 void bubble_sort(int *array, int len) 
 {
@@ -13,15 +14,21 @@ void bubble_sort(int *array, int len)
     }
 }
 
+void test_bubble_sort() {
+    int array[] = {3, 9, 4, 8, 7, 6, 1, 2, 0, 10};
+    int expected[] = {0, 1, 2, 3, 4, 6, 7, 8, 9, 10};
+    int len = sizeof(array) / sizeof(array[0]);
+
+    bubble_sort(array, len);
+
+    for (int i = 0; i < len; i++) {
+        assert(array[i] == expected[i]);
+    }
+}
+
 int main(void) 
 {
-    int yarr[] = {3, 9, 4, 8, 7, 6, 1, 2, 0, 10};
-    
-    bubble_sort(yarr, 10);
+    test_bubble_sort();
 
-    printf("[");
-    for (int i = 0; i < 10; i++)
-        printf("%d, ", yarr[i]);	
-    printf("\b\b]\n");
     return 0;
 }
