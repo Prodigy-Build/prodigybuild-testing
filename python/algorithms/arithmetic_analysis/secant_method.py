e53470049865482b266f1d13883638a4bf08673a
swechter-prodigybuild <136387756+swechter-prodigybuild@users.noreply.github.com>
2023-09-27 18:19:22 -0500

    Committing multiple files via GitHub API

--- python/algorithms/arithmetic_analysis/secant_method.py
+++ python/algorithms/arithmetic_analysis/secant_method.py
@@ -27,3 +27,6 @@ def secant_method(lower_bound: float, upper_bound: float, repeats: int) -> float
 
 if __name__ == "__main__":
     print(f"Example: {secant_method(1, 3, 2)}")
+
+import doctest
+doctest.testmod()
