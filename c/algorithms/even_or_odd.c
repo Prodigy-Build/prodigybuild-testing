f366e5ae56d35073b546e84e4943b52d654e3d30
swechter-prodigybuild <136387756+swechter-prodigybuild@users.noreply.github.com>
2023-09-26 21:08:41 -0500

    Conductor code update based on ticket ID conductor/task/Github_test/5717_1695780510863

--- c/algorithms/even_or_odd.c
+++ c/algorithms/even_or_odd.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 
-int main()
-{
-    long num;
-    printf("Enter a number\n");
-    scanf("%ld", &num);
+int isEvenOrOdd(int num) {
     int rem = num % 2;
     
-    if(rem == 0)
-    printf("Even");
+    if (rem == 0)
+        return 1;
     else
-    printf("Odd");
+        return 0;
+}
+
+int main() {
+    int num;
+    printf("Enter a number\n");
+    scanf("%d", &num);
+    
+    if (isEvenOrOdd(num))
+        printf("Even");
+    else
+        printf("Odd");
+        
+    return 0;
 }
