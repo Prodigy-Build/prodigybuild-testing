a4c80d0a5231bdc23739d22ebc215a7bd439a348
swechter-prodigybuild <136387756+swechter-prodigybuild@users.noreply.github.com>
2023-09-26 21:11:06 -0500

    Conductor code update based on ticket ID conductor/task/Github_test/5717_1695780652875

---
+++ index.spec.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Index from './index';
+
+describe('Index', () => {
+  test('renders Index component', () => {
+    render(<Index />);
+  });
+});
