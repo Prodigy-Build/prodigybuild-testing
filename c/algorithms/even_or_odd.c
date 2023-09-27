0162f1224f4931f8cb07d4760e0da724fde0f734
swechter-prodigybuild <136387756+swechter-prodigybuild@users.noreply.github.com>
2023-09-26 21:17:34 -0500

    Conductor code update based on ticket ID conductor/task/Github_test/5717_1695781046735

--- c/algorithms/even_or_odd.c
+++ c/algorithms/even_or_odd.c
@@ -1,14 +1,40 @@
 #include <stdio.h>
+#include <assert.h>
 
-int main()
-{
-    long num;
-    printf("Enter a number\n");
-    scanf("%ld", &num);
+// Function to determine if a number is even or odd
+int isEvenOrOdd(long num) {
     int rem = num % 2;
     
     if(rem == 0)
-    printf("Even");
+        return 1; // Even
     else
-    printf("Odd");
+        return 0; // Odd
+}
+
+// Unit tests
+void testIsEvenOrOdd() {
+    assert(isEvenOrOdd(2) == 1); // Even number
+    assert(isEvenOrOdd(5) == 0); // Odd number
+    assert(isEvenOrOdd(0) == 1); // Even number
+    assert(isEvenOrOdd(-8) == 1); // Negative even number
+    assert(isEvenOrOdd(-11) == 0); // Negative odd number
+    assert(isEvenOrOdd(2.5) == 1); // Casting to long: Even number
+    assert(isEvenOrOdd(3.9) == 0); // Casting to long: Odd number
+}
+
+int main() {
+    // Running unit tests
+    testIsEvenOrOdd();
+    
+    long num;
+    printf("Enter a number\n");
+    scanf("%ld", &num);
+    
+    int isEven = isEvenOrOdd(num);
+    if (isEven)
+        printf("Even");
+    else
+        printf("Odd");
+        
+    return 0;
 }
