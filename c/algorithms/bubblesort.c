dc4544f316927b46db9c46d26fd47f5aec0d2189
swechter-prodigybuild <136387756+swechter-prodigybuild@users.noreply.github.com>
2023-09-26 21:10:56 -0500

    Conductor code update based on ticket ID conductor/task/Github_test/5717_1695780652875

--- c/algorithms/bubblesort.c
+++ c/algorithms/bubblesort.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <assert.h>
 
 void bubble_sort(int *array, int len) 
 {
@@ -13,15 +14,22 @@ void bubble_sort(int *array, int len)
     }
 }
 
-int main(void) 
+void test_bubble_sort()
 {
     int yarr[] = {3, 9, 4, 8, 7, 6, 1, 2, 0, 10};
+    int expected[] = {0, 1, 2, 3, 4, 6, 7, 8, 9, 10};
     
     bubble_sort(yarr, 10);
+    
+    for (int i = 0; i < 10; i++) {
+        assert(yarr[i] == expected[i]);
+    }
+}
 
-    printf("[");
-    for (int i = 0; i < 10; i++)
-        printf("%d, ", yarr[i]);	
-    printf("\b\b]\n");
+int main(void) 
+{
+    test_bubble_sort();
+    
+    printf("All tests passed!\n");
     return 0;
 }
