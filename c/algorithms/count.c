2fea181fb4e1e328e645f84f9f928f9503e3d79b
swechter-prodigybuild <136387756+swechter-prodigybuild@users.noreply.github.com>
2023-09-26 21:11:00 -0500

    Conductor code update based on ticket ID conductor/task/Github_test/5717_1695780652875

--- c/algorithms/count.c
+++ c/algorithms/count.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <assert.h>
 
 int count(int num) 
 {
@@ -25,10 +26,38 @@ int count(int num)
     }
 }
 
+void test_count()
+{
+    int num;
+    
+    // Test case 1
+    num = 5;
+    count(num);
+    // Expected output: [01234]
+    
+    // Test case 2
+    num = 30;
+    count(num);
+    // Expected output: 
+    // [
+    // 0, 
+    // 1, 
+    // 2, 
+    // ...
+    // 29, 
+    // 30
+    // ]
+}
+
 int main()
 {
     int num;
     printf("Count:~$ ");
     scanf("%d", &num);
     count(num);
+    
+    // Run unit tests
+    test_count();
+    
+    return 0;
 }
