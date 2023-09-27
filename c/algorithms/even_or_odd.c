942e5b7be0b3c620dc5bd1417ed4cc5ba868c584
swechter-prodigybuild <136387756+swechter-prodigybuild@users.noreply.github.com>
2023-09-26 21:11:04 -0500

    Conductor code update based on ticket ID conductor/task/Github_test/5717_1695780652875

--- c/algorithms/even_or_odd.c
+++ c/algorithms/even_or_odd.c
@@ -1,14 +1,47 @@
 #include <stdio.h>
+#include <assert.h>
+
+void test_even_or_odd()
+{
+    long num;
+    
+    num = 2;
+    assert(even_or_odd(num) == 0);
+    
+    num = 7;
+    assert(even_or_odd(num) == 1);
+    
+    num = -4;
+    assert(even_or_odd(num) == 0);
+    
+    num = -9;
+    assert(even_or_odd(num) == 1);
+    
+    printf("All test cases pass\n");
+}
+
+int even_or_odd(long num)
+{
+    int rem = num % 2;
+    
+    if(rem == 0)
+        return 0;
+    else
+        return 1;
+}
 
 int main()
 {
     long num;
     printf("Enter a number\n");
     scanf("%ld", &num);
-    int rem = num % 2;
     
-    if(rem == 0)
-    printf("Even");
+    if (even_or_odd(num) == 0)
+        printf("Even");
     else
-    printf("Odd");
+        printf("Odd");
+        
+    test_even_or_odd();
+    
+    return 0;
 }
