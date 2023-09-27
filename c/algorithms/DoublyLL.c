6bc9269f088f55b622fbab3857312b031efe4824
swechter-prodigybuild <136387756+swechter-prodigybuild@users.noreply.github.com>
2023-09-26 21:11:02 -0500

    Conductor code update based on ticket ID conductor/task/Github_test/5717_1695780652875

--- c/algorithms/DoublyLL.c
+++ c/algorithms/DoublyLL.c
@@ -1,63 +1,27 @@
-// Implementing Doubly linked list.
-#include <stdio.h>
-#include <stdlib.h>
+#include <assert.h>
 
-struct Node {
-    int value;
-    struct Node *next;
-    struct Node *prev;
-};
-struct Node *head;
+void testInsert() {
+    head = NULL;
 
-struct Node *CreateNode() {
-    struct Node *new = (struct Node*) malloc(sizeof(struct Node));
-    return new;
+    Insert(5);
+    assert(head != NULL);
+    assert(head->value == 5);
+    assert(head->next == NULL);
+    assert(head->prev == NULL);
+
+    Insert(10);
+    assert(head != NULL);
+    assert(head->value == 10);
+    assert(head->next != NULL);
+    assert(head->prev == NULL);
+    assert(head->next->value == 5);
 }
 
-void Insert(int val) { /*Inserting element at head*/
-    struct Node *NewNode = CreateNode(); /*NewNode is created everytime function is called*/
-    NewNode->value = val; /*Value assigned to NewNode*/
-    NewNode->next = head; /*NewNode's next points to head*/
-    NewNode->prev = NULL; /*NewNode's previous points to NULL*/
-    if (head != NULL) { 
-        head->prev = NewNode;
-    }
-    head = NewNode;
+void runTests() {
+    testInsert();
 }
 
-void Display() {
-    struct Node *temp = head;
-    printf("\nForward:\n"); /*Printing normally in forward manner*/
-    while(temp!=NULL) {
-        printf("%d ",temp->value);
-        temp = temp->next;
-    }
-}
-
-void ReverseDisplay() {
-    struct Node *temp = head;
-    while(temp->next!=NULL) { /*Moving to the last node*/
-        temp = temp->next;
-    }
-
-    printf("\nBackward:\n"); /*Printing in backward manner*/
-    while(temp!=NULL) {
-        printf("%d ",temp->value);
-        temp = temp->prev;
-    }
-    printf("\n");
-}
-
-void main() {
-    int n, val;
-    printf("Enter number of elements: ");
-    scanf("%d",&n);
-
-    for (int i=0; i<n; i++) {
-        printf("Enter element: ");
-        scanf("%d",&val);
-        Insert(val); /*Inserting value everytime loop executes*/
-    }
-    Display();
-    ReverseDisplay();
+int main() {
+    runTests();
+    return 0;
 }
