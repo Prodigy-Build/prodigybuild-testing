bfb3b00767626d126ad759b1f5becb37086e0351
Jerry Virgo <125318477+jvirgo-vs@users.noreply.github.com>
2023-09-27 16:25:56 -0500

    Conductor code update based on ticket ID conductor/task/Conductor_issue_with_repo/496_1695849952954

--- cpp/examples/AutomaticDifferentiation/main.cpp
+++ cpp/examples/AutomaticDifferentiation/main.cpp
@@ -1,4 +1,28 @@
-#include "../../lib/numericCppExamplesLib/dualNumbers.h"
+```cpp/examples/AutomaticDifferentiation/main.cpp``` updated code:
+
+```cpp
+#include <iostream>
+#include <cmath>
+
+double pow(double base, double exponent) {
+  return std::pow(base, exponent);
+}
+
+class Dual {
+public:
+  double real;
+  double dual;
+
+  Dual(double realValue, double dualValue) : real(realValue), dual(dualValue) {}
+
+  Dual operator+(const Dual& other) const {
+    return Dual(real + other.real, dual + other.dual);
+  }
+
+  Dual operator*(const Dual& other) const {
+    return Dual(real * other.real, real * other.dual + other.real * dual);
+  }
+};
 
 int main() {
   Dual x(2, 1);
@@ -6,3 +30,4 @@ int main() {
   std::cout << "Derivative of 5x^2+4x+1 at x = 2 is: " << f.dual << std::endl;
   return 0;
 }
+```
