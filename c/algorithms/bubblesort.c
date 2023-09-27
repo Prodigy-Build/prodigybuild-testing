4aff62abd94b0e1e9a3bd71e1723fbecd7ed2b0f
swechter-prodigybuild <136387756+swechter-prodigybuild@users.noreply.github.com>
2023-09-26 21:08:35 -0500

    Conductor code update based on ticket ID conductor/task/Github_test/5717_1695780510863

--- c/algorithms/bubblesort.c
+++ c/algorithms/bubblesort.c
@@ -1,4 +1,46 @@
+```
 #include <stdio.h>
+#include <assert.h>
+
+void bubble_sort(int *array, int len);
+
+void test_bubble_sort() {
+    int test1[] = {3, 9, 4, 8, 7, 6, 1, 2, 0, 10};
+    bubble_sort(test1, 10);
+    assert(test1[0] == 0);
+    assert(test1[1] == 1);
+    assert(test1[2] == 2);
+    assert(test1[3] == 3);
+    assert(test1[4] == 4);
+    assert(test1[5] == 6);
+    assert(test1[6] == 7);
+    assert(test1[7] == 8);
+    assert(test1[8] == 9);
+    assert(test1[9] == 10);
+
+    int test2[] = {5, 4, 3, 2, 1};
+    bubble_sort(test2, 5);
+    assert(test2[0] == 1);
+    assert(test2[1] == 2);
+    assert(test2[2] == 3);
+    assert(test2[3] == 4);
+    assert(test2[4] == 5);
+}
+
+int main(void) 
+{
+    test_bubble_sort();
+
+    int yarr[] = {3, 9, 4, 8, 7, 6, 1, 2, 0, 10};
+    
+    bubble_sort(yarr, 10);
+
+    printf("[");
+    for (int i = 0; i < 10; i++)
+        printf("%d, ", yarr[i]);	
+    printf("\b\b]\n");
+    return 0;
+}
 
 void bubble_sort(int *array, int len) 
 {
@@ -12,16 +54,4 @@ void bubble_sort(int *array, int len)
         }
     }
 }
-
-int main(void) 
-{
-    int yarr[] = {3, 9, 4, 8, 7, 6, 1, 2, 0, 10};
-    
-    bubble_sort(yarr, 10);
-
-    printf("[");
-    for (int i = 0; i < 10; i++)
-        printf("%d, ", yarr[i]);	
-    printf("\b\b]\n");
-    return 0;
-}
+```
