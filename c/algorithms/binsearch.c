8b686008155141614039523d461570f32346686a
swechter-prodigybuild <136387756+swechter-prodigybuild@users.noreply.github.com>
2023-09-28 11:53:20 -0500

    Conductor code update based on ticket ID conductor/task/Conductor_5_file_test/953_1695919998004

--- c/algorithms/binsearch.c
+++ c/algorithms/binsearch.c
@@ -1,29 +1,63 @@
 #include <stdio.h>
+#include <assert.h>
 
 int binsearch(int yarr[10], int element)
 {
-    int mid = sizeof(yarr) % 2;
-    int left = (int)yarr / 2 - mid;
-    int right = yarr - left;
-    extern int i;
-    
-    if(element == mid)
-    printf("%d", &mid);
-    
-    if(element != yarr[right])
+    int mid = sizeof(yarr) / sizeof(yarr[0]) / 2;
+    int left = mid;
+    int right = 10 - mid;
+    int i;
+
+    if (element == yarr[mid])
     {
-        for(i = 0; i < left; i++)
+        printf("%d", yarr[mid]);
+    }
+    
+    if (element != yarr[right])
+    {
+        for (i = 0; i < left; i++)
         {
-            if(element == i)
+            if (element == yarr[i])
             {
-            //Does this so that it doesn't print multiple times
-            printf("%d", i);
+                //Does this so that it doesn't print multiple times
+                printf("%d", yarr[i]);
             }
         }
     }
-};
+}
 
-int main(int argc, int argv[])
+int main(int argc, char *argv[])
 {
-    binsearch(argv[0], argv[1]);
+    int yarr[10];
+    int element;
+
+    // Test 1
+    yarr[0] = 1;
+    yarr[1] = 2;
+    yarr[2] = 3;
+    yarr[3] = 4;
+    yarr[4] = 5;
+    yarr[5] = 6;
+    yarr[6] = 7;
+    yarr[7] = 8;
+    yarr[8] = 9;
+    yarr[9] = 10;
+    element = 5;
+    binsearch(yarr, element);
+
+    // Test 2
+    yarr[0] = 1;
+    yarr[1] = 2;
+    yarr[2] = 3;
+    yarr[3] = 4;
+    yarr[4] = 5;
+    yarr[5] = 6;
+    yarr[6] = 7;
+    yarr[7] = 8;
+    yarr[8] = 9;
+    yarr[9] = 10;
+    element = 11;
+    binsearch(yarr, element);
+
+    return 0;
 }
