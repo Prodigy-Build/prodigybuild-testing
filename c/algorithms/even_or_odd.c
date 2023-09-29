5d4dc0cb6cc16b3ec5992517e3a62c8c266d51c8
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-09-29 17:30:09 +0530

    Conductor code update based on ticket ID conductor/task/testtttttttt/2209_1695988793874

--- c/algorithms/even_or_odd.c
+++ c/algorithms/even_or_odd.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int main()
-{
-    long num;
-    printf("Enter a number\n");
-    scanf("%ld", &num);
-    int rem = num % 2;
-    
-    if(rem == 0)
-    printf("Even");
+// Function to check if a number is even or odd
+bool isEven(int num) {
+    return num % 2 == 0;
+}
+
+// Unit test cases
+int main() {
+    // Test case 1: Even number
+    int num1 = 4;
+    if (isEven(num1))
+        printf("Test case 1 passed\n");
     else
-    printf("Odd");
+        printf("Test case 1 failed\n");
+
+    // Test case 2: Odd number
+    int num2 = 3;
+    if (!isEven(num2))
+        printf("Test case 2 passed\n");
+    else
+        printf("Test case 2 failed\n");
+
+    // Test case 3: Zero
+    int num3 = 0;
+    if (isEven(num3))
+        printf("Test case 3 passed\n");
+    else
+        printf("Test case 3 failed\n");
+
+    return 0;
 }
