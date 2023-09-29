2fbad6f62228e44dc7a69b008e0e5a4046165e23
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-09-29 17:30:11 +0530

    Conductor code update based on ticket ID conductor/task/testtttttttt/2209_1695988793874

--- c/algorithms/fac.c
+++ c/algorithms/fac.c
@@ -11,14 +11,34 @@ int fak(int n) {
     return n * fak(n-1);
 }
 
+int test_fak() {
+    if (fak(1) != 1)
+        return 0;
+    if (fak(2) != 2)
+        return 0;
+    if (fak(3) != 6)
+        return 0;
+    if (fak(4) != 24)
+        return 0;
+    if (fak(5) != 120)
+        return 0;
+    if (fak(-1) != 1)
+        return 0;
+    return 1;
+}
+
 int main() {
     printf("fak(1): %d\n", fak(1));
     printf("fak(2): %d\n", fak(2));
     printf("fak(3): %d\n", fak(3));
     printf("fak(4): %d\n", fak(4));
     printf("fak(5): %d\n", fak(5));
-    
     printf("fak(-1): %d\n", fak(-1));
+
+    if (test_fak())
+        printf("All test cases pass\n");
+    else
+        printf("Test cases failed\n");
 
     return 1;
 }
