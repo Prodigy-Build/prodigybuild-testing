5eefcf8f1b579bc0a77a321b4017d0671dd70aef
swechter-prodigybuild <136387756+swechter-prodigybuild@users.noreply.github.com>
2023-09-29 13:10:24 -0500

    Conductor code update based on ticket ID conductor/task/Conductor_10_file_test/954_1696011001115

---
+++ python/algorithms/arithmetic_analysis/newton_raphson.test.py
@@ -0,0 +1,10 @@
+import pytest
+
+from arithmetic_analysis.newton_raphson import newton_raphson
+
+
+def test_newton_raphson():
+    assert newton_raphson("sin(x)", 2) == 3.1415926536808043
+    assert newton_raphson("x**2 - 5*x +2", 0.4) == 0.4384471871911695
+    assert newton_raphson("x**2 - 5", 0.1) == 2.23606797749979
+    assert newton_raphson("log(x)- 1", 2) == 2.718281828458938
