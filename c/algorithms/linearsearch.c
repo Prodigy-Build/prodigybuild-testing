ca83875326d0ea8d2c44de4ed945399f494147d1
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-09-30 00:18:43 +0530

    Conductor code update based on ticket ID conductor/task/test/2219_1696013296549

--- c/algorithms/linearsearch.c
+++ c/algorithms/linearsearch.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int linsearch(int *yarr, int val)
+bool linsearch(int *yarr, int val, int size)
 {
-    for(int i = 0; i < sizeof(yarr); i++)
+    for(int i = 0; i < size; i++)
     {
         if(yarr[i] == val)
         {
-            return 0;
+            return true;
         }    
-        else
-        {
-          return 1;
-        }
     }
+    return false;
 }
 
 int main()
 {
-    int *yarr = {"Shluck much", "much Shluck"};
-    int val = "Shluck Much";
-    linsearch(yarr, val);
+    int yarr[] = {1, 2, 3, 4, 5};
+    int val = 3;
+    int size = sizeof(yarr) / sizeof(yarr[0]);
+    linsearch(yarr, val, size);
 }
