30a59ef1fcdf527f6f6616e323e02414bcb5af16
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-09-29 17:55:10 +0530

    Conductor code update based on ticket ID conductor/task/testtttttttt/2209_1695990295929

--- c/algorithms/even_or_odd.c
+++ c/algorithms/even_or_odd.c
@@ -1,14 +1,52 @@
 #include <stdio.h>
+#include <assert.h>
+
+void testIsEven()
+{
+    long num1 = 4;
+    long num2 = -6;
+    long num3 = 0;
+
+    assert(isEven(num1) == 1);
+    assert(isEven(num2) == 1);
+    assert(isEven(num3) == 1);
+}
+
+void testIsOdd()
+{
+    long num1 = 3;
+    long num2 = -5;
+    long num3 = 7;
+
+    assert(isOdd(num1) == 1);
+    assert(isOdd(num2) == 1);
+    assert(isOdd(num3) == 1);
+}
+
+int isEven(long num)
+{
+    return (num % 2 == 0);
+}
+
+int isOdd(long num)
+{
+    return (num % 2 != 0);
+}
 
 int main()
 {
     long num;
     printf("Enter a number\n");
     scanf("%ld", &num);
-    int rem = num % 2;
-    
-    if(rem == 0)
-    printf("Even");
+
+    if(isEven(num))
+        printf("Even");
     else
-    printf("Odd");
+        printf("Odd");
+
+    // Run unit tests
+    testIsEven();
+    testIsOdd();
+
+    return 0;
 }
