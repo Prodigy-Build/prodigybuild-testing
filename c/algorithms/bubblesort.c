c5e65bd58f718df55bdc993c3e177ed1d69d580c
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-09-29 17:55:01 +0530

    Conductor code update based on ticket ID conductor/task/testtttttttt/2209_1695990295929

--- c/algorithms/bubblesort.c
+++ c/algorithms/bubblesort.c
@@ -1,7 +1,8 @@
+```c/algorithms/bubblesort.c
 #include <stdio.h>
+#include <assert.h>
 
-void bubble_sort(int *array, int len) 
-{
+void bubble_sort(int *array, int len) {
     for (int i = 0; i < len; i++) {
         for (int j = 0; j < len - i - 1; j++) {
             if (array[j] > array[j + 1]) {
@@ -13,15 +14,31 @@ void bubble_sort(int *array, int len)
     }
 }
 
-int main(void) 
-{
-    int yarr[] = {3, 9, 4, 8, 7, 6, 1, 2, 0, 10};
-    
-    bubble_sort(yarr, 10);
+void test_bubble_sort() {
+    int arr1[] = {3, 9, 4, 8, 7, 6, 1, 2, 0, 10};
+    int sorted1[] = {0, 1, 2, 3, 4, 6, 7, 8, 9, 10};
+    bubble_sort(arr1, 10);
+    for (int i = 0; i < 10; i++) {
+        assert(arr1[i] == sorted1[i]);
+    }
 
-    printf("[");
-    for (int i = 0; i < 10; i++)
-        printf("%d, ", yarr[i]);	
-    printf("\b\b]\n");
+    int arr2[] = {5, 4, 3, 2, 1};
+    int sorted2[] = {1, 2, 3, 4, 5};
+    bubble_sort(arr2, 5);
+    for (int i = 0; i < 5; i++) {
+        assert(arr2[i] == sorted2[i]);
+    }
+
+    int arr3[] = {1, 2, 3, 4, 5};
+    int sorted3[] = {1, 2, 3, 4, 5};
+    bubble_sort(arr3, 5);
+    for (int i = 0; i < 5; i++) {
+        assert(arr3[i] == sorted3[i]);
+    }
+}
+
+int main(void) {
+    test_bubble_sort();
     return 0;
 }
+```
