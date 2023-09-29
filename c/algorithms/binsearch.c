fe36949d649c114c84001a69d104c854d12f087b
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-09-29 17:29:56 +0530

    Conductor code update based on ticket ID conductor/task/testtttttttt/2209_1695988793874

--- c/algorithms/binsearch.c
+++ c/algorithms/binsearch.c
@@ -1,29 +1,51 @@
+```c
 #include <stdio.h>
+#include <stdbool.h>
 
-int binsearch(int yarr[10], int element)
+int binsearch(int yarr[], int size, int element)
 {
-    int mid = sizeof(yarr) % 2;
-    int left = (int)yarr / 2 - mid;
-    int right = yarr - left;
-    extern int i;
-    
-    if(element == mid)
-    printf("%d", &mid);
-    
-    if(element != yarr[right])
-    {
-        for(i = 0; i < left; i++)
-        {
-            if(element == i)
-            {
-            //Does this so that it doesn't print multiple times
-            printf("%d", i);
-            }
-        }
+    int left = 0;
+    int right = size - 1;
+    int mid;
+
+    while (left <= right) {
+        mid = left + (right - left) / 2;
+
+        if (yarr[mid] == element)
+            return mid;
+
+        if (yarr[mid] < element)
+            left = mid + 1;
+
+        else
+            right = mid - 1;
     }
-};
 
-int main(int argc, int argv[])
-{
-    binsearch(argv[0], argv[1]);
+    return -1;
 }
+
+void test_binsearch() {
+    int yarr[] = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
+
+    if(binsearch(yarr, 10, 23) != 5)
+        printf("Test 1 failed");
+
+    if(binsearch(yarr, 10, 8) != 2)
+        printf("Test 2 failed");
+
+    if(binsearch(yarr, 10, 91) != 9)
+        printf("Test 3 failed");
+
+    if(binsearch(yarr, 10, 1) != -1)
+        printf("Test 4 failed");
+
+    printf("All tests passed");
+}
+
+int main(int argc, char* argv[])
+{
+    test_binsearch();
+
+    return 0;
+}
+```
