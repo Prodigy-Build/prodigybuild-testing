f4df293c684cabd9f8b63cf7f82ca6cbe76b56b3
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-10-03 17:26:57 +0530

    PB-2 (task) Issue 2

--- c/algorithms/BST.c
+++ c/algorithms/BST.c
@@ -1,6 +1,6 @@
-// Implementation of Binary Search Tree 
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 struct BST {
     int data;
@@ -13,38 +13,51 @@ struct BST *CreateNode() {
     new->left = NULL;
     new->right = NULL;
     return new; 
-};
+}
 
 void Insert(struct BST** RootPtr, int value) {
     struct BST* temp = *RootPtr;
-    if (temp == NULL) { /*When list is empty*/
+    if (temp == NULL) { 
         struct BST* NewNode = CreateNode();
         NewNode->data = value;
         *RootPtr = NewNode;
-    } else if (value <= temp->data) { /*If user value is less then current node value insert in left of the node...*/
+    } else if (value <= temp->data) { 
         struct BST* NewNode = CreateNode();
         NewNode->data = value;
         temp->left = NewNode;
-    } else { /*If user value is greater then current node value insert at right of the node*/
+    } else { 
         struct BST* NewNode = CreateNode();
         NewNode->data = value;
         temp->right = NewNode;
     }
 }
 
-int Search(struct BST* RootPtr, int item) { /*Implemented search using recursion*/
-    if(RootPtr == NULL) {
-        return 0; /*Returns 0 if list is empty*/
-    } else if(item == RootPtr->data) {
-        return 1; /*Returns 1 when element found*/
-    } else if(item < RootPtr->data) {
-        Search(RootPtr->left, item); /*Otherwise search in left side of binary tree if searching value is less then the current node value*/
+int Search(struct BST* RootPtr, int item) { 
+    if (RootPtr == NULL) {
+        return 0; 
+    } else if (item == RootPtr->data) {
+        return 1; 
+    } else if (item < RootPtr->data) {
+        return Search(RootPtr->left, item); 
     } else {
-        Search(RootPtr->right, item); /*Otherwise search in right side of binary tree if searching value is greater then the current node value*/
+        return Search(RootPtr->right, item); 
     }
 }
 
-void main() {
+void test() {
+    struct BST* RootPtr = NULL;
+    Insert(&RootPtr, 5);
+    Insert(&RootPtr, 3);
+    Insert(&RootPtr, 8);
+    
+    assert(Search(RootPtr, 5) == 1);
+    assert(Search(RootPtr, 3) == 1);
+    assert(Search(RootPtr, 8) == 1);
+    assert(Search(RootPtr, 10) == 0);
+    assert(Search(RootPtr, 2) == 0);
+}
+
+int main() {
     struct BST* RootPtr = NULL;
     int item, cont, key;
     do {
@@ -64,4 +77,8 @@ void main() {
     } else {
         printf("\nNot Found\n");
     }
+
+    test();
+
+    return 0;
 }
--- c/algorithms/bubblesort.c
+++ c/algorithms/bubblesort.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <assert.h>
 
 void bubble_sort(int *array, int len) 
 {
@@ -13,8 +14,33 @@ void bubble_sort(int *array, int len)
     }
 }
 
+void test_bubble_sort() 
+{
+    int arr[] = {3, 9, 4, 8, 7};
+    bubble_sort(arr, 5);
+    assert(arr[0] == 3);
+    assert(arr[1] == 4);
+    assert(arr[2] == 7);
+    assert(arr[3] == 8);
+    assert(arr[4] == 9);
+    
+    int arr2[] = {5};
+    bubble_sort(arr2, 1);
+    assert(arr2[0] == 5);
+    
+    int arr3[] = {1, 2, 3, 4, 5};
+    bubble_sort(arr3, 5);
+    assert(arr3[0] == 1);
+    assert(arr3[1] == 2);
+    assert(arr3[2] == 3);
+    assert(arr3[3] == 4);
+    assert(arr3[4] == 5);
+}
+
 int main(void) 
 {
+    test_bubble_sort();
+    
     int yarr[] = {3, 9, 4, 8, 7, 6, 1, 2, 0, 10};
     
     bubble_sort(yarr, 10);
