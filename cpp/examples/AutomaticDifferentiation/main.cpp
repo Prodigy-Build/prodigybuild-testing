a32adf9b974150ba6229d49fc2d922133faad485
swechter-prodigybuild <136387756+swechter-prodigybuild@users.noreply.github.com>
2023-10-05 22:08:46 -0500

    PB-15 (task) Conductor 1 file test

--- cpp/examples/AutomaticDifferentiation/main.cpp
+++ cpp/examples/AutomaticDifferentiation/main.cpp
@@ -1,8 +1,17 @@
 #include "../../lib/numericCppExamplesLib/dualNumbers.h"
+#include <cassert>
 
-int main() {
+void test_automatic_differentiation() {
   Dual x(2, 1);
   Dual f = 5 * pow(x, 2) + 4 * x + 1;
-  std::cout << "Derivative of 5x^2+4x+1 at x = 2 is: " << f.dual << std::endl;
+
+  assert(f.real == 21); // Check the real part of f
+  assert(f.dual == 14); // Check the dual part of f
+}
+
+int main() {
+  test_automatic_differentiation();
+
+  std::cout << "All tests passed!" << std::endl;
   return 0;
 }
