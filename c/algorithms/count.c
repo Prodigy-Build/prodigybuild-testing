00a796b8da0bf58e992c328887f9ec07ebe8a10d
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-10-06 17:45:06 +0530

    PB-2 (task) Issue 2

--- c/algorithms/count.c
+++ c/algorithms/count.c
@@ -1,28 +1,33 @@
+```c
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 
 int count(int num) 
 {
-    printf("[");
+    int countArray[num];
     
     if(num > 20)
     {
-        int i;
-        for(i = 0; i < num; i++)
-            printf("\n%d, \n", i);
-            
-        if(i == num + 1)
-            printf("]");
-    } else
+        for(int i = 0; i < num; i++)
+        {
+            countArray[i] = i;
+        }
+    } 
+    else
     {
-        int i;
-        for(i = 0; i < num; i++)
-            printf("%d", i);
-            
-        if(i == num)
-            printf("]");
+        for(int i = 0; i < num; i++)
+        {
+            countArray[i] = i;
+        }
     }
+    
+    printf("[");
+    for(int i = 0; i < num; i++)
+    {
+        printf("%d, ", countArray[i]);
+    }
+    printf("]");
 }
 
 int main()
@@ -32,3 +37,42 @@ int main()
     scanf("%d", &num);
     count(num);
 }
+```
+Unit Test Cases:
+```c
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+void testCountFunction()
+{
+    // Test case 1
+    printf("Test case 1:\n");
+    printf("Expected Output: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]\n");
+    printf("Actual Output: ");
+    count(10);
+    printf("\n\n");
+    
+    // Test case 2
+    printf("Test case 2:\n");
+    printf("Expected Output: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]\n");
+    printf("Actual Output: ");
+    count(21);
+    printf("\n\n");
+    
+    // Test case 3
+    printf("Test case 3:\n");
+    printf("Expected Output: []\n");
+    printf("Actual Output: ");
+    count(0);
+    printf("\n\n");
+}
+
+int main()
+{
+    // Run unit test cases
+    testCountFunction();
+    
+    return 0;
+}
+```
