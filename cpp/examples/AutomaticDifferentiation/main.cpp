ee20b4ce6b08a093e0ba0516e79c7ccc3cf551ac
swechter-prodigybuild <136387756+swechter-prodigybuild@users.noreply.github.com>
2023-10-05 22:07:22 -0500

    PB-15 (task) Conductor 1 file test

--- cpp/examples/AutomaticDifferentiation/main.cpp
+++ cpp/examples/AutomaticDifferentiation/main.cpp
@@ -1,8 +1,13 @@
 #include "../../lib/numericCppExamplesLib/dualNumbers.h"
+#include <gtest/gtest.h>
 
-int main() {
+TEST(AutomaticDifferentiation, TestDerivativeCalculation) {
   Dual x(2, 1);
   Dual f = 5 * pow(x, 2) + 4 * x + 1;
-  std::cout << "Derivative of 5x^2+4x+1 at x = 2 is: " << f.dual << std::endl;
-  return 0;
+  ASSERT_EQ(f.dual, 14);
+}
+
+int main(int argc, char** argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
