24677097ffbcdc72eec0b11b7382d15e55fdc615
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-10-06 18:29:24 +0530

    PB-1 (task) Issue 1

--- c/algorithms/binsearch.c
+++ c/algorithms/binsearch.c
@@ -27,3 +27,25 @@ int main(int argc, int argv[])
 {
     binsearch(argv[0], argv[1]);
 }
+
+/* Add unit test cases here */
+
+/* Test case 1:
+   Input: yarr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, element = 5
+   Expected output: 4
+*/
+
+/* Test case 2:
+   Input: yarr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, element = 10
+   Expected output: 9
+*/
+
+/* Test case 3:
+   Input: yarr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, element = 2
+   Expected output: 1
+*/
+
+/* Test case 4:
+   Input: yarr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, element = 15
+   Expected output: No output
+*/
