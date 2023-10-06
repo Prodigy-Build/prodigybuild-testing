176e4578182a7d542b575729ecb6e512c362c0e3
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-10-06 18:34:00 +0530

    PB-2 (task) Issue 2

--- c/algorithms/count.c
+++ c/algorithms/count.c
@@ -32,3 +32,26 @@ int main()
     scanf("%d", &num);
     count(num);
 }
+
+// Unit test cases
+
+void testCount()
+{
+    // Test case 1
+    int num1 = 10;
+    printf("Expected output for num1: [0123456789]\n");
+    printf("Actual output for num1: ");
+    count(num1);
+
+    // Test case 2
+    int num2 = 25;
+    printf("Expected output for num2: \n0, \n1, \n2, \n3, \n4, \n5, \n6, \n7, \n8, \n9, \n10, \n11, \n12, \n13, \n14, \n15, \n16, \n17, \n18, \n19, \n20, \n21, \n22, \n23, \n24, \n25\n]\n");
+    printf("Actual output for num2: ");
+    count(num2);
+}
+
+int main()
+{
+    testCount();
+    return 0;
+}
