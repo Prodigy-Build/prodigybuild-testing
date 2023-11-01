c89b133d16462ddcf57aaeb5f10ba012a21e6718
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-11-01 18:28:14 +0530

    PB-2 (task) Create Billing Details page

--- c/algorithms/binsearch.c
+++ c/algorithms/binsearch.c
@@ -1,29 +1,45 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int binsearch(int yarr[10], int element)
+int binsearch(int yarr[], int size, int element)
 {
-    int mid = sizeof(yarr) % 2;
-    int left = (int)yarr / 2 - mid;
-    int right = yarr - left;
-    extern int i;
-    
-    if(element == mid)
-    printf("%d", &mid);
-    
-    if(element != yarr[right])
-    {
-        for(i = 0; i < left; i++)
-        {
-            if(element == i)
-            {
-            //Does this so that it doesn't print multiple times
-            printf("%d", i);
-            }
+    int left = 0;
+    int right = size - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        
+        if (yarr[mid] == element) {
+            return mid;
+        }
+        
+        if (yarr[mid] < element) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         }
     }
-};
+    
+    return -1;
+}
 
-int main(int argc, int argv[])
+// Unit test cases
+int main()
 {
-    binsearch(argv[0], argv[1]);
+    int arr[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    
+    // Test case 1: Finding an existing element (10) in the array
+    int element1 = 10;
+    int index1 = binsearch(arr, size, element1);
+    
+    // Test case 2: Finding a non-existing element (7) in the array
+    int element2 = 7;
+    int index2 = binsearch(arr, size, element2);
+    
+    // Printing the results
+    printf("Test case 1: Element %d found at index %d\n", element1, index1);
+    printf("Test case 2: Element %d not found\n", element2);
+    
+    return 0;
 }
