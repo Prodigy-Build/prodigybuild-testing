8a316fa03d832f7b486376eecbb6debff26fe7dc
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-11-01 17:25:11 +0530

    PB-2 (task) Create Billing Details page

--- c/algorithms/binsearch.c
+++ c/algorithms/binsearch.c
@@ -1,29 +1,40 @@
 #include <stdio.h>
 
-int binsearch(int yarr[10], int element)
+// Function declaration
+int binsearch(int yarr[], int element);
+
+int main(int argc, char* argv[])
 {
-    int mid = sizeof(yarr) % 2;
-    int left = (int)yarr / 2 - mid;
-    int right = yarr - left;
-    extern int i;
-    
-    if(element == mid)
-    printf("%d", &mid);
-    
-    if(element != yarr[right])
+    int yarr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int element = 5;
+
+    // Function call
+    binsearch(yarr, element);
+
+    return 0;
+}
+
+// Function definition
+int binsearch(int yarr[], int element)
+{
+    int mid = sizeof(yarr) / sizeof(yarr[0]) / 2;
+    int left = mid;
+    int right = sizeof(yarr) / sizeof(yarr[0]);
+    int i;
+
+    if (element == yarr[mid])
     {
-        for(i = 0; i < left; i++)
+        printf("%d", &mid);
+    }
+    
+    if (element != yarr[right])
+    {
+        for (i = 0; i < left; i++)
         {
-            if(element == i)
+            if (element == yarr[i])
             {
-            //Does this so that it doesn't print multiple times
-            printf("%d", i);
+                printf("%d", i);
             }
         }
     }
-};
-
-int main(int argc, int argv[])
-{
-    binsearch(argv[0], argv[1]);
 }
