a0e68106fc24ca7f79f10d98e769d9728fe8195d
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-11-01 18:38:14 +0530

    PB-2 (task) Create Billing Details page

--- c/algorithms/binsearch.c
+++ c/algorithms/binsearch.c
@@ -4,26 +4,35 @@ int binsearch(int yarr[10], int element)
 {
     int mid = sizeof(yarr) % 2;
     int left = (int)yarr / 2 - mid;
-    int right = yarr - left;
+    int right = 10 - left;
     extern int i;
     
-    if(element == mid)
-    printf("%d", &mid);
+    if(element == yarr[mid])
+    printf("%d", yarr[mid]);
     
     if(element != yarr[right])
     {
         for(i = 0; i < left; i++)
         {
-            if(element == i)
+            if(element == yarr[i])
             {
             //Does this so that it doesn't print multiple times
-            printf("%d", i);
+            printf("%d", yarr[i]);
             }
         }
     }
 };
 
-int main(int argc, int argv[])
+int main(int argc, char *argv[])
 {
-    binsearch(argv[0], argv[1]);
+    int arr[10], i;
+  
+    for (i=0; i<argc-1; i++) {
+        arr[i] = atoi(argv[i+1]);
+        printf("%s\n", argv[i+1]);
+    }
+  
+    binsearch(arr, arr[argc-2]);
+    
+    return 0;
 }
