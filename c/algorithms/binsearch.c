2d4f333327e980f2e3affc7593cd2ad7a26ac695
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-11-01 18:36:11 +0530

    PB-2 (task) Create Billing Details page

--- c/algorithms/binsearch.c
+++ c/algorithms/binsearch.c
@@ -1,29 +1,44 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int binsearch(int yarr[10], int element)
+int binsearch(int yarr[], int size, int element)
 {
-    int mid = sizeof(yarr) % 2;
-    int left = (int)yarr / 2 - mid;
-    int right = yarr - left;
-    extern int i;
+    int left = 0;
+    int right = size - 1;
     
-    if(element == mid)
-    printf("%d", &mid);
-    
-    if(element != yarr[right])
-    {
-        for(i = 0; i < left; i++)
-        {
-            if(element == i)
-            {
-            //Does this so that it doesn't print multiple times
-            printf("%d", i);
-            }
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        
+        if (yarr[mid] == element) {
+            return mid;
+        }
+        
+        if (yarr[mid] < element) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         }
     }
-};
+    
+    return -1;
+}
 
-int main(int argc, int argv[])
-{
-    binsearch(argv[0], argv[1]);
+// Unit Test Cases
+void test() {
+    int arr[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    
+    // Element present in the array
+    int index = binsearch(arr, size, 12);
+    printf("Index of element 12: %d\n", index);
+    
+    // Element not present in the array
+    index = binsearch(arr, size, 5);
+    printf("Index of element 5: %d\n", index);
+}
+
+int main() {
+    test();
+    
+    return 0;
 }
