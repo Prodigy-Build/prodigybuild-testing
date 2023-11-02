e2c9323990770e4b431731f42e88a338ba1ffb0f
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-11-02 17:17:05 +0530

    PB-3 (task) Issue 2

--- c/algorithms/binsearch.c
+++ c/algorithms/binsearch.c
@@ -8,7 +8,7 @@ int binsearch(int yarr[10], int element)
     extern int i;
     
     if(element == mid)
-    printf("%d", &mid);
+        printf("%d", &mid);
     
     if(element != yarr[right])
     {
@@ -16,14 +16,41 @@ int binsearch(int yarr[10], int element)
         {
             if(element == i)
             {
-            //Does this so that it doesn't print multiple times
-            printf("%d", i);
+                //Does this so that it doesn't print multiple times
+                printf("%d", i);
             }
         }
     }
-};
+}
 
-int main(int argc, int argv[])
+int isPrime(int n)
 {
-    binsearch(argv[0], argv[1]);
+    if(n <= 1)
+        return 0;
+        
+    for(int i = 2; i * i <= n; i++)
+    {
+        if(n % i == 0)
+            return 0;
+    }
+    
+    return 1;
+}
+
+int main()
+{
+    int num1, num2;
+    
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+    
+    if(isPrime(num1) && isPrime(num2))
+        printf("Sum of %d and %d is %d", num1, num2, num1 + num2);
+    else
+        printf("Both numbers are not prime.");
+        
+    return 0;
 }
