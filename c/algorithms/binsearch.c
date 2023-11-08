b9fcf009512a82068c9d2d1bc1d1398d8a8f2863
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-11-08 15:21:06 +0530

    PB-2 (task) Integrate issue comments with websocket handling

--- c/algorithms/binsearch.c
+++ c/algorithms/binsearch.c
@@ -1,29 +1,36 @@
+```c
 #include <stdio.h>
 
 int binsearch(int yarr[10], int element)
 {
     int mid = sizeof(yarr) % 2;
     int left = (int)yarr / 2 - mid;
-    int right = yarr - left;
+    int right = sizeof(yarr) / sizeof(yarr[0]) - left;
     extern int i;
     
-    if(element == mid)
-    printf("%d", &mid);
+    if(element == yarr[mid])
+    printf("%d", &yarr[mid]);
     
     if(element != yarr[right])
     {
         for(i = 0; i < left; i++)
         {
-            if(element == i)
+            if(element == yarr[i])
             {
             //Does this so that it doesn't print multiple times
-            printf("%d", i);
+            printf("%d", yarr[i]);
             }
         }
     }
-};
-
-int main(int argc, int argv[])
-{
-    binsearch(argv[0], argv[1]);
 }
+
+int main(int argc, char *argv[])
+{
+    int arr[10];
+    for(int i=0; i<argc-2; i++) {
+        arr[i] = atoi(argv[i+2]);
+    }
+    binsearch(arr, atoi(argv[1]));
+    return 0;
+}
+```
