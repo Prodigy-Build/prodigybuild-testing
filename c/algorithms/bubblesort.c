50540d4a4a0ec5e37895bad45eb5412a331269b8
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-11-09 16:10:22 +0530

    PB-1 (task) Add chatbot for customer support

--- c/algorithms/bubblesort.c
+++ c/algorithms/bubblesort.c
@@ -1,4 +1,6 @@
+```c
 #include <stdio.h>
+#include <assert.h>
 
 void bubble_sort(int *array, int len) 
 {
@@ -13,10 +15,34 @@ void bubble_sort(int *array, int len)
     }
 }
 
+void test_bubble_sort() {
+    // Test case 1
+    int arr1[] = {4, 2, 5, 1, 3};
+    bubble_sort(arr1, 5);
+    int sorted_arr1[] = {1, 2, 3, 4, 5};
+    for (int i = 0; i < 5; i++)
+        assert(arr1[i] == sorted_arr1[i]);
+
+    // Test case 2
+    int arr2[] = {9, 1, 6, 3, 2, 7, 8, 4, 5};
+    bubble_sort(arr2, 9);
+    int sorted_arr2[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    for (int i = 0; i < 9; i++)
+        assert(arr2[i] == sorted_arr2[i]);
+
+    // Test case 3
+    int arr3[] = {1};
+    bubble_sort(arr3, 1);
+    int sorted_arr3[] = {1};
+    for (int i = 0; i < 1; i++)
+        assert(arr3[i] == sorted_arr3[i]);
+}
+
 int main(void) 
 {
+    test_bubble_sort();
+
     int yarr[] = {3, 9, 4, 8, 7, 6, 1, 2, 0, 10};
-    
     bubble_sort(yarr, 10);
 
     printf("[");
@@ -25,3 +51,4 @@ int main(void)
     printf("\b\b]\n");
     return 0;
 }
+```
