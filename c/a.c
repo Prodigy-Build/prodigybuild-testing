958de59ac8346670aff06cd76355b2c5a089731c
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-11-20 17:41:49 +0530

    PB-1 (bug) Bitbucket Integration

---
+++ c/a.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int binsearch(int yarr[10], int element)
+{
+    int mid = sizeof(yarr) / sizeof(yarr[0]) / 2;
+    int left = mid - 1;
+    int right = mid + 1;
+    extern int i;
+    
+    if(element == yarr[mid])
+        printf("%d", yarr[mid]);
+    
+    if(element != yarr[right])
+    {
+        for(i = 0; i < left; i++)
+        {
+            if(element == yarr[i])
+            {
+                //Does this so that it doesn't print multiple times
+                printf("%d", yarr[i]);
+            }
+        }
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    int yarr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    binsearch(yarr, atoi(argv[1]));
+    return 0;
+}
