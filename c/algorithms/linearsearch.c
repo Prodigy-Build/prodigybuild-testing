43a2fbc7ceaa2a7272c9943ab82157035c0695c2
gresap <149705319+gresap@users.noreply.github.com>
2023-11-21 15:00:58 +0530

    PB-3 (task) Refactor the Linear Search file

--- c/algorithms/linearsearch.c
+++ c/algorithms/linearsearch.c
@@ -2,22 +2,20 @@
 
 int linsearch(int *yarr, int val)
 {
-    for(int i = 0; i < sizeof(yarr); i++)
+    int size = sizeof(yarr) / sizeof(yarr[0]);
+    for(int i = 0; i < size; i++)
     {
         if(yarr[i] == val)
         {
             return 0;
         }    
-        else
-        {
-          return 1;
-        }
     }
+    return 1;
 }
 
 int main()
 {
-    int *yarr = {"Shluck much", "much Shluck"};
-    int val = "Shluck Much";
+    int yarr[] = {100, 200, 300, 400, 500};
+    int val = 200;
     linsearch(yarr, val);
 }
