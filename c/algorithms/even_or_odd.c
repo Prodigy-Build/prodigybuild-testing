c3bdd5137e9c4d93cf5665aa82c1a9f66ce095bc
gresap <149705319+gresap@users.noreply.github.com>
2023-11-21 00:58:20 -0800

    PB-4 (task) Refactor the file

--- c/algorithms/even_or_odd.c
+++ c/algorithms/even_or_odd.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 
-int main()
-{
-    long num;
-    printf("Enter a number\n");
-    scanf("%ld", &num);
+// Function to check if a number is even or odd
+void checkEvenOdd(long num) {
     int rem = num % 2;
     
     if(rem == 0)
-    printf("Even");
+        printf("Even");
     else
-    printf("Odd");
+        printf("Odd");
+}
+
+int main() {
+    long num;
+    printf("Enter a number\n");
+    scanf("%ld", &num);
+    
+    // Call the function to check if the number is even or odd
+    checkEvenOdd(num);
+    
+    return 0;
 }
