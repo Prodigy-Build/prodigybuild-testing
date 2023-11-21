6f81cb689dff0c544b3b0ea4c96d1a51c87badc4
gresap <149705319+gresap@users.noreply.github.com>
2023-11-21 00:26:00 -0800

    PB-1 (task) Refactor the file

--- c/algorithms/even_or_odd.c
+++ c/algorithms/even_or_odd.c
@@ -1,14 +1,36 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+bool isEven(int num);
 
 int main()
 {
-    long num;
+    int num;
     printf("Enter a number\n");
-    scanf("%ld", &num);
+    scanf("%d", &num);
+    
+    if (isEven(num) == true)
+    {
+        printf("Even");
+    }
+    else
+    {
+        printf("Odd");
+    }
+    
+    return 0;
+}
+
+bool isEven(int num)
+{
     int rem = num % 2;
     
-    if(rem == 0)
-    printf("Even");
+    if (rem == 0)
+    {
+        return true;
+    }
     else
-    printf("Odd");
+    {
+        return false;
+    }
 }
