c8d0e57cc616f9f88240d7d15e95b97d794c64a7
gresap <149705319+gresap@users.noreply.github.com>
2023-11-21 15:00:03 +0530

    PB-4 (task) Refactor the Merge Sort file

--- c/algorithms/mergesort.c
+++ c/algorithms/mergesort.c
@@ -1,41 +1,45 @@
 #include<stdio.h>
-#include<time.h>
-#include<stdlib.h>
 
 // function to merge the sub-arrays
 void merge(int a[],int low,int mid ,int high)
 {
-	int b[20]; //same size of a[]
-	int i, j, k;
-	i = low, j = mid + 1,k = low;
+	int b[20];
+	int i, k;
+	int j = mid + 1;
+	i = low;
+	k = low;
+
 	while(i <= mid && j <= high)
 	{
-		if(a[i]<=a[j])
-		    b[k++]=a[i++];
+		if(a[i] <= a[j])
+		    b[k++] = a[i++];
 		else
-		   b[k++]=a[j++]; //copying the elements 
+		   b[k++] = a[j++];
 	}
-	while (i<=mid)
-		b[k++]=a[i++];
-	while 
-		(j<=high) b[k++]=a[j++];
-		for (k=low;k<=high;k++)
-	        a[k]=b[k];
+
+	while (i <= mid)
+		b[k++] = a[i++];
+
+	while (j <= high)
+		b[k++] = a[j++];
+
+	for (k = low; k <= high; k++)
+	    a[k]=b[k];
 }
 
 // merge sort function
 void mergesort(int a[],int low,int high)
 {
 	int mid;
-	if(low>=high)
-	  return;
-	mid=(low+high)/2;
+	if (low >= high)
+	    return;
+	mid = (low + high) / 2;
 	mergesort(a,low,mid);
 	mergesort(a,mid+1,high);
 	merge(a,low,mid,high);
 }
 
-// main fucntion
+// main function
 int main()
 {
 	int a[7] = {83, 20, 9, 50, 115, 61, 17};
@@ -45,9 +49,8 @@ int main()
 	
 	printf("\n Sorted numbers are: ");
 
-	// function to print the sorted array
 	int k;
 	for(k = 0; k < 7; k++)
-	    printf("%d, ",a[k]);
+	    printf("%d, ", a[k]);
 	return 0;
 }
