42bfa64880793d3132f39330e8c87a458bb74f64
swechter-prodigybuild <136387756+swechter-prodigybuild@users.noreply.github.com>
2023-12-05 21:01:29 -0600

    PB-1 (task) Github 5 file unit tests

--- python/algorithms/backtracking/all_permutations.py
+++ python/algorithms/backtracking/all_permutations.py
@@ -1,51 +1,77 @@
-"""
-        In this problem, we want to determine all possible permutations
-        of the given sequence. We use backtracking to solve this problem.
-
-        Time complexity: O(n! * n),
-        where n denotes the length of the given sequence.
-"""
-from __future__ import annotations
+from typing import List
+import unittest
 
 
-def generate_all_permutations(sequence: list[int | str]) -> None:
-    create_state_space_tree(sequence, [], 0, [0 for i in range(len(sequence))])
+def generate_all_permutations(sequence: List[int | str]) -> List[List[int | str]]:
+    permutations = []
+    create_state_space_tree(sequence, [], 0, [False] * len(sequence), permutations)
+    return permutations
 
 
 def create_state_space_tree(
-    sequence: list[int | str],
-    current_sequence: list[int | str],
+    sequence: List[int | str],
+    current_sequence: List[int | str],
     index: int,
-    index_used: list[int],
+    index_used: List[bool],
+    permutations: List[List[int | str]],
 ) -> None:
-    """
-    Creates a state space tree to iterate through each branch using DFS.
-    We know that each state has exactly len(sequence) - index children.
-    It terminates when it reaches the end of the given sequence.
-    """
-
     if index == len(sequence):
-        print(current_sequence)
+        permutations.append(current_sequence.copy())
         return
 
     for i in range(len(sequence)):
         if not index_used[i]:
             current_sequence.append(sequence[i])
             index_used[i] = True
-            create_state_space_tree(sequence, current_sequence, index + 1, index_used)
+            create_state_space_tree(sequence, current_sequence, index + 1, index_used, permutations)
             current_sequence.pop()
             index_used[i] = False
 
 
-"""
-remove the comment to take an input from the user
+class TestAllPermutations(unittest.TestCase):
+    def test_generate_all_permutations(self):
+        # Test case 1
+        sequence_1 = [3, 1, 2, 4]
+        expected_1 = [
+            [3, 1, 2, 4],
+            [3, 1, 4, 2],
+            [3, 2, 1, 4],
+            [3, 2, 4, 1],
+            [3, 4, 1, 2],
+            [3, 4, 2, 1],
+            [1, 3, 2, 4],
+            [1, 3, 4, 2],
+            [1, 2, 3, 4],
+            [1, 2, 4, 3],
+            [1, 4, 3, 2],
+            [1, 4, 2, 3],
+            [2, 3, 1, 4],
+            [2, 3, 4, 1],
+            [2, 1, 3, 4],
+            [2, 1, 4, 3],
+            [2, 4, 3, 1],
+            [2, 4, 1, 3],
+            [4, 3, 1, 2],
+            [4, 3, 2, 1],
+            [4, 1, 3, 2],
+            [4, 1, 2, 3],
+            [4, 2, 3, 1],
+            [4, 2, 1, 3],
+        ]
+        self.assertCountEqual(generate_all_permutations(sequence_1), expected_1)
 
-print("Enter the elements")
-sequence = list(map(int, input().split()))
-"""
+        # Test case 2
+        sequence_2 = ["A", "B", "C"]
+        expected_2 = [
+            ["A", "B", "C"],
+            ["A", "C", "B"],
+            ["B", "A", "C"],
+            ["B", "C", "A"],
+            ["C", "A", "B"],
+            ["C", "B", "A"],
+        ]
+        self.assertCountEqual(generate_all_permutations(sequence_2), expected_2)
 
-sequence: list[int | str] = [3, 1, 2, 4]
-generate_all_permutations(sequence)
 
-sequence_2: list[int | str] = ["A", "B", "C"]
-generate_all_permutations(sequence_2)
+if __name__ == "__main__":
+    unittest.main()
