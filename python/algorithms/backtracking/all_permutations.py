ba4ddbbd8c2c9b08bcb33e52dd8a2f5e8bb301cc
swechter-prodigybuild <136387756+swechter-prodigybuild@users.noreply.github.com>
2023-12-05 20:24:16 -0600

    PB-1 (task) Github 5 file unit tests

--- python/algorithms/backtracking/all_permutations.py
+++ python/algorithms/backtracking/all_permutations.py
@@ -1,51 +1,49 @@
-"""
-        In this problem, we want to determine all possible permutations
-        of the given sequence. We use backtracking to solve this problem.
-
-        Time complexity: O(n! * n),
-        where n denotes the length of the given sequence.
-"""
-from __future__ import annotations
+import unittest
 
 
-def generate_all_permutations(sequence: list[int | str]) -> None:
-    create_state_space_tree(sequence, [], 0, [0 for i in range(len(sequence))])
+class TestAllPermutations(unittest.TestCase):
+    def test_generate_all_permutations(self):
+        sequence = [3, 1, 2, 4]
+        permutations = [
+            [3, 1, 2, 4],
+            [3, 1, 4, 2],
+            [3, 2, 1, 4],
+            [3, 2, 4, 1],
+            [3, 4, 1, 2],
+            [3, 4, 2, 1],
+            [1, 3, 2, 4],
+            [1, 3, 4, 2],
+            [1, 2, 3, 4],
+            [1, 2, 4, 3],
+            [1, 4, 3, 2],
+            [1, 4, 2, 3],
+            [2, 1, 3, 4],
+            [2, 1, 4, 3],
+            [2, 3, 1, 4],
+            [2, 3, 4, 1],
+            [2, 4, 1, 3],
+            [2, 4, 3, 1],
+            [4, 1, 2, 3],
+            [4, 1, 3, 2],
+            [4, 2, 1, 3],
+            [4, 2, 3, 1],
+            [4, 3, 1, 2],
+            [4, 3, 2, 1],
+        ]
+
+        self.assertCountEqual(generate_all_permutations(sequence), permutations)
+
+        sequence_2 = ["A", "B", "C"]
+        permutations_2 = [
+            ["A", "B", "C"],
+            ["A", "C", "B"],
+            ["B", "A", "C"],
+            ["B", "C", "A"],
+            ["C", "A", "B"],
+            ["C", "B", "A"],
+        ]
+
+        self.assertCountEqual(generate_all_permutations(sequence_2), permutations_2)
 
 
-def create_state_space_tree(
-    sequence: list[int | str],
-    current_sequence: list[int | str],
-    index: int,
-    index_used: list[int],
-) -> None:
-    """
-    Creates a state space tree to iterate through each branch using DFS.
-    We know that each state has exactly len(sequence) - index children.
-    It terminates when it reaches the end of the given sequence.
-    """
-
-    if index == len(sequence):
-        print(current_sequence)
-        return
-
-    for i in range(len(sequence)):
-        if not index_used[i]:
-            current_sequence.append(sequence[i])
-            index_used[i] = True
-            create_state_space_tree(sequence, current_sequence, index + 1, index_used)
-            current_sequence.pop()
-            index_used[i] = False
-
-
-"""
-remove the comment to take an input from the user
-
-print("Enter the elements")
-sequence = list(map(int, input().split()))
-"""
-
-sequence: list[int | str] = [3, 1, 2, 4]
-generate_all_permutations(sequence)
-
-sequence_2: list[int | str] = ["A", "B", "C"]
-generate_all_permutations(sequence_2)
+unittest.main()
