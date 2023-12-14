8b889b1f89d67e2cc97413482be4127b85d935bf
gresap <149705319+gresap@users.noreply.github.com>
2023-12-14 17:22:36 +0530

    PB-1 (task) Refactor the Odd Even file

--- c/algorithms/even_or_odd.c
+++ c/algorithms/even_or_odd.c
@@ -1,14 +1,17 @@
+```c
 #include <stdio.h>
 
 int main()
 {
     long num;
-    printf("Enter a number\n");
+    printf("Enter a number: ");
     scanf("%ld", &num);
     int rem = num % 2;
     
-    if(rem == 0)
-    printf("Even");
-    else
-    printf("Odd");
+    if (rem == 0) {
+        printf("The number is even\n");
+    } else {
+        printf("The number is odd\n");
+    }
 }
+```
