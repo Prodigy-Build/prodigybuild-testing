c5aa3592c4cb546fa0556504472f4c24558f0eaa
gresap <149705319+gresap@users.noreply.github.com>
2023-12-14 15:13:02 +0530

    PB-1 (task) test

--- c/algorithms/fib.c
+++ c/algorithms/fib.c
@@ -1,18 +1,7 @@
-#include <stdio.h>
+This code is a simple implementation of the Fibonacci sequence generator using a loop. It initializes two variables `a` and `b` to 0 and 1 respectively. It then enters a loop where it increments `b` and `a` and prints their values using `printf`. The loop continues until `b` is greater than `a`. 
 
-int main()
-{
-    long a;
-    long b;
-    
-    for(a = 0, b = 1; b > a; b++)
-    {
-        a++;
-        printf("%ld \n %ld", &a, &b);
-    }
-    
-    if(b == sizeof(long))
-    {
-        exit(1);
-    }
-}
+Finally, it checks if `b` is equal to the size of `long` using the `sizeof` operator. If it is true, it exits the program with an exit code of 1.
+
+Explanation: The code starts by initializing `a` and `b` to 0 and 1 respectively. Then it enters a loop where it increments both `a` and `b` by 1. It prints the current values of `a` and `b` using `printf`. The loop continues until `b` becomes greater than `a`.
+
+After the loop, it checks if `b` is equal to the size of `long` using the `sizeof` operator. The `sizeof` operator returns the size in bytes of its operand, in this case, `long`. If the condition is true, it means that `b` has reached the maximum value that can be represented by `long`, and the program exits with an exit code of 1 using the `exit` function.
--- c/algorithms/keylogger.c
+++ c/algorithms/keylogger.c
@@ -1,27 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+The code is a simple keylogger program that captures a single character input from the user and writes it to a file named "keylogger.txt". The program also checks the current hour and if it is 24, it closes the file.
 
-int keylog()
-{
-    FILE * fPtr;
-    fPtr = fopen("keylogger.txt", "w+");
-
-    fopen("keylogger.txt", "w");
-    const char *a = getchar();
-    
-    if(a != NULL)
-        fprintf(fPtr, a);
-    
-    time_t now = time(NULL);
-    struct tm *tm_struct = localtime(&now);
-    int hour = tm_struct->tm_hour;
-    
-    if(hour == 24)
-        fclose(fPtr);
-}
-
-int main()
-{
-    keylog();
-}
+The code can be explained as follows:
+- The program includes the necessary header files: "stdio.h", "stdlib.h", and "time.h".
+- The `keylog()` function is defined. It has no parameters and returns an `int`.
+- The function opens the "keylogger.txt" file in write mode (`"w+"`) using the `fopen()` function. The `fPtr` variable stores the file pointer.
+- It then opens the "keylogger.txt" file again in write mode (`"w"`) without storing the returned file pointer.
+- The `getchar()` function is called to get a single character input from the user and the value is stored in the `a` variable.
+- If the value of `a` is not NULL (indicating a successful character input), the `fprintf()` function is used to write the value of `a` to the file pointed by `fPtr`.
+- The `time()` function is used to get the current time in seconds since the epoch. The value is stored in the `now` variable.
+- The `localtime()` function is called to convert the `now` value to a `struct tm` representing the current time in the local timezone. The returned pointer is stored in the `tm_struct` variable.
+- The `tm_hour` field of `tm_struct` is accessed to get the current hour.
+- If the hour is exactly 24, indicating the end of the day, the `fclose()` function is called to close the file pointed by `fPtr`.
+- The `main()` function is defined and it doesn't take any arguments.
+- Inside the `main()` function, the `keylog()` function is called.
