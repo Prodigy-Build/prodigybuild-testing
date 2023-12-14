32196dd5affe7fd8d833f236a8e3a1c2251dfbe6
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-12-14 22:34:51 +0530

    PB-2 (task) Issue 2

--- c/algorithms/fib.c
+++ c/algorithms/fib.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h> // Added for exit function
 
 int main()
 {
-    long a;
-    long b;
+    long a = 0; // Initialize variable a
+    long b = 1; // Initialize variable b
     
-    for(a = 0, b = 1; b > a; b++)
+    while (b > a) // Changed the for loop to a while loop
     {
-        a++;
-        printf("%ld \n %ld", &a, &b);
+        a++; // Increment variable a
+        printf("%ld \n %ld", a, b); // Removed & from printf arguments
+        b++; // Increment variable b
     }
     
-    if(b == sizeof(long))
+    if (b == sizeof(long))
     {
         exit(1);
     }
