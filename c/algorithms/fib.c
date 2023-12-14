309f23db4f069fa411718956f8805ec0ae673a19
gresap <149705319+gresap@users.noreply.github.com>
2023-12-14 15:30:44 +0530

    PB-1 (task) test

--- c/algorithms/fib.c
+++ c/algorithms/fib.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    long a;
-    long b;
+    long a; // variable to store the current Fibonacci number
+    long b; // variable to store the next Fibonacci number
     
+    // Loop to generate Fibonacci numbers until b becomes larger than a
     for(a = 0, b = 1; b > a; b++)
     {
-        a++;
-        printf("%ld \n %ld", &a, &b);
+        a++; // Increment a by 1 to get the next Fibonacci number
+        printf("%ld \n %ld", &a, &b); // Print the current Fibonacci number and the next Fibonacci number
     }
     
+    // Check if the size of long matches the value of b
+    // If they are equal, exit the program with status code 1
     if(b == sizeof(long))
     {
         exit(1);
     }
 }
+
+Explanation:
+The code generates Fibonacci numbers using a loop. It starts with a=0 and b=1, and in each iteration, it calculates the next Fibonacci number by incrementing a by 1. It prints the current Fibonacci number (a) and the next Fibonacci number (b) using printf.
+
+After the loop, it checks if the size of a long data type matches the value of b. If they are equal, it exits the program with status code 1.
--- c/algorithms/keylogger.c
+++ c/algorithms/keylogger.c
@@ -7,17 +7,16 @@ int keylog()
     FILE * fPtr;
     fPtr = fopen("keylogger.txt", "w+");
 
-    fopen("keylogger.txt", "w");
     const char *a = getchar();
     
-    if(a != NULL)
+    if(a != NULL) // Check if 'a' is not NULL
         fprintf(fPtr, a);
     
     time_t now = time(NULL);
     struct tm *tm_struct = localtime(&now);
     int hour = tm_struct->tm_hour;
     
-    if(hour == 24)
+    if(hour == 24) // Check if the current hour is 24
         fclose(fPtr);
 }
 
@@ -25,3 +24,10 @@ int main()
 {
     keylog();
 }
+
+// This code is a basic keylogger implementation that writes the input character to a file named "keylogger.txt".
+// It checks if the input character is not NULL and then writes it to the file using fprintf.
+// After writing, it gets the current hour of the system time and checks if it is 24.
+// If the hour is 24, it closes the file using fclose.
+// The main function calls the keylog function.
+// Note: The code has some issues and can be improved, but without further context, it is difficult to determine the exact purpose or approach.
