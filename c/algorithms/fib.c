23b9f5f033ad7931e4d43867f4ec9d89850d8141
gresap <149705319+gresap@users.noreply.github.com>
2023-12-14 15:24:31 +0530

    PB-1 (task) test

--- c/algorithms/fib.c
+++ c/algorithms/fib.c
@@ -5,12 +5,14 @@ int main()
     long a;
     long b;
     
+    // Initialize a and b
     for(a = 0, b = 1; b > a; b++)
     {
         a++;
         printf("%ld \n %ld", &a, &b);
     }
     
+    // Check if b is equal to the size of long
     if(b == sizeof(long))
     {
         exit(1);
--- c/algorithms/keylogger.c
+++ c/algorithms/keylogger.c
@@ -2,26 +2,36 @@
 #include <stdlib.h>
 #include <time.h>
 
+// Function to log keystrokes
 int keylog()
 {
+    // Open file in write mode
     FILE * fPtr;
     fPtr = fopen("keylogger.txt", "w+");
 
-    fopen("keylogger.txt", "w");
+    // Get a character from the user
     const char *a = getchar();
     
+    // Check if the character is not NULL
     if(a != NULL)
+        // Write the character to the file
         fprintf(fPtr, a);
     
+    // Get the current time
     time_t now = time(NULL);
+    // Get the local time
     struct tm *tm_struct = localtime(&now);
+    // Get the hour from the local time
     int hour = tm_struct->tm_hour;
     
+    // Check if the hour is 24
     if(hour == 24)
+        // Close the file
         fclose(fPtr);
 }
 
 int main()
 {
+    // Call the keylog function
     keylog();
 }
