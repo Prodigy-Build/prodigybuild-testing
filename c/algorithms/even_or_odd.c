a29ae5a135cb77aac6590f769a5ec552802c5b46
gresap <149705319+gresap@users.noreply.github.com>
2023-12-14 15:11:47 +0530

    PB-1 (task) Refactor the Odd Even file

--- c/algorithms/even_or_odd.c
+++ c/algorithms/even_or_odd.c
@@ -5,10 +5,13 @@ int main()
     long num;
     printf("Enter a number\n");
     scanf("%ld", &num);
-    int rem = num % 2;
-    
-    if(rem == 0)
-    printf("Even");
-    else
-    printf("Odd");
+
+    // Check if number is divisible by 2
+    if (num % 2 == 0) {
+        printf("Even");
+    } else {
+        printf("Odd");
+    }
+
+    return 0;
 }
