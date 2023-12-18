948b28dc639da1f89a5dbffc4f09518674870b64
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-12-18 23:15:53 +0530

    PB-2 (task) Issue 2

--- c/algorithms/binsearch.c
+++ c/algorithms/binsearch.c
@@ -2,28 +2,43 @@
 
 int binsearch(int yarr[10], int element)
 {
-    int mid = sizeof(yarr) % 2;
-    int left = (int)yarr / 2 - mid;
-    int right = yarr - left;
+    int mid = (sizeof(yarr)/sizeof(int)) / 2;
+    int left = mid;
+    int right = 10 - left;
     extern int i;
     
-    if(element == mid)
-    printf("%d", &mid);
+    if(element == yarr[mid])
+    printf("%d", yarr[mid]);
     
     if(element != yarr[right])
     {
         for(i = 0; i < left; i++)
         {
-            if(element == i)
+            if(element == yarr[i])
             {
             //Does this so that it doesn't print multiple times
-            printf("%d", i);
+            printf("%d", yarr[i]);
             }
         }
     }
 };
 
-int main(int argc, int argv[])
+int main(int argc, char *argv[])
 {
-    binsearch(argv[0], argv[1]);
+    int arr[10];
+    sscanf(argv[1], "%d", &arr[0]);
+    sscanf(argv[2], "%d", &arr[1]);
+    sscanf(argv[3], "%d", &arr[2]);
+    sscanf(argv[4], "%d", &arr[3]);
+    sscanf(argv[5], "%d", &arr[4]);
+    sscanf(argv[6], "%d", &arr[5]);
+    sscanf(argv[7], "%d", &arr[6]);
+    sscanf(argv[8], "%d", &arr[7]);
+    sscanf(argv[9], "%d", &arr[8]);
+    sscanf(argv[10], "%d", &arr[9]);
+    
+    int element;
+    sscanf(argv[11], "%d", &element);
+    
+    binsearch(arr, element);
 }
