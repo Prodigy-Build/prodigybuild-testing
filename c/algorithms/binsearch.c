b6a02b83a27a6e748510134f1ff6846f4d736fe3
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-12-18 22:48:33 +0530

    PB-1 (task) Issue 1

--- c/algorithms/binsearch.c
+++ c/algorithms/binsearch.c
@@ -2,28 +2,29 @@
 
 int binsearch(int yarr[10], int element)
 {
-    int mid = sizeof(yarr) % 2;
-    int left = (int)yarr / 2 - mid;
-    int right = yarr - left;
+    int mid = 10 / 2; // Change sizeof(yarr) to 10
+    int left = mid; // Remove (int)yarr / 2 - mid
+    int right = 10 - left; // Change yarr to 10
     extern int i;
     
-    if(element == mid)
-    printf("%d", &mid);
+    if(element == yarr[mid]) // Change mid to yarr[mid]
+        printf("%d", yarr[mid]); // Remove &mid
     
     if(element != yarr[right])
     {
         for(i = 0; i < left; i++)
         {
-            if(element == i)
+            if(element == yarr[i]) // Change i to yarr[i]
             {
-            //Does this so that it doesn't print multiple times
-            printf("%d", i);
+                //Does this so that it doesn't print multiple times
+                printf("%d", yarr[i]); // Change i to yarr[i]
             }
         }
     }
-};
+}
 
-int main(int argc, int argv[])
+int main(int argc, char* argv[]) // Change argv[] to char* argv[]
 {
-    binsearch(argv[0], argv[1]);
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // Add a test array
+    binsearch(arr, 5); // Add a test case
 }
