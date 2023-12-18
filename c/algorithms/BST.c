4c88d06db1e94a348effa258442a36361fe64e1c
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-12-18 19:32:35 +0530

    PB-1 (task) Issue 1

--- c/algorithms/BST.c
+++ c/algorithms/BST.c
@@ -1,6 +1,7 @@
 // Implementation of Binary Search Tree 
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 struct BST {
     int data;
@@ -38,30 +39,48 @@ int Search(struct BST* RootPtr, int item) { /*Implemented search using recursion
     } else if(item == RootPtr->data) {
         return 1; /*Returns 1 when element found*/
     } else if(item < RootPtr->data) {
-        Search(RootPtr->left, item); /*Otherwise search in left side of binary tree if searching value is less then the current node value*/
+        return Search(RootPtr->left, item); /*Otherwise search in left side of binary tree if searching value is less then the current node value*/
     } else {
-        Search(RootPtr->right, item); /*Otherwise search in right side of binary tree if searching value is greater then the current node value*/
+        return Search(RootPtr->right, item); /*Otherwise search in right side of binary tree if searching value is greater then the current node value*/
     }
 }
 
-void main() {
+void testInsert() {
     struct BST* RootPtr = NULL;
-    int item, cont, key;
-    do {
-        printf("Enter item: ");
-        scanf("%d",&item);
-        Insert(&RootPtr, item);
+    Insert(&RootPtr, 5);
+    assert(RootPtr != NULL);
+    assert(RootPtr->data == 5);
+    assert(RootPtr->left == NULL);
+    assert(RootPtr->right == NULL);
 
-        printf("\n1 to keep inserting/ 0 to Exit: ");
-        scanf("%d",&cont);
-    } while(cont == 1);
+    Insert(&RootPtr, 3);
+    assert(RootPtr->left != NULL);
+    assert(RootPtr->left->data == 3);
+    assert(RootPtr->left->left == NULL);
+    assert(RootPtr->left->right == NULL);
 
-    printf("\nEnter element to search: ");
-    scanf("%d",&key);
+    Insert(&RootPtr, 7);
+    assert(RootPtr->right != NULL);
+    assert(RootPtr->right->data == 7);
+    assert(RootPtr->right->left == NULL);
+    assert(RootPtr->right->right == NULL);
+}
 
-    if(Search(RootPtr, key) == 0) {
-        printf("\nFound\n");
-    } else {
-        printf("\nNot Found\n");
-    }
+void testSearch() {
+    struct BST* RootPtr = NULL;
+    Insert(&RootPtr, 5);
+    Insert(&RootPtr, 3);
+    Insert(&RootPtr, 7);
+
+    assert(Search(RootPtr, 5) == 1);
+    assert(Search(RootPtr, 3) == 1);
+    assert(Search(RootPtr, 7) == 1);
+    assert(Search(RootPtr, 2) == 0);
+    assert(Search(RootPtr, 8) == 0);
+}
+
+int main() {
+    testInsert();
+    testSearch();
+    return 0;
 }
