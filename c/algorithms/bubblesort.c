f81479557ab0db439e0327bab3b9d3df3a36d30f
gresap <149705319+gresap@users.noreply.github.com>
2023-12-18 13:34:08 +0530

    PB-5 (task) Refactor the Insertion Sort file

--- c/algorithms/bubblesort.c
+++ c/algorithms/bubblesort.c
@@ -1,6 +1,9 @@
+Here is the updated code:
+
+```c
 #include <stdio.h>
 
-void bubble_sort(int *array, int len) 
+void bubble_sort(int *array, int len)
 {
     for (int i = 0; i < len; i++) {
         for (int j = 0; j < len - i - 1; j++) {
@@ -13,15 +16,16 @@ void bubble_sort(int *array, int len)
     }
 }
 
-int main(void) 
+int main(void)
 {
     int yarr[] = {3, 9, 4, 8, 7, 6, 1, 2, 0, 10};
-    
+
     bubble_sort(yarr, 10);
 
     printf("[");
     for (int i = 0; i < 10; i++)
-        printf("%d, ", yarr[i]);	
+        printf("%d, ", yarr[i]);
     printf("\b\b]\n");
     return 0;
 }
+```
