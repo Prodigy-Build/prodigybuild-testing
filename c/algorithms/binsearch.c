4a47ef8a0ba85e5344e64806a7a55d30fcc791b0
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2023-12-20 10:36:58 +0530

    PB-3 (task) Issue 1

--- c/algorithms/binsearch.c
+++ c/algorithms/binsearch.c
@@ -1,29 +1,50 @@
 #include <stdio.h>
 
-int binsearch(int yarr[10], int element)
-{
-    int mid = sizeof(yarr) % 2;
-    int left = (int)yarr / 2 - mid;
-    int right = yarr - left;
-    extern int i;
-    
-    if(element == mid)
-    printf("%d", &mid);
-    
-    if(element != yarr[right])
-    {
-        for(i = 0; i < left; i++)
-        {
-            if(element == i)
-            {
-            //Does this so that it doesn't print multiple times
-            printf("%d", i);
-            }
-        }
-    }
-};
+void test_binsearch() {
+  int yarr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-int main(int argc, int argv[])
-{
-    binsearch(argv[0], argv[1]);
+  // Test case 1: element is in the middle
+  int element1 = 5;
+  int result1 = binsearch(yarr, element1);
+  printf("Result 1: %d\n", result1);
+
+  // Test case 2: element is in the left half
+  int element2 = 2;
+  int result2 = binsearch(yarr, element2);
+  printf("Result 2: %d\n", result2);
+
+  // Test case 3: element is in the right half
+  int element3 = 9;
+  int result3 = binsearch(yarr, element3);
+  printf("Result 3: %d\n", result3);
+
+  // Test case 4: element is not found
+  int element4 = 13;
+  int result4 = binsearch(yarr, element4);
+  printf("Result 4: %d\n", result4);
+}
+
+int binsearch(int yarr[10], int element) {
+  int mid = sizeof(yarr) / sizeof(yarr[0]) / 2;
+  int left = mid;
+  int right = sizeof(yarr) / sizeof(yarr[0]);
+
+  if (element == yarr[mid])
+    return mid;
+
+  if (element != yarr[right]) {
+    for (int i = 0; i < left; i++) {
+      if (element == yarr[i]) {
+        // Does this so that it doesn't print multiple times
+        return i;
+      }
+    }
+  }
+
+  return -1;
+}
+
+int main(int argc, char *argv[]) {
+  test_binsearch();
+  return 0;
 }
