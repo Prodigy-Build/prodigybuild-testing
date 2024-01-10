967347903e59b0925747edc89f5963cce0b024e1
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2024-01-10 16:34:27 +0530

    PB-600 (task) Issue 3

---
+++ c/algorithms/count_123.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+void count(int num)
+{
+    printf("[");
+    
+    if(num > 20)
+    {
+        int i;
+        for(i = 0; i < num; i++)
+            printf("\n%d, \n", i);
+            
+        if(i == num + 1)
+            printf("]");
+    } 
+    else
+    {
+        int i;
+        for(i = 0; i < num; i++)
+            printf("%d", i);
+            
+        if(i == num)
+            printf("]");
+    }
+}
+
+int main()
+{
+    int num;
+    printf("Count:~$ ");
+    scanf("%d", &num);
+    count(num);
+}
