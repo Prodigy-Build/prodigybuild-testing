5c1144d1c1fc1548fbce355c417e95ab2ee6b987
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2024-01-10 14:31:33 +0530

    PB-6 (task) Issue 3

--- c/algorithms/binsearch.c
+++ c/algorithms/binsearch.c
@@ -1,29 +1,39 @@
 #include <stdio.h>
+#include <assert.h>
 
 int binsearch(int yarr[10], int element)
 {
-    int mid = sizeof(yarr) % 2;
-    int left = (int)yarr / 2 - mid;
-    int right = yarr - left;
-    extern int i;
+    int mid;
+    int left = 0;
+    int right = 9;
+    int i;
     
-    if(element == mid)
-    printf("%d", &mid);
-    
-    if(element != yarr[right])
+    while(left <= right)
     {
-        for(i = 0; i < left; i++)
-        {
-            if(element == i)
-            {
-            //Does this so that it doesn't print multiple times
-            printf("%d", i);
-            }
-        }
-    }
-};
+        mid = left + (right - left) / 2;
+        
+        if(yarr[mid] == element)
+            return mid;
+            
+        if(yarr[mid] < element)
+            left = mid + 1;
 
-int main(int argc, int argv[])
+        if(yarr[mid] > element)
+            right = mid - 1;
+    }
+    return -1;
+}
+
+void testCases() 
 {
-    binsearch(argv[0], argv[1]);
+    int testArray[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    assert(binsearch(testArray, 3) == 2);
+    assert(binsearch(testArray, 5) == 4);
+    assert(binsearch(testArray, 11) == -1);
+}
+
+int main()
+{
+    testCases();
+    return 0;
 }
