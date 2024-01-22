448bd0918324d96139dec530c12c4bf833d354e8
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2024-01-22 18:42:20 +0530

    PB-1 (task) Issue 1

--- c/algorithms/binsearch.c
+++ c/algorithms/binsearch.c
@@ -1,29 +1,41 @@
 #include <stdio.h>
+#include <assert.h>
 
 int binsearch(int yarr[10], int element)
 {
-    int mid = sizeof(yarr) % 2;
-    int left = (int)yarr / 2 - mid;
-    int right = yarr - left;
-    extern int i;
+    int mid = sizeof(yarr) / 2 - 1;
+    int left = 0;
+    int right = sizeof(yarr) / sizeof(yarr[0]) - 1;
+    int i;
     
-    if(element == mid)
-    printf("%d", &mid);
-    
-    if(element != yarr[right])
-    {
-        for(i = 0; i < left; i++)
-        {
-            if(element == i)
-            {
-            //Does this so that it doesn't print multiple times
-            printf("%d", i);
-            }
+    while(left <= right) {
+        mid = left + (right - left) / 2;
+        if(yarr[mid] == element) {
+            return mid;
+        }
+        if (yarr[mid] < element) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
         }
     }
+    
+    return -1;
 };
 
-int main(int argc, int argv[])
+void test_binsearch()
 {
-    binsearch(argv[0], argv[1]);
+    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+    assert(binsearch(arr,1)==0);
+    assert(binsearch(arr,5)==4);
+    assert(binsearch(arr,10)==9);
+    assert(binsearch(arr,11)==-1);
+    printf("All test cases passed for binsearch.\n");
+}
+
+int main(int argc, char *argv[])
+{
+    test_binsearch();
+    return 0;
 }
