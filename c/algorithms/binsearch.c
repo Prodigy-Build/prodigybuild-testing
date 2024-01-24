25e840c17b81ab584aae24527341ec943802a518
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2024-01-24 18:22:42 +0530

    PB-1 (task) Issue 1

--- c/algorithms/binsearch.c
+++ c/algorithms/binsearch.c
@@ -1,29 +1,33 @@
 #include <stdio.h>
+#include <assert.h>
 
 int binsearch(int yarr[10], int element)
 {
-    int mid = sizeof(yarr) % 2;
-    int left = (int)yarr / 2 - mid;
-    int right = yarr - left;
-    extern int i;
-    
-    if(element == mid)
-    printf("%d", &mid);
-    
-    if(element != yarr[right])
-    {
-        for(i = 0; i < left; i++)
-        {
-            if(element == i)
-            {
-            //Does this so that it doesn't print multiple times
-            printf("%d", i);
-            }
-        }
+    int mid = sizeof(yarr) / 2;
+    int left = 0;
+    int right = sizeof(yarr) - 1;
+    while (left <= right) {
+        mid = left + (right - left) / 2;
+        if (yarr[mid] == element)
+            return mid;
+        if (yarr[mid] < element)
+            left = mid + 1;
+        else
+            right = mid - 1;
     }
-};
+    return -1;
+}
+
+void test_binsearch() {
+    int yarr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    assert(binsearch(yarr, 5) == 4);
+    assert(binsearch(yarr, 11) == -1);
+    assert(binsearch(yarr, -1) == -1);
+    printf("All test cases passed successfully\n");
+}
 
 int main(int argc, int argv[])
 {
-    binsearch(argv[0], argv[1]);
+    test_binsearch();
+    return 0;
 }
