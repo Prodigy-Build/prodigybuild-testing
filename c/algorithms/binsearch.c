9f92e52814b2c50b24bbd120146dede11daeac79
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2024-01-29 15:15:15 +0530

    PB-1 (task) Test issue

--- c/algorithms/binsearch.c
+++ c/algorithms/binsearch.c
@@ -1,29 +1,56 @@
 #include <stdio.h>
+#include <assert.h>
 
 int binsearch(int yarr[10], int element)
 {
-    int mid = sizeof(yarr) % 2;
-    int left = (int)yarr / 2 - mid;
-    int right = yarr - left;
+    int mid = sizeof(yarr) / sizeof(yarr[0]) / 2;
+    int left = mid;
+    int right = sizeof(yarr) / sizeof(yarr[0]) - 1;
     extern int i;
     
-    if(element == mid)
-    printf("%d", &mid);
+    if(element == yarr[mid])
+    {
+        printf("%d", yarr[mid]);
+        return mid;
+    }
     
     if(element != yarr[right])
     {
         for(i = 0; i < left; i++)
         {
-            if(element == i)
+            if(element == yarr[i])
             {
-            //Does this so that it doesn't print multiple times
-            printf("%d", i);
+                printf("%d", yarr[i]);
+                return i;
             }
         }
     }
-};
+    
+    return -1;
+}
 
-int main(int argc, int argv[])
+void test_binsearch()
 {
-    binsearch(argv[0], argv[1]);
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    
+    // Test case 1: element is in the middle
+    assert(binsearch(arr, 5) == 4);
+    
+    // Test case 2: element is in the left half
+    assert(binsearch(arr, 2) == 1);
+    
+    // Test case 3: element is in the right half
+    assert(binsearch(arr, 8) == 7);
+    
+    // Test case 4: element is not in the array
+    assert(binsearch(arr, 11) == -1);
+    
+    printf("All test cases passed!\n");
+}
+
+int main(int argc, char *argv[])
+{
+    test_binsearch();
+    
+    return 0;
 }
