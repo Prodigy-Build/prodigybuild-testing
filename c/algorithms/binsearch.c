5a14acb232a08e5ef935f3b1540832cd596f6b0a
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2024-02-12 12:17:33 +0530

    PB-619 (task) test1 ~

--- c/algorithms/binsearch.c
+++ c/algorithms/binsearch.c
@@ -1,29 +1,47 @@
 #include <stdio.h>
 
-int binsearch(int yarr[10], int element)
+int binsearch(int yarr[], int size, int element)
 {
-    int mid = sizeof(yarr) % 2;
-    int left = (int)yarr / 2 - mid;
-    int right = yarr - left;
-    extern int i;
+    int left = 0;
+    int right = size - 1;
+    int mid;
     
-    if(element == mid)
-    printf("%d", &mid);
-    
-    if(element != yarr[right])
+    while(left <= right)
     {
-        for(i = 0; i < left; i++)
+        mid = left + (right - left) / 2;
+        
+        if(yarr[mid] == element)
         {
-            if(element == i)
-            {
-            //Does this so that it doesn't print multiple times
-            printf("%d", i);
-            }
+            return mid;
+        }
+        
+        if(yarr[mid] < element)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
         }
     }
-};
+    
+    return -1;
+}
 
-int main(int argc, int argv[])
+int main(int argc, char *argv[])
 {
-    binsearch(argv[0], argv[1]);
+    int array[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int element = atoi(argv[1]);
+    int result = binsearch(array, 10, element);
+    
+    if(result != -1)
+    {
+        printf("Element found at index %d\n", result);
+    }
+    else
+    {
+        printf("Element not found in array\n");
+    }
+    
+    return 0;
 }
