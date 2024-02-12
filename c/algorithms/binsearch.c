82517d83c13f9bff258d3ca0b20cf7447b5f4152
heena-prodigy <135728238+heena-prodigy@users.noreply.github.com>
2024-02-12 12:24:43 +0530

    PB-623 (task) Test VCS issue

--- c/algorithms/binsearch.c
+++ c/algorithms/binsearch.c
@@ -2,28 +2,43 @@
 
 int binsearch(int yarr[10], int element)
 {
-    int mid = sizeof(yarr) % 2;
-    int left = (int)yarr / 2 - mid;
-    int right = yarr - left;
-    extern int i;
+    int mid;
+    int left = 0;
+    int right = 10 - 1;
+    int i;
     
-    if(element == mid)
-    printf("%d", &mid);
-    
-    if(element != yarr[right])
+    while (left <= right)
     {
-        for(i = 0; i < left; i++)
+        mid = left + (right - left) / 2;
+        
+        if (yarr[mid] == element)
         {
-            if(element == i)
-            {
-            //Does this so that it doesn't print multiple times
-            printf("%d", i);
-            }
+            return mid;
+        }
+        else if (yarr[mid] < element)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
         }
     }
+    return -1;
 };
 
-int main(int argc, int argv[])
+int main(int argc, char *argv[])
 {
-    binsearch(argv[0], argv[1]);
+    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+    int element = atoi(argv[1]);
+
+    int result = binsearch(arr, element);
+
+    if (result != -1){
+     printf("Element found at index %d\n", result);
+     } 
+     else{
+       printf("Element not found\n");
+     }
+    return 0;
 }
