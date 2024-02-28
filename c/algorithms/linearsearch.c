607a850392248c5da6a12bb0d403cf2b0638d633
rahulprodigy <136299980+rahulprodigy@users.noreply.github.com>
2024-02-28 15:19:12 +0530

    PBD-6 (task) Small

--- c/algorithms/linearsearch.c
+++ c/algorithms/linearsearch.c
@@ -1,23 +1,61 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int linsearch(int *yarr, int val)
+// Function to perform linear search
+bool linsearch(int *yarr, int size, int val)
 {
-    for(int i = 0; i < sizeof(yarr); i++)
+    for(int i = 0; i < size; i++)
     {
         if(yarr[i] == val)
         {
-            return 0;
+            return true;
         }    
-        else
-        {
-          return 1;
-        }
+    }
+    return false;
+}
+
+// Unit test cases
+void test_linsearch()
+{
+    int arr1[] = {1, 2, 3, 4, 5};
+    int size1 = sizeof(arr1) / sizeof(arr1[0]);
+    int val1 = 3;
+    if(linsearch(arr1, size1, val1))
+    {
+        printf("Test case 1 passed\n");
+    }
+    else
+    {
+        printf("Test case 1 failed\n");
+    }
+    
+    int arr2[] = {10, 20, 30, 40, 50};
+    int size2 = sizeof(arr2) / sizeof(arr2[0]);
+    int val2 = 25;
+    if(!linsearch(arr2, size2, val2))
+    {
+        printf("Test case 2 passed\n");
+    }
+    else
+    {
+        printf("Test case 2 failed\n");
+    }
+    
+    int arr3[] = {100, 200, 300, 400, 500};
+    int size3 = sizeof(arr3) / sizeof(arr3[0]);
+    int val3 = 100;
+    if(linsearch(arr3, size3, val3))
+    {
+        printf("Test case 3 passed\n");
+    }
+    else
+    {
+        printf("Test case 3 failed\n");
     }
 }
 
 int main()
 {
-    int *yarr = {"Shluck much", "much Shluck"};
-    int val = "Shluck Much";
-    linsearch(yarr, val);
+    test_linsearch();
+    return 0;
 }
